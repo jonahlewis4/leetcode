238db9323ad216d599b1c35ce05ac91cf8cdc583
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2024-12-25 17:26:52 -0800

    Time: 126 ms (5.79%), Space: 52.7 MB (5.01%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,27 @@
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        //sort into ascending order
+        std::sort(intervals.begin(), intervals.end(), [](vector<int> x, vector<int> y) {
+            return x[0] < y[0];
+        });
+        
+        vector<vector<int>> result;
+        result.push_back(intervals[0]);
+        for(int i = 1; i < intervals.size(); i++){
+            //if previous and current overlap, merge them by 
+            //setting the result to the merged.
+            int n = result.size();
+            if(intervals[i][0] <= result[n - 1][1]){
+                int mergeStart = min(result[n - 1][0], intervals[i][0]);
+                int mergeEnd = max(result[n - 1][1], intervals[i][1]);
+                result[n - 1][0] = mergeStart;
+                result[n - 1][1] = mergeEnd;
+            } else {
+                result.push_back(intervals[i]);
+            }
+        }
+        return result;
+    }
+};
