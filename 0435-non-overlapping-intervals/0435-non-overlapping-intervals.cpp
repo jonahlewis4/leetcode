4b0dad23c01bf9642dd831f408459f0ee6b2d279
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2024-12-25 19:12:43 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,30 +1,6 @@
 class Solution {
 public:
-    
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        //sort
-        std::sort(intervals.begin(), intervals.end(), [](vector<int> &x, vector<int> &y){
-            return x[0] < y[0];
-        });
         
-        //walk through while tracking the endPoint for overlapping
-        //if we do not have an overlap, set the endPoint to the new one becausse it is a new interval
-        //if we have an overlap, we 'delete' the one with the further endpoint because
-        //we want to keep the one that extends less to the right and allows us to get away with less deletions.
-
-        
-        int end = intervals[0][1];
-        int deletions = 0;
-        for(int i = 1; i < intervals.size(); i++){
-            if(intervals[i][0] < end){
-                deletions++;
-                end = min(end, intervals[i][1]);
-            } else {
-                end = intervals[i][1];
-            }
-        }
-        return deletions;
-
-
     }
 };
