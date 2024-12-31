8a042ba4a7c31c38e188268cc75d44e789f8d572
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2024-12-31 00:01:20 -0800

    Time: 93 ms (5.01%), Space: 73.1 MB (72.22%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+
+        unordered_map<int, int> map;  
+        cout<<nums.size()<<endl;
+
+        for(int i = 0; i < nums.size(); i++){
+           if(map.count(nums[i]) == 0){
+            map[nums[i]] = 1;
+           }
+           else {
+            return true;
+           }
+        }
+        return false;
+        
+    }
+};
