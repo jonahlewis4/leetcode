e0e31ef5665732ec8828077e393cb20ef50f1baf
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-01-01 14:28:43 -0800

    Time: 0 ms (100%), Space: 10.6 MB (47.37%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
+        
+        if(n == 0){
+            return {0};
+        }
+
         vector<int> res(n + 1);
-        for(int i = 0; i <= n; i++){
-            res[i] = num1s(i);
+        res[0] = 0;
+        res[1] = 1;
+
+        int offsetCounter = 0;
+        int offset = 2;
+
+        for(int i = 2; i <= n; i++){
+            res[i] = 1 + res[i - offset];
+            offsetCounter++;
+            if(offsetCounter >= offset){
+                offset *= 2;
+                offsetCounter = 0;
+            }
         }
         return res;
     }
-    int num1s(int n){
-        int num1s = 0;
-        while( n > 0){
-            n = n & (n - 1);
-            num1s++;
-        }
-        return num1s;
-    }
+    
 };
