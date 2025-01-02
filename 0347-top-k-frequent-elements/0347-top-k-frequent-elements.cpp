0f312cb9950b14a2dd4673e23639b2f25f4cc368
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-01-01 22:06:15 -0800

    Time: 1 ms (74.37%), Space: 18 MB (26.1%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -6,17 +6,29 @@ public:
         for(const auto & num : nums){
             frequency[num]++;
         }
-        
-        std::priority_queue<pair<int, int>> pq;
+
+        int maxCount = INT_MIN;
         for(const auto & keyVal : frequency){
-            pq.push({keyVal.second, keyVal.first});
-        }   
+            maxCount = std::max(maxCount, keyVal.second);
+        }
+
+        std::vector<vector<int>> count(maxCount, vector<int>(0));
+
+        for(const auto & keyVal : frequency) {
+            count[keyVal.second - 1].push_back(keyVal.first);
+        }
 
         std::vector<int> res(k);
+
+        int countIndex = count.size() - 1;
         for(int i = 0; i < k; i++){
-            res[i] = pq.top().second;
-            pq.pop();
+            while(count[countIndex].empty()){
+                countIndex--;
+            }
+            res[i] = count[countIndex].back();
+            count[countIndex].pop_back();
         }
         return res;
+    
     }
 };
