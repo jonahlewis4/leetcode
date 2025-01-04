9057fb8d1da452bdb6bdbc9a30ed1e718d5dd4f8
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-01-03 18:17:17 -0800

    Time: 0 ms (100%), Space: 19.4 MB (42.37%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,43 +1,20 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        for(int i = 0; i < numbers.size(); i++){
-            int cand = numbers[i];
-            int compliment = target - cand;
-            int j = binarySearch(numbers, compliment);
-            if(i == j){
-                int left = i - 1 >= 0 ? numbers[i - 1] : INT_MIN;
-                int right = i + 1 < numbers.size() ? numbers[i + 1] : INT_MAX;
-
-                if(left == compliment){
-                    return {i + 1, i - 1 + 1};
-                }
-                if(right == compliment){
-                    return {i + 1, i + 1 + 1};
-                }
-
-                return {};
-            } else if (j >= 0){
-                return {i + 1, j + 1};
+        int l = 0;
+        int r = numbers.size() - 1;
+        while( l < r ){
+            int sum = numbers[l] + numbers[r];
+            if(sum == target){
+                return {l + 1, r + 1};
+            }
+            else if(sum > target){
+                r--;
+            }
+            else{
+                l++;
             }
         }
         return {};
-    }
-    int binarySearch(vector<int> & nums, int n){
-        int l = 0;
-        int r = nums.size() - 1;
-
-        while(l <= r){
-            int m = (l + r) / 2;
-            if(nums[m] == n){
-                return m;
-            } else if(nums[m] < n){
-                l = m + 1;
-            } else if(nums[m] > n){
-                r = m - 1;
-            }
-            
-        }
-        return -1;
     }
 };
