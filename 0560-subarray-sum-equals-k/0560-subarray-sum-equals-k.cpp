df21fca0db2b0a30242b7c5c8698ba103f796511
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-01-04 02:04:25 -0800

    Time: 51 ms (55.48%), Space: 51.1 MB (17.15%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> numCSum;
+        numCSum[0] = 1;
+        
+        int cSum = 0;
         int matches = 0;
         for(int i = 0; i < nums.size(); i++){
-            int sum = 0;
-            for(int j = i; j < nums.size(); j++){
-                sum += nums[j];
-                if(sum == k){
-                    matches++;
-                }
-            }
+            //see how many times the difference exists
+            cSum += nums[i];
+            matches += numCSum[cSum - k];
+            numCSum[cSum]++;
         }
         return matches;
+
     }
 
     
