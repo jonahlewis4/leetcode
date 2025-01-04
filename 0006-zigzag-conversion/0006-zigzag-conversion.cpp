f747c32d367b95528e5b9911116ec64906f19d07
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-01-04 15:05:55 -0800

    Time: 0 ms (100%), Space: 11 MB (87.21%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -4,38 +4,24 @@ public:
         if(numRows == 1){
             return s;
         }
-        int colSize = ceil((double)s.size() / (2 * numRows - 2)) * (numRows - 1);
-        cout<<"Colsize: "<<colSize<<endl;
-        cout<<"s size: "<<s.size()<<endl;
-        vector<vector<char>> grid(numRows, vector<char>(colSize, ' '));
+        string res;
+        int n = s.size(); 
+        int charsInSection = 2 * numRows - 2;
 
-        int r = 0;
-        int c = 0;
-        int i   = 0;
-        while(i < s.size())
-        {
-            while(i < s.size() && r < numRows && c < colSize){ 
-                
-                grid[r][c] = s[i];
-                r++;
-                i++;
-            }
-            r -= 2;
-            c++;
-            while(i < s.size() && r > 0 && c < colSize){
-        
-                grid[r][c] = s[i];
-                r--;
-                c++;
-                i++;
-            }
-        }
-        string res = "";
-        for(int    r = 0; r < numRows; r++){
-            for(int c = 0; c < grid[r].size(); c++){
-                if(grid[r][c] != ' '){
-                    res += grid[r][c];
+        for(int r = 0; r < numRows; r++){
+            int i = r;
+            while(i < n){
+                res += s[i];
+
+                if(r != 0 && r != numRows - 1){
+                    //grab next one
+                    int followIdx = i + (charsInSection - 2 * r);
+                    if(followIdx < n){
+                        res += s[followIdx];
+
+                    }
                 }
+                i += charsInSection;
             }
         }
         return res;
