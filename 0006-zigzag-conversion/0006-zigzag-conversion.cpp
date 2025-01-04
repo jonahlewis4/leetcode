c15dead2f869abe78076c27600d707d8d4244307
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-01-04 14:33:51 -0800

    Time: 1077 ms (5.03%), Space: 37.1 MB (5.86%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -4,29 +4,39 @@ public:
         if(numRows == 1){
             return s;
         }
-        
-        vector<vector<char>> grid(numRows, vector<char>(s.size(), ' '));
+        int colSize = ceil((double)s.size() / (2 * numRows - 2)) * (numRows - 1);
+        cout<<"Colsize: "<<colSize<<endl;
+        cout<<"s size: "<<s.size()<<endl;
+        vector<vector<char>> grid(numRows, vector<char>(colSize, ' '));
 
         int r = 0;
         int c = 0;
         int i   = 0;
         while(i < s.size())
         {
-            while(i < s.size() && r < numRows){ 
+            while(i < s.size() && r < numRows && c < colSize){ 
+                cout<<"i: "<<i<<endl;
+                cout<<"r: "<<r<<endl;
+                cout<<"c: "<<c<<endl;
+                cout<<endl;
                 grid[r][c] = s[i];
                 r++;
                 i++;
             }
             r -= 2;
             c++;
-            while(i < s.size() && r > 0 && c < grid[r].size()){
+            while(i < s.size() && r > 0 && c < colSize){
+                cout<<"i: "<<i<<endl;
+                cout<<"r: "<<r<<endl;
+                cout<<"c: "<<c<<endl;
+                cout<<endl;
                 grid[r][c] = s[i];
                 r--;
                 c++;
                 i++;
             }
         }
-
+        cout<<"built grid"<<endl;
         string res = "";
         for(int    r = 0; r < numRows; r++){
             for(int c = 0; c < grid[r].size(); c++){
