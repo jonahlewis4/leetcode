c23d8f897c5a1a3f03181e0a7c19b9898500e2c4
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-01-03 23:21:11 -0800

    Time: 39 ms (95.85%), Space: 29 MB (69.37%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,43 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size();
         std::sort(nums.begin(), nums.end());
 
-        std::vector<vector<int>> res;
-        for(int i = 0; i < nums.size() - 2; i++){
-            int first = nums[i];
-            int target = 0 - nums[i];
+        int n = nums.size();
 
+        vector<vector<int>> res;
+        for(int i = 0; i < n - 2; i++){
             int l = i + 1;
-            int r = nums.size() - 1;
-
+            int r = n - 1;
+            int target = 0 - nums[i];
             while(l < r){
-        
-
-
                 int sum = nums[l] + nums[r];
-
-                if(sum == target){
-                    res.push_back({first, nums[l], nums[r]});
+                if(sum < target){
+                    l++;
+                }
+                else if (sum > target){
+                    r--;
+                }
+                else {
+                    res.push_back({nums[i], nums[l], nums[r]});
                     l++;
                     while(l < r && nums[l] == nums[l - 1]){
                         l++;
                     }
                 }
-                if( sum < target){
-                    l++;
-                } else {
-                    r--;
-                }
-
-                
-                // while(r > l && r + 1 < nums.size() && nums[r] == nums[r + 1]){
-                //     r--;
-                // }
             }
-
-            while(i + 1 < nums.size() && nums[i + 1] == nums[i]){
+            while(i + 1 < n - 2 && nums[i] == nums[i + 1]){
                 i++;
             }
         }
