741b93cfd48682c210efd4e64d9c1825787907bd
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-01-04 22:55:17 -0800

    Time: 26 ms (60.11%), Space: 80.2 MB (73.34%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -12,7 +12,7 @@ public:
         heights.push_back(0);
         for(int i = 0; i < heights.size(); i++){
             int min = i;
-            while(!stack.empty() && heights[stack.top().index] > heights[i]){
+            while(!stack.empty() && heights[stack.top().index] >= heights[i]){
                 int area = (i - stack.top().start) * heights[stack.top().index];
                 maxArea = max(area, maxArea);
                 min = stack.top().start;
