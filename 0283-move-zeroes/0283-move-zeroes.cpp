ebc997c48df0910c45be4f4afa00aa114bf4c7e7
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-01-05 18:55:46 -0800

    Time: 0 ms (100%), Space: 23.8 MB (55.45%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
-  void moveZeroes(vector<int>& nums) {
-      for (int lastNonZeroFoundAt = 0, cur = 0; cur < nums.size(); cur++) {
-          if (nums[cur] != 0) {
-              swap(nums[lastNonZeroFoundAt++], nums[cur]);
-          }
-      }
-  }
+    void moveZeroes(vector<int>& nums) {
+        
+        int l = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] != 0){
+                swap(nums[i], nums[l]);
+                l++;
+            } 
+        }
+    }
 };
