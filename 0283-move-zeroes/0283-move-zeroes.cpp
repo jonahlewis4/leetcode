021222692f96e035194238d3fa088e2b57702508
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-01-05 18:05:53 -0800

    Time: 186 ms (5.01%), Space: 23.9 MB (35.49%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int l = 0;
+        int r = 0;
+        
+        int numZeroes = 0;
+
+     
+        while(true){
+            
+            while(l < nums.size() && nums[l] != 0){
+                l++;
+            }
+            r = l;
+            while(r < nums.size() - numZeroes && nums[r] == 0){
+                r++;
+            }
+            if(r >= nums.size() || l >= nums.size() || l > r){
+                break;
+            }
+            std::swap(nums[l], nums[r]);
+            cout<<"swap"<<endl;
+        }
+    }
+};
