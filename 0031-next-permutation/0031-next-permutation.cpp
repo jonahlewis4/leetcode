48a5802245d35cb10179266bbdf68be221137604
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-01-06 01:18:04 -0800

    Time: 0 ms (100%), Space: 15.8 MB (21.99%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int i = nums.size() - 1;
+        while(i - 1 >= 0 && nums[i - 1] >= nums[i]){
+            i--;
+        }
+        if(i - 1 < 0){
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+        i--;
+        int replaced = nums[i];
+
+        int j = nums.size() - 1;
+        while(j >= 0 && nums[j] <= replaced){
+            j--;
+        }
+
+        if(j < 0){
+
+        }
+        int firstLarger = nums[j];
+
+        nums[i] = firstLarger;
+
+        int distance = (nums.size() - 1 - i)/2 ;
+        for(int k = nums.size() - 1; k >= nums.size() - distance && k >= 0; k--){
+            int match = i + nums.size() - k;
+            swap(nums[match], nums[k]);
+        }
+
+        
+        nums[ i + nums.size() - j] = replaced;
+    }
+};
