e53be3e3e12f09ab99dea46f7836baf56884304d
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-01-05 16:40:25 -0800

    Time: 1101 ms (5%), Space: 11.6 MB (76.72%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if(s.size() == 0){
+            return 0;
+        }
+        
+        std::unordered_map<char, int> map;
+        int l = 0;
+        int r = 0;
+
+        int longest = 0;
+
+        while(r < s.size()){
+            cout<<"l: "<<l<<" is "<<s[l]<<endl;
+            cout<<"r: "<<r<<" is "<<s[r]<<endl;
+            cout<<endl;
+            //longest = max(longest, r - l );
+            if(map.find(s[r]) == map.end()){
+                cout<<"not in map"<<endl;
+                map[s[r]] = r;
+            } else {
+                int newL = (map[s[r]]) + 1;
+                if(longest < r - l){
+                    cout<<"new longest: "<<r - l<<endl;
+                }
+                longest = max(longest, r - l);
+
+                cout<<"newL: "<<newL<<endl;
+                while(l < newL){
+                    //map.erase(map[s[l]]);
+                    l++;
+                }
+                map[s[r]] = r;
+            }
+            r++;
+        }
+        longest = max(longest, r - l );
+
+        return longest;
+    }
+};
