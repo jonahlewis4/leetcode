cd732a98ba1b5709fcb374af15eb74b27162f245
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-01-09 23:55:53 -0800

    Time: 0 ms (100%), Space: 11.5 MB (50.56%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,39 +1,22 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int num0s = 0;
-        int num1s = 0;
-        int num2s = 0;
+        int zeroRight = 0;
+        int twoLeft = nums.size() - 1;
 
-        for(const int num : nums){
-            switch (num) {
-                case 0: {
-                    num0s++;
-                    break;
-                }
-                case 1: {
-                    num1s++;
-                    break;
-                }
-                case 2: {
-                    num2s++;
-                    break;
-                }
+
+
+
+        for(int i = zeroRight; i <= twoLeft; i++){
+            if(nums[i] == 0){
+                std::swap(nums[i], nums[zeroRight]);
+                zeroRight++;
+            } else if (nums[i] == 2){
+                std::swap(nums[i], nums[twoLeft]);
+                twoLeft--;
+                i--;
             }
-        }    
-
-        int i = 0;
-        for(int j = 0; j < num0s; j++){
-            nums[i] = 0;
-            i++;
         }
-        for(int j = 0; j < num1s; j++){
-            nums[i] = 1;
-            i++;
-        }
-        for(int j = 0; j < num2s; j++){
-            nums[i] = 2;
-            i++;
-        }
+        
     }
 };
