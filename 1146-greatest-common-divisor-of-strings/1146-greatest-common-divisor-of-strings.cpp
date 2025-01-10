10e4d8c61e2121e7d27ba9d1211e4e9135d241f9
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-01-09 22:00:37 -0800

    Time: 0 ms (100%), Space: 9.9 MB (17.98%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
@@ -1,46 +1,13 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        if(str1.size() < str2.size()){
-            std::swap(str1, str2);
+        if(str1 + str2 != str2 + str1){
+            return "";
         }
-        std::unordered_set<string> divs;
-        for(int i = 0; i < str2.size(); i++){
-            int k = 0;
-            bool match = true;
-            for(int j = 0; j < str2.size(); j++){
-                if(str2[j] != str2[k]){
-                    match = false;
-                    break;
-                }
-                k++;
-                k = k % (i + 1);
-            }
-            if(match && k == 0){
-                divs.insert(str2.substr(0, i + 1));
-            }
-        }
-        
-        string longest = "";
-        for(const auto & divisor : divs){
-            if(divides(str1, divisor) && divisor.size() > longest.size()){
-                longest = divisor;
-            }
-        }
-        return longest;
+
+        int gcdLength = gcd(str1.size(), str2.size());
+        return (str1 + str2).substr(0, gcdLength);
     }
 
-    bool divides(const string &s, const string& possibleDivisor){
-        int divisorIndex = 0;
-        int sIndex = 0;
-        while(sIndex < s.size()){
-            if(s[sIndex] != possibleDivisor[divisorIndex]){
-                return false;
-            }
-            sIndex++;
-            divisorIndex++;
-            divisorIndex = divisorIndex % possibleDivisor.size();
-        }
-        return divisorIndex == 0;
-    }
+  
 };
