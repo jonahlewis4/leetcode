a69a7b0ac33ee10b175014f8bd946d05c992dde5
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-01-10 23:01:23 -0800

    Time: 256 ms (88.03%), Space: 181.6 MB (87.32%) - LeetHub

--- 3594-identify-the-largest-outlier-in-an-array/3594-identify-the-largest-outlier-in-an-array.cpp
+++ 3594-identify-the-largest-outlier-in-an-array/3594-identify-the-largest-outlier-in-an-array.cpp
@@ -3,18 +3,18 @@ public:
     int getLargestOutlier(vector<int>& nums) {
         int sum = 0;
 
-        std::unordered_map<int, vector<int>> set;
+        std::unordered_map<int, int> set;
 
         for (int i = 0; i < nums.size(); i++){
             sum += nums[i];
-            set[nums[i]].push_back(i);
+            set[nums[i]]++;
         }
 
         int maxOutlier = INT_MIN;
         for(int i = 0; i < nums.size(); i++){
             int twiceGoal = sum - nums[i];
             if(twiceGoal % 2 == 0 && set.find(twiceGoal / 2) != set.end()){
-                if((twiceGoal / 2 == nums[i] && set[twiceGoal / 2].size() > 1) || twiceGoal / 2 != nums[i]){
+                if((twiceGoal / 2 == nums[i] && set[twiceGoal / 2] > 1) || twiceGoal / 2 != nums[i]){
                     maxOutlier = max(maxOutlier, nums[i]);
                 }
             }
