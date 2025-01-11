eb6090b40f70e2eba6c486d4e805491cb4852bdc
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-01-10 23:36:18 -0800

    Time: 6 ms (50.2%), Space: 10.4 MB (26.47%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -2,26 +2,29 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         std::unordered_map<char, int> smallCount;
+
         for(const char c : s1){
             smallCount[c]++;
         }
-        
 
-        std::unordered_map<char, int> largeCount;
-
-        int l = 0; 
+        int l = 0;
         int r = 0;
+
+
+        std::unordered_map<char, int> bigCount;
         while(r < s2.size()){
-            largeCount[s2[r]]++;
-            while(largeCount[s2[r]] > smallCount[s2[r]]){
-                largeCount[s2[l]]--;
-                l++;
-            }
+            bigCount[s2[r]]++;
             
+            while(bigCount[s2[r]] > smallCount[s2[r]]){
+                bigCount[s2[l]]--;
+                l++;
+            }    
             if(r - l + 1 == s1.size()){
                 return true;
             }
             r++;
+
+
         }
         return false;
        
