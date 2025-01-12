b27956e1faede16c8203a1e6f9359bd0b7cc8909
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-01-11 23:22:25 -0800

    Time: 10 ms (39.01%), Space: 11 MB (38.51%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-       
         std::unordered_map<char, int> counts;
+        int longest = INT_MIN;
 
         int l = 0;
         int r = 0;
-        int maxLen = 0;
         int maxChar = 0;
         while(r < s.size()){
             counts[s[r]]++;
             maxChar = max(maxChar, counts[s[r]]);
-            if(maxChar + k < r - l + 1){
+            while(maxChar + k < r - l + 1){
                 counts[s[l]]--;
                 l++;
-            }
-            maxLen = max(maxLen, r - l + 1);
+            } 
             r++;
         }
-
-        return maxLen;
+        return maxChar + k <= s.size() ? maxChar + k : s.size();
     }
 };
