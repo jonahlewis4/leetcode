bdb7e53030eb80107914d9599398568467bc833e
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-01-15 00:42:55 -0800

    Time: 3 ms (34.63%), Space: 94.9 MB (85.11%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if(nums1.size() > nums2.size()){
+        if(nums2.size() > nums1.size()){
             std::swap(nums1, nums2);
         }
-
-        int halfCount = (nums1.size() + nums2.size()) / 2;
+        int halfLen = (nums1.size() + nums2.size()) / 2;
 
         int l = 0;
-        int r = nums1.size();
-
-        while (l <= r)  {
+        int r = nums2.size();
+        while(l <= r){
             int m = (l + r) / 2;
-            int m2 = halfCount - m;
+            int l2 = m - 1 >= 0 ? nums2[m - 1] : INT_MIN;
+            int r2 = m < nums2.size() ? nums2[m] : INT_MAX;
+            int m1 = halfLen - m;
+            int l1 = m1 - 1 >= 0 ? nums1[m1 - 1] : INT_MIN;
+            int r1 = m1 < nums1.size() ? nums1[m1] : INT_MAX;
 
-            int l1 = m > 0 ? nums1[m - 1] : INT_MIN;
-            int l2 = m2 > 0 ? nums2[m2 - 1] : INT_MIN;
-            int r1 = m < nums1.size() ? nums1[m] : INT_MAX;
-            int r2 = m2 < nums2.size() ? nums2[m2] : INT_MAX;
             if(l1 <= r2 && l2 <= r1){
-                
+                //return ans;
                 if((nums1.size() + nums2.size()) % 2 == 0){
-                    return ( double(max(l1, l2)) + (min(r1, r2)) ) / 2;
+                    return ((double)max(l1, l2) + min(r2, r1))/2;
+                } else {
+                    return min(r1, r2);
                 }
-                return min(r1, r2);
-            }
-            if(l1 > r2){
-                r = m - 1;
             } else {
-                l = m + 1;
+                if(l2 > r1){
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
             }
+
+
         }
-        return -1;      
+        return -1;
     }
 };
