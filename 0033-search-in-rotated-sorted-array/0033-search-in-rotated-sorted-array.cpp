4ed98d0ca5affad4b9210da042b89f9b8c105b91
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-01-14 22:52:34 -0800

    Time: 0 ms (100%), Space: 15.2 MB (27.96%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,59 +1,36 @@
 class Solution {
-private:
-    int pivot;
-    vector<int> *nms;
-    int get(int i){
-        return nms->at((i + pivot )%nms->size() );
-    }
+    
 public:
     int search(vector<int>& nums, int target) {
         int l = 0;
         int r = nums.size() - 1;
-        int pivot = 0;
-        while(l <= r){
+        while( l <= r){
             int m = (l + r) / 2;
-            int mVal = nums[m];
-            int rIdx = m + 1 < nums.size() ? m + 1 : 0;
-            int rVal = nums[rIdx];
-            if(mVal == target){
+            if(nums[m] == target){
                 return m;
             }
-            if(rVal == target){
-                return rIdx;
-            }
-
-            if(mVal > nums[0] && rVal <= nums[0]){
-                pivot = rIdx;
-                break;
-            } else if (mVal > nums[0]){
-                l = m + 1;
-            } else {
-                r = m - 1;
-            }
             
-        }
-
-        l = 0;
-        r = nums.size();
-
-        this->nms = &nums;
-        this->pivot = pivot;
-
-        while(l <= r){
-            int m = (l + r) / 2;
-
-            if(get(m) == target){
-                return (m + pivot) % nums.size();
-            }
-            if(get(m) < target){
-                l = m + 1;
+            if(nums[m] < nums[l]){
+                if(target < nums[m]){
+                    r = m - 1;
+                }
+                else if(target >= nums[l]){
+                    r = m - 1;
+                }
+                else {
+                    l = m + 1;
+                }
             } else {
-                r = m - 1;
+                if(target > nums[m]){
+                    l = m + 1;
+                }
+                else if(target < nums[l]){
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
             }
         }
-
-
-
         return -1;
     }
 };
