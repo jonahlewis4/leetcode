bb8b2a8703165bced3831218a7aaa163ca19676d
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-01-15 21:19:51 -0800

    Time: 0 ms (100%), Space: 31.2 MB (85.16%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -5,12 +5,13 @@ public:
         int r = nums.size() - 1;
         while(l <= r){
             int m = (l + r) / 2;
-            if(nums[m] > target){
-                r = m - 1;
-            } else if (nums[m] < target){
-                l = m + 1;
-            } else{
+            if(nums[m] == target){
                 return m;
+            }
+            else if (nums[m] < target){
+                l = m + 1;
+            } else {
+                r = m - 1;
             }
         }
         return -1;
