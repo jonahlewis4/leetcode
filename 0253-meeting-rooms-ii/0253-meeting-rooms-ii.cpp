512de6a0e79cd53a31d71015623769bfaf491399
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-01-16 23:00:51 -0800

    Time: 2 ms (67.27%), Space: 16.8 MB (35.19%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -1,34 +1,25 @@
 class Solution {
 private:
-    int findLargestEnd(vector<vector<int>>& intervals){
-        int largestEnd = intervals[0][1];
-        for(const vector<int> & startEnd : intervals){
-            largestEnd = max(largestEnd, startEnd[1]);
-        }
-        return largestEnd;
-    }
-    void incDec(vector<int> &counts, vector<vector<int>>& intervals){
-        for(const vector<int> & startEnd : intervals){
-            counts[startEnd[0]]++;
-            counts[startEnd[1]]--;
-        }
-    }
-    int maxPrefixSum(vector<int> &counts){
-        int maxPrefix = counts[0];
-        for(int i = 1; i < counts.size(); i++){
-            counts[i] += counts[i - 1];
-            maxPrefix = max(maxPrefix, counts[i]);
-        }
-        return maxPrefix;
-    }
+
 
 
 
 public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
-        int largestEnd = findLargestEnd(intervals);
-        vector<int> counts(largestEnd + 1);
-        incDec(counts, intervals);
-        return maxPrefixSum(counts);
-    }
+        vector<pair<int, int>> events;
+        for(const auto & interval : intervals){
+            events.push_back({interval[0], 1});
+            events.push_back({interval[1], -1});
+        }
+
+        sort(events.begin(), events.end());
+        
+        int rooms = 0;
+        int maxRooms = 0;
+        for(const auto & event : events){
+            rooms += event.second;
+            maxRooms = max(maxRooms, rooms);
+        }
+        return maxRooms;
+    }    
 };
