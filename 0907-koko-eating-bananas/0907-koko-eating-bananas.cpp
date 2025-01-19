f51151c80a7a0c51973818480dfbd8d02ba3f888
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-01-18 22:55:44 -0800

    Time: 6 ms (80.58%), Space: 23 MB (13.29%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,32 +1,31 @@
 class Solution {
 private:
-    vector<int> *piles;
-    long long timeToEat(int eatingSpeed){
-        long long time = 0;
-        for(const int i : *(this->piles)){
-            time += ceil((double(i)) / eatingSpeed);
+    long long timeToEat(vector<int>& piles, int eatingSpeed){
+        long long sum = 0;
+        for(const auto & pile : piles){
+            sum += std::ceil(double(pile) / eatingSpeed);
         }
-        return time;
+        return sum;
     }
-
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        this->piles = &piles;
-
         int l = 1;
-        int r = *std::max_element(piles.begin(), piles.end()) ;
+        int r = *std::max_element(piles.begin(), piles.end());
 
-        int minEatingSpeed = INT_MAX;
-        while(l <= r){
-            int eatingSpeed = (l + r) / 2;
-            long long timeToEat = this->timeToEat(eatingSpeed);
-            if(timeToEat > h){
-                l = eatingSpeed + 1;
+
+        int minEating = INT_MAX;
+        while( l <= r){
+            int m = (l + r) / 2;
+            long long eatingTime = timeToEat(piles, m);
+
+            if(eatingTime <= h){
+                minEating = std::min(minEating, m);
+                r = m - 1;
             } else {
-                minEatingSpeed = min(minEatingSpeed, eatingSpeed);
-                r = eatingSpeed - 1;
-            }   
-        }   
-        return minEatingSpeed;
+                l = m + 1;
+            }
+        }
+        return minEating;
+
     }
 };
