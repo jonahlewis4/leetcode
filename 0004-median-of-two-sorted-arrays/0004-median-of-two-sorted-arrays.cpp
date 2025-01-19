5addc0811c97f5d090174454b54d0dd208cc4930
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-01-19 15:11:02 -0800

    Time: 0 ms (100%), Space: 95 MB (89.4%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,38 +1,40 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        
+
+        if(nums1.size() > nums2.size()){
+            swap(nums1, nums2);
+        }
+
+
+        
         int len = (nums1.size() + nums2.size());
         int halfLen = len / 2;
 
-        if(nums1.size() > nums2.size()){
-            std::swap(nums1, nums2);
-        }
-        //nums1 is the smaller of the two lists.
-        int l = 0;
+        int l = 0; 
         int r = nums1.size();
         while(l <= r){
-            int sM = (l + r) / 2;
-            int lM = halfLen - sM;
+            int botM = (l + r) / 2;
+            int topM = halfLen - botM;
+            int botL = botM - 1 >= 0 ? nums1[botM - 1] : INT_MIN;
+            int topL = topM - 1 >= 0 ? nums2[topM - 1] : INT_MIN;
+            
+            int botR = botM < nums1.size() ? nums1[botM] : INT_MAX;
+            int topR = topM < nums2.size() ? nums2[topM] : INT_MAX;
 
-            int sL = sM - 1 < 0 ? INT_MIN : nums1[sM - 1];
-            int lL = lM - 1 < 0 ? INT_MIN : nums2[lM - 1];
-
-            int sR = sM >= nums1.size() ? INT_MAX : nums1[sM];
-            int lR = lM >= nums2.size() ? INT_MAX : nums2[lM];
-
-            if(lL > sR){
-                l = sM + 1;
-            } else if (sL > lR){
-                r = sM - 1;
-            } else {
+            if(botL <= topR && topL <= botR){
                 if(len % 2 == 0){
-                    return (std::max(sL, lL) + std::min(sR, lR)) / 2.0;
+                    return (max(botL, topL) + min(botR, topR)) / 2.0;
                 } else {
-                    return std::min(sR, lR);
+                    return min(botR, topR);
                 }
+            } else if(botL > topR){
+                r = botM - 1;
+            } else {
+                l = botM + 1;
             }
         }
         return -1;
-        
     }
 };
