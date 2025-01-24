5799b2561a511702c6a7603c82d504032d890757
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-01-23 19:30:25 -0800

    Time: 0 ms (100%), Space: 17.7 MB (28.55%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,31 +1,15 @@
 class Solution {
 private:
-    vector<vector<int>> cache;
 public:
     int maxProfit(vector<int>& prices) {
         //maximum = 0;
-        cache = vector<vector<int>>(2, vector<int>(prices.size(), INT_MIN));
-        return dfs(prices, 0, true);
-    }
-    int dfs(vector<int> & prices, int i, bool buying){
-        if(i >= prices.size()){
-            return 0;
+        vector<vector<int>> cache;
+
+        cache = vector<vector<int>>(2, vector<int>(prices.size() + 1, 0));
+        for(int i = prices.size() - 1; i >= 0; i--){
+            cache[true][i] = max(cache[true][i + 1], cache[false][i + 1] - prices[i]);
+            cache[false][i] = max(cache[false][i + 1], cache[true][i + 1] + prices[i]);
         }
-        if(cache[buying][i] != INT_MIN){
-            return cache[buying][i];
-        }
-        if(buying){
-            int buy = dfs(prices, i + 1, false) - prices[i];
-            int skip = dfs(prices, i + 1, true);
-            int res = max(buy, skip);
-            cache[true][i] = res;
-            return max(buy, skip);
-        } else {
-            int sell = dfs(prices, i + 1, true) + prices[i];
-            int skip = dfs(prices, i + 1, false);
-            int res = max(sell, skip);
-            cache[false][i] = res;
-            return max(sell, skip);
-        }
+        return cache[true][0];
     }
 };
