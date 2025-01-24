3a47b9b2c40e957d71ff806f1428bc58cfd544c7
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-01-23 19:47:06 -0800

    Time: 0 ms (100%), Space: 16.9 MB (92.76%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -3,18 +3,11 @@ private:
 public:
     int maxProfit(vector<int>& prices) {
         int total = 0;
-        int localMin = prices[0];
-        int localMax = prices[0];
-        for(int i = 0; i < prices.size(); i++){
-            if(prices[i] < localMax){
-                total += localMax - localMin;
-                localMax = prices[i];
-                localMin = prices[i];
-            } else {
-                localMax = prices[i];
-            }
+        for(int i = 1; i < prices.size(); i++){
+            if(prices[i] > prices[i - 1]){
+                total += prices[i] - prices[i - 1];
+            } 
         }
-        total += localMax - localMin;
         return total;
     }
 };
