89547baec58680c130eb46e8e006b03c02ff1f2f
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-01-26 15:52:26 -0800

    Time: 0 ms (100%), Space: 8.1 MB (34.41%) - LeetHub

--- 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.cpp
+++ 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.cpp
@@ -2,14 +2,18 @@ class Solution {
 public:
     int kthFactor(int n, int k) {
         vector<int> factors;
-        for(int i = 1; i <= n; i++){
+        for(int i = 1; i <= sqrt(n); i++){
             if(n % i == 0){
                 factors.push_back(i);
+                if(n / i != i){
+                    factors.push_back(n / i);
+                }
             }
         }
         if(factors.size() < k){
             return -1;
         }
+        sort(factors.begin(), factors.end());
         return factors[k - 1];
     }
 };
