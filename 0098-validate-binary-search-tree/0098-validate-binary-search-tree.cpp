c0fc81f3a3f38e27c9ee30306a3e08dbb31e5d54
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-01-27 07:48:32 -0800

    Time: 0 ms (100%), Space: 21.8 MB (94.1%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -12,24 +12,15 @@
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
-        if(root == nullptr){
-            return true;
-        }
-        return dfs(root -> left, (long long)INT_MIN - 1, root->val) && dfs(root->right, root->val, (long long)INT_MAX + 1);
+        return dfs(root, (long long) INT_MIN - 1, (long long) INT_MAX + 1);
     }
     bool dfs(TreeNode* root, long long min, long long max){
         if(root == nullptr){
             return true;
         }
-        if(root -> val <= min){
+        if(root->val <= min || root->val >= max){
             return false;
         }
-        if(root -> val >= max) {
-            return false;
-        }
-        return dfs(root -> left,  min, root->val) && dfs(root->right, root->val, max);
-
+        return dfs(root->left, min, root->val) && dfs(root->right, root->val, max);
     }
-
-    
 };
