58784011dc74f72a46ea9d226b6d03007ad2783a
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-01-29 20:20:38 -0800

    Time: 11 ms (5.07%), Space: 18.3 MB (6.41%) - LeetHub

---
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
@@ -0,0 +1,32 @@
+class Solution {
+private:
+    vector<int> days;
+    vector<int> costs;
+public:
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        this->days = days;
+        unordered_map<int, unordered_map<int, int>> cache;
+
+        this->costs = costs;
+        return dfs(0, 0, cache);
+    }
+    int dfs(int ticketUntil, int dayIdx, unordered_map<int, unordered_map<int, int>>& cache){
+        if(dayIdx >= days.size()){
+            return 0;
+        }
+        if(days[dayIdx] < ticketUntil){
+            return dfs(ticketUntil, dayIdx + 1, cache);
+        }
+        if(cache.find(ticketUntil) != cache.end() && cache[ticketUntil].find(dayIdx) != cache[ticketUntil].end()){
+            return cache[ticketUntil][dayIdx];
+        }
+        int i = dayIdx;
+        int option1 = dfs(days[dayIdx] + 1, i + 1, cache) + costs[0];
+        int option2 = dfs(days[dayIdx] + 7, i + 1, cache) + costs[1];
+        int option3 = dfs(days[dayIdx] + 30, i + 1, cache) + costs[2];
+
+
+        cache[ticketUntil][dayIdx] = min({option1, option2, option3});
+        return min({option1, option2, option3});    
+    }
+};
