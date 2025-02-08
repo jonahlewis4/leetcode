a38b366dde6f542ec621badb23e5165f01e76e72
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-02-08 00:11:49 -0800

    Time: 0 ms (100%), Space: 12.7 MB (68.15%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> map;
+        unordered_map<int, int> nextGreatest;
+        std::stack<int> stack;
+
         for(int i = 0; i < nums2.size(); i++){
-            map[nums2[i]] = i;
+            while(!stack.empty() && stack.top() < nums2[i]){
+                int top = stack.top();
+                stack.pop();
+                nextGreatest[top] = nums2[i];
+            }
+            stack.push(nums2[i]);
         }
 
-        vector<int> res(nums1.size(), -1);
+        while(!stack.empty()){
+            nextGreatest[stack.top()] = -1;
+            stack.pop();
+        }
+
+        vector<int> res(nums1.size());
+
         for(int i = 0; i < nums1.size(); i++){
-            for(int j = map[nums1[i]] + 1; j < nums2.size(); j++){
-                if(nums2[j] > nums1[i]){
-                    res[i] = nums2[j];
-                    break;
-                }
-            }
+            res[i] = nextGreatest[nums1[i]];
         }
         return res;
     }
