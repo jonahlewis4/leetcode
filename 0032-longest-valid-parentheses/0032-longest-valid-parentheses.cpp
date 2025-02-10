15f5ee2980dbf2f3c46517a2c83a924c6c886dab
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-02-09 17:01:26 -0800

    Time: 2 ms (32.38%), Space: 10.2 MB (75.37%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -2,23 +2,20 @@ class Solution {
 public:
     int longestValidParentheses(string s) {
         int maxans = 0;
-        vector<int> dp(s.length());
-        for (int i = 1; i < s.length(); i++) {
-            if(s[i] == ')' && s[i - 1] == '('){
-                int preceeding = i - 2 >= 0 ? dp[i - 2] : 0;
-                dp[i] = preceeding + 2;
-            } else if(s[i] == ')' && s[i - 1] == ')'){
-                int openPreceedingIdx = i - dp[i - 1] - 1;
-                int possibleAdditionVal = openPreceedingIdx - 1 >= 0 ? dp[openPreceedingIdx - 1] : 0;
-                int preceedingVal = dp[i - 1];
-                if(openPreceedingIdx >= 0 && s[openPreceedingIdx] == '('){
-                    dp[i] = possibleAdditionVal + preceedingVal;
-                    dp[i] += 2;
+        stack<int> stack;
+        stack.push(-1);
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == '('){
+                stack.push(i);
+            } else {
+                stack.pop();
+                if(stack.empty()){
+                    stack.push(i);
+                } else {
+                    maxans = max(maxans, i - stack.top());
                 }
             }
-            maxans = max(maxans, dp[i]);
         }
-
         return maxans;
     }
 };
