c18db78937f713eb1c6cfa17b1f6d15913d77bbf
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-02-12 13:15:36 -0800

    Time: 11 ms (15.32%), Space: 14.9 MB (86.34%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,39 +17,56 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        if(head == nullptr){
-            return nullptr;
-        }
-        
-        Node* cur = head;
-        while(cur != nullptr){
-            Node* next = cur->next;
-            cur->next = new Node(cur->val);
-            cur->next->next = next;
-            cur = next;
-        }
-
-        cur = head;
-        Node* res = head->next;
-        while(cur != nullptr){
-            if(cur-> random == nullptr){
-                cur->next->random = nullptr;
-            } else {
-                cur->next->random = cur->random->next;
-            }
-            Node* next = cur->next->next;
-            cur = next;
-        }
-
-        cur = head;
-        while(cur != nullptr){
-            Node* next = cur->next->next;
-            if(next != nullptr){
-                cur->next->next = next->next;
-            }
-            cur->next = next;
-            cur = cur->next;
-        }
-        return res;
+    if(head == nullptr){
+        return nullptr;
     }
+    Node* cur = head;
+    while(cur != nullptr){
+        auto next = cur->next;
+        cur->next = new Node(cur->val);
+        cur->next->next = next;
+        cur = next;
+    }
+    //printList(head);
+    cur = head;
+
+    while(cur != nullptr){
+        if(cur->random == nullptr){
+            cur->next->random = nullptr;
+        } else {
+            cur->next->random = cur->random->next;
+        }
+        cur = cur->next->next;
+    }
+
+    //printList(head);
+
+    Node* newHead = head->next;
+    cur = head;
+
+    while(cur != nullptr){
+        Node* next = cur->next;
+        auto val = cur->val;
+
+        if(cur->next != nullptr){
+            cur->next = cur->next->next;
+        } else {
+            cur->next = nullptr;
+        }
+        cur = next;
+    }
+    return newHead;
+
+
+
+}
+
+void printList(Node* head){
+    Node* cur = head;
+    while(cur != nullptr){
+        cout<<cur->val<<" ";
+        cur = cur->next;
+    }
+    cout<<endl;
+}
 };
