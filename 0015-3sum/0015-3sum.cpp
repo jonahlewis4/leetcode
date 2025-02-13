1bcf70e703103c62251c8c03cd7e183caac28f9f
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-02-13 11:31:02 -0800

    Time: 40 ms (91.81%), Space: 30 MB (42.75%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -2,14 +2,9 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         std::sort(nums.begin(), nums.end());
-
-        int n = nums.size();
-
         vector<vector<int>> res;
-        for(int i = 0; i < n - 2; i++){
-            int l = i + 1;
-            int r = n - 1;
-            int target = 0 - nums[i];
+
+        auto twoSum = [&res, nums](int l, int r, int target, int &i) {
             while(l < r){
                 int sum = nums[l] + nums[r];
                 if(sum < target){
@@ -26,9 +21,19 @@ public:
                     }
                 }
             }
-            while(i + 1 < n - 2 && nums[i] == nums[i + 1]){
+            while(i + 1 < nums.size() - 2 && nums[i] == nums[i + 1]){
                 i++;
             }
+        };
+
+        int n = nums.size();
+
+        for(int i = 0; i < n - 2; i++){
+            int l = i + 1;
+            int r = n - 1;
+            int target = 0 - nums[i];
+            twoSum(l, r, target, i);
+
         }
         return res;
     }
