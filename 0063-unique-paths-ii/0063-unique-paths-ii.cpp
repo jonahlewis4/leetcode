508b2c6bae12f1713af57f4ce29ee7060d8b0908
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-02-23 16:18:27 -0800

    Time: 0 ms (100%), Space: 11.4 MB (89.2%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        vector<vector<long long>> dp(obstacleGrid.size(), vector<long long>(obstacleGrid[0].size(), 0));
+        vector<long long> dp(obstacleGrid[0].size(), 0);
         
-        for(int i = dp.size() - 1; i >= 0; i--){
-            for(int j = dp[i].size() - 1; j >= 0; j--){
+        for(int i = obstacleGrid.size() - 1; i >= 0; i--){
+            long long right = 0;
+            for(int j = dp.size() - 1; j >= 0; j--){
                 //get right
                 //get below
-                if(i == dp.size() - 1 && j == dp[i].size() - 1){
+                if(i == obstacleGrid.size() - 1 && j == dp.size() - 1){
                     if(obstacleGrid[i][j] == 1){
-                        dp[i][j] = 0;
+                        dp[j] = 0;
                     } else {
-                        dp[i][j] = 1;
+                        dp[j] = 1;
                     }
+                    right = dp[j];
                     continue;
                 }
                 if(obstacleGrid[i][j] == 1){
-                    dp[i][j] = 0;
+                    dp[j] = 0;
+                    right = dp[j];
                     continue;
                 }
-                int right = j + 1 < dp[i].size() ? dp[i][j + 1] : 0;
-                int left = i + 1 < dp.size() ? dp[i + 1][j] : 0;
-
-                dp[i][j] = (long long)left + right;
+                int below = i + 1 < obstacleGrid.size() ? dp[j] : 0;
+                dp[j] = (long long)below + right;
+                right = dp[j];
             }
         }
-        return dp[0][0];
+        return dp[0];
     }
 };
