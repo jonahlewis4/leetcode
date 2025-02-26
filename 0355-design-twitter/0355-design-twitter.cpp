cbe9273ba47a0c3197fb65d27f85da11396c6434
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-02-26 11:14:32 -0800

    Time: 8 ms (61.94%), Space: 55.7 MB (84.3%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.cpp
+++ 0355-design-twitter/0355-design-twitter.cpp
@@ -1,154 +1,52 @@
 class Twitter {
 private:
-    class User {
-        private:
-            int id;
-            unordered_set<int> following;
-            unordered_set<int> followers;
-            unordered_set<int> tweets;
-
-        public:
-            User(int id){
-                this->id = id;
-                this->follow(this->id);
-                this->addFollower(this->id);
-            }
-            void follow(int followeeId){
-                following.insert(followeeId);
-            }
-            void unFollow(int followeeId){
-                following.erase(followeeId);
-            }
-
-            void addFollower(int followerId){
-                followers.insert(followerId);
-            }
-            void removeFollower(int followerId){
-                followers.erase(followerId);
-            }
-            void addTweet(int tweetId){
-                tweets.insert(tweetId);
-            }
-            const bool isFollowing(int followeeId) const{
-                if(following.find(followeeId) == following.end()){
-                    return false;
-                }
-                return true;
-            }
-            const int UserId() const{
-                return this->id;
-            }
-            const unordered_set<int>& Following() const {
-                return following;
-            }
-            const unordered_set<int>& Tweets() const {
-                return tweets;
-            }
-
-
-    };
-    class Tweet {
-        private:
-            int tweetId;
-            int userId;
-            int timestamp;
-        public:
-            Tweet(int tweetId, int userId, int timestamp){
-                this->tweetId = tweetId;
-                this->userId = userId;
-                this->timestamp = timestamp;
-            }
-            const int UserId() const {
-                return this->userId;
-            }
-            const int TweetId() const{
-                return this->tweetId;
-            }
-            const int Timestamp() const{
-                return this->timestamp;
-            }
-            const unique_ptr<Tweet> Copy() const{
-                return make_unique<Tweet>(tweetId, userId, timestamp);
-            }
-        
-    };
-    unordered_map<int, unique_ptr<User>> users;
-    unordered_map<int, unique_ptr<Tweet>> tweets;
-
-    void addUserIfDoesntExist(int userId){
-        if(users.find(userId) == users.end()){
-            users[userId] = make_unique<User>(userId);
-        }
-    }
-
+    unordered_map<int, vector<pair<int, int>>> userTweets;
+    unordered_map<int, unordered_set<int>> userFollowers;
     int timestamp;
-    
 public:
     Twitter() {
         this->timestamp = 0;
     }
     
     void postTweet(int userId, int tweetId) {
-        addUserIfDoesntExist(userId);
-        users[userId]->addTweet(tweetId);
+        userTweets[userId].push_back({timestamp, tweetId});
         this->timestamp++;
-        tweets[tweetId] = make_unique<Tweet>(tweetId, userId, this->timestamp);
     }
     
     vector<int> getNewsFeed(int userId) {
-        addUserIfDoesntExist(userId);
-        const auto cmp = [](const auto & a, const auto & b){
-            return a->Timestamp() > b->Timestamp();
-        };
-        priority_queue<unique_ptr<Tweet>,  vector<unique_ptr<Tweet>>, decltype(cmp)> pq;
+        userFollowers[userId].insert(userId);
 
-
-        const auto following = users[userId]->Following();
-        for(const auto &userId : following){
-            const auto &user = users[userId];
-            const auto tweetz = user->Tweets();
-            for(const auto & tweetId : tweetz){
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        for(const int & followeeId : userFollowers[userId]){
+            for(const auto & p : userTweets[followeeId]){
+                const int timestamp = p.first;
+                const int tweetId = p.second;
                 if(pq.size() < 10){
-                    pq.push(make_unique<Tweet>(tweetId, userId, this->tweets[tweetId]->Timestamp()));
-                } else if (this->tweets[tweetId]->Timestamp() > pq.top()->Timestamp()){
-                    pq.push(make_unique<Tweet>(tweetId, userId, this->tweets[tweetId]->Timestamp()));
+                    pq.push({timestamp, tweetId});
+
+                } else if (timestamp > pq.top().first){
                     pq.pop();
+                    pq.push({timestamp, tweetId});
                 }
             }
         }
 
         vector<int> ans(pq.size());
         for(int i = ans.size() - 1; i >= 0; i--){
-            ans[i] = pq.top()->TweetId();
+            ans[i] = pq.top().second;
             pq.pop();
         }
-
         return ans;
+
     }
     
     void follow(int followerId, int followeeId) {
-        addUserIfDoesntExist(followerId);
-        addUserIfDoesntExist(followeeId);
-        users[followerId]->follow(followeeId);
-        users[followeeId]->addFollower(followerId);
-
+        userFollowers[followerId].insert(followeeId);
     }
     
     void unfollow(int followerId, int followeeId) {
-        addUserIfDoesntExist(followerId);
-        addUserIfDoesntExist(followeeId);
-        users[followerId]->unFollow(followeeId);
-        users[followeeId]->removeFollower(followerId);
-    }
-
-    void printUsers() const{
-        cout<<"___________________________________"<<endl;
-        for(const auto & p : users){
-            const auto &user = p.second;
-            cout<<"User id: "<<user->UserId()<<endl;
-        }
-    }
-
+        userFollowers[followerId].erase(followeeId);
+    }   
 };
 
 /**
