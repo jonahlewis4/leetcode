97508b2131a90638306c122a9d28c63b92d5d397
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-02-27 17:11:53 -0800

    Time: 2 ms (42.74%), Space: 9.5 MB (99.94%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> res;
-        vector<int> cur = {};
-        dfs(nums, res, cur);
-        return res;
+        vector<vector<int>> subsets;
+        unordered_map<int, bool> available;
+        for(auto itr = nums.begin(); itr != nums.end(); itr++){
+            available[*itr] = true;
+        }
+        findSubsets({}, subsets, available);
+        subsets.push_back({});
+        return subsets;
     }
-    void dfs(const vector<int> & nums, vector<vector<int>> &res, vector<int>& cur){
-        res.push_back(cur);
-        for(const int num : nums){
-            if(cur.empty() || num > cur.back()){
-                cur.push_back(num);
-                dfs(nums, res, cur);
-                cur.pop_back();
-            }
+    void findSubsets(vector<int> subset, vector<vector<int>> &subsets, unordered_map<int, bool> available)   
+    {
+        for(auto itr = available.begin(); itr != available.end();){
+            int num = itr->first;
+            subset.push_back(num);
+            subsets.push_back(subset);
+            itr = available.erase(itr);
+            findSubsets(subset, subsets, available);
+            subset.pop_back();
         }
     }
 };
