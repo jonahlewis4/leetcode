ff2d1992a066b92cc23b2fa670c33a1edb6762cd
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-03-01 12:26:54 -0800

    Time: 3 ms (42.14%), Space: 14 MB (61.59%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -2,50 +2,35 @@ class Solution {
 private:
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        unordered_map<int, int> freq;
         vector<vector<int>> res;
         vector<int> cand;
         sort(candidates.begin(), candidates.end());
-        for(const int i : candidates){
-            freq[i]++;
-        }
+        
 
-        const auto candidatesNoDup = removeDupFromSorted(candidates);
-        calc(0, cand, res, candidatesNoDup, target, freq);
+        calc(0, cand, res, candidates, target);
         return res;
     }
-    vector<int> removeDupFromSorted(const vector<int> &list){
-        vector<int> ans;
-        for(int i = 0; i < list.size(); i++){
-            if(i > 0 && list[i - 1] == list[i]){
-                continue;
-            }
-            ans.push_back(list[i]);
-        }
-        return ans;
-    }
-    void calc(const int i, vector<int>& cand, vector<vector<int>>& res, const vector<int>& candidates, const int target, unordered_map<int, int>& freq){
+    
+    void calc(int i, vector<int>& cand, vector<vector<int>>& res, const vector<int>& candidates, const int target){
         if(target == 0){
             res.push_back(cand);
+            return;
         } else if (target < 0){
             return;
         }
-        if(i > candidates.size()){
+        if(i >= candidates.size()){
             return;
         }
         
+        cand.push_back(candidates[i]);
+        calc(i + 1, cand, res, candidates, target - candidates[i]);
+        cand.pop_back();
 
-        for(int j = i; j < candidates.size(); j++){
-            int newTarget = target;
-            for(int k = 0; k < freq[candidates[j]]; k++){
-                cand.push_back(candidates[j]);
-                newTarget -= candidates[j];
-                calc(j + 1, cand, res, candidates, newTarget, freq);
-            }
-            for(int k = 0; k < freq[candidates[j]]; k++){
-                cand.pop_back();
-            }
+        i++;
+        while(i < candidates.size() && candidates[i] == candidates[i - 1]){
+            i++;
         }
+        calc(i, cand, res, candidates, target);
     }   
     
     
