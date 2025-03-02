ed1bf8bccf8dda7ae8ee01cefec62ef6d4322c0f
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-03-02 11:47:12 -0800

    Time: 4 ms (14.52%), Space: 10.8 MB (39.18%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -3,30 +3,29 @@ public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> res;
         vector<int> cur;
-        calc(res, cur, nums);
+        unordered_set<int> notUsed;
+        for(const int num : nums){
+            notUsed.insert(num);
+        }
+        calc(res, cur, notUsed, nums);
         return res;
 
         
     }
-    void calc(vector<vector<int>>& res, vector<int>& cur, const vector<int>& nums){
-        
+    void calc(vector<vector<int>>& res, vector<int>& cur, unordered_set<int>& notUsed, const vector<int>& nums){
+        vector<int> avail;
         for(const int i : nums){
-            bool alreadyInCur = false;
-            for(const int j : cur){
-                if(i == j){
-                    alreadyInCur = true;
-                    break;
-                }
-            }
-            if(alreadyInCur){
+            if(notUsed.find(i) == notUsed.end()){
                 continue;
             }
             cur.push_back(i);
             if(cur.size() == nums.size()){
                 res.push_back(cur);
             }
-            calc(res, cur, nums);
+            notUsed.erase(i);
+            calc(res, cur, notUsed, nums);
             cur.pop_back();
+            notUsed.insert(i);
         }
     }
 };
