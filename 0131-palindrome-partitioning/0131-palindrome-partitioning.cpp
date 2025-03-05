5c4b5ec0ab62f825355c4381ad1d15c5254136ab
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-03-04 18:50:43 -0800

    Time: 27 ms (95.83%), Space: 57.7 MB (53.98%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> res;
+        vector<string> cur;
+        calc(cur, res, s, 0, 0);  
+        return res;  
+    }
+
+    void calc(vector<string>& cur, vector<vector<string>>& res, const string& s, int l, int r){
+        if(isPal(s, l, r)){
+            cur.push_back(s.substr(l, r - l + 1));
+            if(r == s.size() - 1){
+                res.push_back(cur);
+            } else {
+                calc(cur, res, s, l + 1, r + 1);
+            }
+            cur.pop_back();
+        } 
+        r++;
+        while(r < s.size()){
+            if (isPal(s, l, r)){
+                cur.push_back(s.substr(l, r - l + 1));
+                if(r == s.size() - 1){
+                    res.push_back(cur);
+                } else {
+                    calc(cur, res, s, r + 1, r + 1);
+                }
+                cur.pop_back();
+            }
+            r++;
+        }
+        
+    }
+
+
+
+    bool isPal(const string&s, int l, int r){
+        int ogL = l;
+        int ogR = r;
+        while(l <= r){
+            if(s[l] != s[r]){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+};
