6019bd92970bd7025694939eb2cddd3ac8959278
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-03-06 14:40:08 -0800

    Time: 145 ms (10.35%), Space: 47.3 MB (13.55%) - LeetHub

--- 0912-random-pick-with-weight/0912-random-pick-with-weight.cpp
+++ 0912-random-pick-with-weight/0912-random-pick-with-weight.cpp
@@ -2,11 +2,6 @@ class Solution {
 private:
 vector<int> sums;
 int sum;
-int total0;
-int total1;
-int total2;
-int total3;
-int totalPicks;
 public:
     Solution(vector<int>& w) {
         sums = w;
@@ -21,29 +16,13 @@ public:
         }
         //cout<<"sum: "<<sum<<endl;
         this->sum = sum;
-        total0 = 0;
-        total1 = 0;
-        total2 = 0;
-        total3 = 0;
-        totalPicks = 0;
     }
     
     int pickIndex() {
-        totalPicks++;
         int randomNumber = std::rand() % (sum);
         //cout<<"Random number: "<<randomNumber<<endl;
         for(int i = 0; i < sums.size(); i++){
             if(sums[i] > randomNumber){
-                //cout<<"index "<<i<<" with value "<<sums[i]<<" is greater or equal to "<<randomNumber<<endl;
-                if(i == 0){
-                    total0++;
-                } else if(i == 1){
-                    total1++;
-                } else if (i == 2){
-                    total2++;
-                } else if (i == 3){
-                    total3++;
-                }
                 return i;
             }
         }
