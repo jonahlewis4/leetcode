30f9d3f19c8c125be8ea2a75d3dc34983eaca491
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-03-06 15:03:24 -0800

    Time: 11 ms (85.84%), Space: 47.3 MB (24.93%) - LeetHub

--- 0912-random-pick-with-weight/0912-random-pick-with-weight.cpp
+++ 0912-random-pick-with-weight/0912-random-pick-with-weight.cpp
@@ -1,26 +1,29 @@
 class Solution {
 private:
 vector<int> sums;
+int sum;
 public:
     Solution(vector<int>& w) {
-        // sums.push_back(w[0]);
-        // for(int i = 1; i < w.size(); i++){
-        //     sums.push_back(w[i]);
-        //     sums[i] += sums[i - 1];
-        //     //cout<<sums[i]<<endl;
-        // }
-        for(auto n : w){
-            sums.push_back(n + (sums.empty() ? 0 : sums.back()));
+        sums = w;
+        int sum = 0;
+        sums[0] = w[0];
+        sum += w[0];
+        for(int i = 1; i < w.size(); i++){
+            sums.push_back(w[i]);
+            sums[i] += sums[i - 1];
+            //cout<<sums[i]<<endl;
+            sum += w[i];
         }
+        //cout<<"sum: "<<sum<<endl;
+        this->sum = sum;
     }
     
     int pickIndex() {
-        float random = (float) rand() / RAND_MAX; 
-        int randomNumber = random * sums.back();
+        int randomNumber = std::rand() % (sum);
         //cout<<"Random number: "<<randomNumber<<endl;
         //find entry where previous is less or equal current is greater. Return index of the greater one.
 
-        return lower_bound(begin(sums), end(sums), randomNumber + 1) - begin(sums);
+        return lower_bound(sums.begin(), sums.end(), randomNumber + 1) - sums.begin();
     }
 };
 
