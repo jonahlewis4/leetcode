22d1a1d3a930a16ed7d5f1d5596c62eaf0ddf290
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-03-06 15:32:30 -0800

    Time: 27 ms (22.29%), Space: 47.1 MB (48.89%) - LeetHub

--- 0912-random-pick-with-weight/0912-random-pick-with-weight.cpp
+++ 0912-random-pick-with-weight/0912-random-pick-with-weight.cpp
@@ -5,7 +5,7 @@ int sum;
 public:
     Solution(vector<int>& w) {
         int sum = 0;
-        sums.push_back(w[0]);
+        sums.push_back(w[0] - 1);
         sum += w[0];
         for(int i = 1; i < w.size(); i++){
             sums.push_back(w[i]);
@@ -27,7 +27,7 @@ public:
         int r = sums.size() - 1;
         while(l <= r){
             int m = (l + r) / 2;
-            if(sums[m] <= randomNumber){
+            if(sums[m] < randomNumber){
                 l = m + 1;
             } else {
                 ans = m;
