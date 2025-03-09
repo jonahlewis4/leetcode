cb3d9a2b896a36197597ed49d21b47a864c420a5
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-03-08 18:40:30 -0800

    Time: 0 ms (100%), Space: 10.8 MB (46.27%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,27 +1,32 @@
 class Solution {
+private:
+    vector<vector<int>> res;
+    vector<int> nums;
+    vector<int> cur;
 public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        std::sort(nums.begin(), nums.end());
-        vector<vector<int>> res;
-        vector<int> cur;
-        calc(0, cur, res, nums);
+    vector<vector<int>> subsetsWithDup(const vector<int>& nums) {
+        res = {};
+        this->nums = nums;
+        cur = {};
+        std::sort(this->nums.begin(), this->nums.end());
+        calc(0);
         return res;
     }
-    void calc(int i, vector<int>& cur, vector<vector<int>>& res, const vector<int>& nums){
+    void calc(int i){
         if(i >= nums.size()){
             res.push_back(cur);
             return;
         }
-
+        //we either use the current, and move on to the next, or we forbid the use of that number from now on.
         cur.push_back(nums[i]);
-        calc(i + 1, cur, res, nums);
+        calc(i + 1);
         cur.pop_back();
 
-        int skip = nums[i];
-        while(i < nums.size() && nums[i] == skip){
+        //now it's forbidden, so skip while it matches.
+        int match = nums[i];
+        while(i < nums.size() && nums[i] == match){
             i++;
         }
-        calc(i, cur, res, nums);
-
+        calc(i);
     }
 };
