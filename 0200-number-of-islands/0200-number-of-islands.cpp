152329a53a7309c3d3a01788c70324933b4e5185
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-03-08 18:28:52 -0800

    Time: 30 ms (38.53%), Space: 16.7 MB (61.13%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -7,24 +7,25 @@ public:
         int sum = 0;
         for(int i = 0; i < grid.size(); i++){
             for(int j = 0; j < grid[i].size(); j++){
-                sum += dfs(i, j);
+                if(this->grid[i][j] == '1'){
+                    dfs(i, j);
+                    sum++;
+                }   
             }
         }
         return sum;
     }
-    int dfs(int i, int j){
+    void dfs(int i, int j){
         if(i < 0 || i >= grid.size() || j < 0 || j >= grid[i].size()){
-            return 0;
+            return;
         }
         if(grid[i][j] == '1'){
-            grid[i][j] = 0;
+            grid[i][j] = '0';
             dfs(i + 1, j);
             dfs(i, j + 1);
             dfs(i - 1, j);
             dfs(i, j - 1);
-            return 1;
         }
-
-        return 0;
+        return;
     }
 };
