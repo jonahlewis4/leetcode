b85893da71ea655c1cb46287bb31de69c578edb7
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-03-08 19:00:10 -0800

    Time: 507 ms (46.46%), Space: 12 MB (13.43%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,63 +1,47 @@
 class Solution {
 private: 
-vector<vector<bool>> visited;
-bool exists;
-string cur;
-vector<vector<char>> board;
-string word;
-int idx;
-
+    vector<vector<char>> board;
+    string word;
+    bool exists;
+    vector<vector<bool>> visited;
 public:
-    bool exist(vector<vector<char>>& board, string word) {
-        this->exists = false;
-        visited = vector<vector<bool>>(board.size(), vector<bool>(board[0].size()));
-        cur = "";
+    bool exist(const vector<vector<char>>& board, const string word) {
         this->board = board;
         this->word = word;
-        idx = 0;
-        for(int i = 0; i < board.size(); i++){
-            for(int j = 0; j < board[i].size(); j++){
-                calc(i, j);
-                if(exists){
-                    return true;
-                }
+        visited = vector<vector<bool>>(board.size(), vector<bool>(board[0].size(), false));
+        exists = false;
+        for(int row = 0; row < board.size(); row++){
+            for(int col = 0; col < board[row].size(); col++){
+                calc(0, row, col);
             }
         }
-        
         return exists;
     }
-    void calc(const int i, const int j){
-        
-        if(cur.size() > word.size() || exists == true || i >= board.size() || i < 0 || j >= board[i].size() || j < 0){
+    void calc(int i, int row, int col){
+        if(exists){
             return;
         }
-        if(visited[i][j]){
-            return;
-        }
-        cur += board[i][j];
-        //cout<<"idx is: "<<idx<<endl;
-        //cout<<"cur: "<<cur<<endl;
-        if(cur[idx] != word[idx]){
-            cur.pop_back();
-            return;
-        }
-        //cout<<"they are the same "<<endl;
-        idx++;
-        if(cur == word){
+        if(i == word.size()){
+            cout<<i<<endl;
             exists = true;
-            cur.pop_back();
-            idx--;
             return;
         }
-        //cout<<cur<<endl;
-        visited[i][j] = true;
-        calc(i + 1, j);
-        calc(i - 1, j);
-        calc(i, j + 1);
-        calc(i, j - 1);
-        visited[i][j] = false;
-        cur.pop_back();
-        idx--;
+        if(row < 0 || col < 0 || row >= board.size() || col >= board[row].size()){
+            return;
+        }
+        
+        if(word[i] != board[row][col]){
+            return;
+        }
+        if(visited[row][col]){
+            return;
+        }
+        visited[row][col] = true;
+        calc(i + 1, row - 1, col);
+        calc(i + 1, row, col - 1);
+        calc(i + 1, row + 1, col);
+        calc(i + 1, row, col + 1);
+        visited[row][col] = false;
 
     }
 };
