100a916b71abda04ae50bde72e5a9dcfd3014f33
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-03-08 21:22:55 -0800

    Time: 19 ms (8.05%), Space: 19.8 MB (5.4%) - LeetHub

---
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -0,0 +1,31 @@
+class Solution {
+private:
+    const int maxTime = 1440;
+    int diff(const string& a, const string& b) const {
+        const int time1 = numerize(a);
+        const int time2 = numerize(b);
+
+        const int regularDiff = abs(time1 - time2);
+        const int rolloverDiff = abs(time2 + maxTime - time1);
+        const int rolloverDiff2 = abs(time1 + maxTime - time2);
+
+        return min({regularDiff, rolloverDiff, rolloverDiff2});
+    }
+    int numerize(const string& a) const {
+        int hours = stoi(a.substr(0, 2));
+        int minutes = stoi(a.substr(3, 2));
+        return hours * 60 + minutes;
+    }
+public:
+    int findMinDifference(vector<string>& timePoints) {
+        sort(timePoints.begin(), timePoints.end());
+
+        int minDiff = INT_MAX;
+        int n = timePoints.size();
+        timePoints.push_back(timePoints[0]);
+        for(int i = 0; i < n; i++){
+            minDiff = min(minDiff, diff(timePoints[i], timePoints[i + 1]));
+        }
+        return minDiff;
+    }
+};
