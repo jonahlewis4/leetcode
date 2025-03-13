a9114cea7bbf5dca567ae2791c378693e29e4933
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-03-12 18:18:24 -0700

    Time: 463 ms (48.1%), Space: 11.6 MB (35.87%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -2,46 +2,44 @@ class Solution {
 private: 
     vector<vector<char>> board;
     string word;
-    bool exists;
+    bool res;
     vector<vector<bool>> visited;
 public:
-    bool exist(const vector<vector<char>>& board, const string word) {
+    bool exist(vector<vector<char>>& board, string word) {
         this->board = board;
         this->word = word;
+        this->res = false; 
         visited = vector<vector<bool>>(board.size(), vector<bool>(board[0].size(), false));
-        exists = false;
         for(int row = 0; row < board.size(); row++){
             for(int col = 0; col < board[row].size(); col++){
                 calc(0, row, col);
             }
         }
-        return exists;
+        return res;
     }
-    void calc(int i, int row, int col){
-        if(exists){
-            return;
-        }
+    void calc(const int i, const int row, const int col) {
         if(i == word.size()){
-            cout<<i<<endl;
-            exists = true;
-            return;
-        }
-        if(row < 0 || col < 0 || row >= board.size() || col >= board[row].size()){
+            res = true;
             return;
         }
         
-        if(word[i] != board[row][col]){
+        if(row < 0 or row >= board.size() or col < 0 or col >= board[row].size()){
             return;
         }
         if(visited[row][col]){
             return;
         }
+        if(word[i] != board[row][col]){
+            return;
+        }
         visited[row][col] = true;
-        calc(i + 1, row - 1, col);
-        calc(i + 1, row, col - 1);
         calc(i + 1, row + 1, col);
+        calc(i + 1, row - 1, col);
         calc(i + 1, row, col + 1);
+        calc(i + 1, row, col - 1);
         visited[row][col] = false;
+        
+
 
     }
 };
