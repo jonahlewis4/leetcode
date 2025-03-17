b02afea0ce8a1293ac7c6ac3eb6644d82a2a72e9
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-03-16 23:07:27 -0700

    Time: 0 ms (100%), Space: 7.8 MB (79.01%) - LeetHub

--- 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
@@ -8,45 +8,22 @@ public:
         this->k = k;
         this->n = n;
         this->i = 1;
-        for(int j = 1; j <= 9; j++){
-            if(i == k){
-                return j;
-            }
-            int numElements = numElem(j, j + 1);
-            
-            if(numElements + i == k){
-                return j + 1;
-            } else if(numElements + i > k){
-                i++;
-                return lexi(j);
+        
+        int res = 1;
+        while(i < k){
+            int dist = numElem(res, res + 1);
+            if(i + dist <= k){
+                res++;
+                i += dist;
             } else {
-                i += numElements;
-                continue;
+                res *= 10;
+                i++;
             }
         }
-        return -1;
+        return res;
         
     }
 
-    int lexi(const int curNum){
-        if(i == k){
-            return curNum * 10;
-        }
-        int curNumTen = curNum * 10;
-        for(int j = 0; j <= 9; j++){
-            int numElements = numElem(curNumTen + j, curNumTen + j + 1);
-            if(i + numElements == k){
-                return curNum * 10 + j + 1;
-            } else if(i + numElements > k){
-                i++;
-                return lexi(curNumTen + j);
-            } else {
-                i += numElements;
-                continue;
-            }
-        }
-        return -1;
-    }
     int numElem(long l, long r) const{
         int sum = 0;
         while(l <= n){
