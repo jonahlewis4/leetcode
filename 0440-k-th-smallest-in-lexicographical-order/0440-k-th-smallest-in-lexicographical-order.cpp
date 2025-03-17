a0d4c1f7141220a5327c175a4481a9a789be8d90
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-03-16 23:04:46 -0700

    Time: 3 ms (0.88%), Space: 7.9 MB (43.21%) - LeetHub

--- 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
@@ -1,31 +1,61 @@
 class Solution {
+private: 
+    int k;
+    int n;
+    int i;
 public:
     int findKthNumber(int n, int k) {
-        int curr = 1;
-        k--;
-
-        while (k > 0) {
-            int step = countSteps(n, curr, curr + 1);
-            if (step <= k) {
-                curr++;
-                k -= step;
+        this->k = k;
+        this->n = n;
+        this->i = 1;
+        for(int j = 1; j <= 9; j++){
+            if(i == k){
+                return j;
+            }
+            int numElements = numElem(j, j + 1);
+            
+            if(numElements + i == k){
+                return j + 1;
+            } else if(numElements + i > k){
+                i++;
+                return lexi(j);
             } else {
-                curr *= 10;
-                k--;
+                i += numElements;
+                continue;
             }
         }
-
-        return curr;
+        return -1;
+        
     }
 
-private:
-    int countSteps(int n, long prefix1, long prefix2) {
-        int steps = 0;
-        while (prefix1 <= n) {
-            steps += min((long)(n + 1), prefix2) - prefix1;
-            prefix1 *= 10;
-            prefix2 *= 10;
+    int lexi(const int curNum){
+        if(i == k){
+            return curNum * 10;
         }
-        return steps;
+        int curNumTen = curNum * 10;
+        for(int j = 0; j <= 9; j++){
+            int numElements = numElem(curNumTen + j, curNumTen + j + 1);
+            if(i + numElements == k){
+                return curNum * 10 + j + 1;
+            } else if(i + numElements > k){
+                i++;
+                return lexi(curNumTen + j);
+            } else {
+                i += numElements;
+                continue;
+            }
+        }
+        return -1;
     }
+    int numElem(long l, long r) const{
+        int sum = 0;
+        while(l <= n){
+            sum += min(r, (long)n + 1) - l;
+            l *= 10;
+            r *= 10;
+        }
+        return sum;
+    }
+
+    
 };
