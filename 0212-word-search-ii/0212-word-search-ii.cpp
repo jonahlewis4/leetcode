7ecb9bfa7811cd5c51b88f44bdabeda092fb9e60
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-03-18 22:39:34 -0700

    Time: 608 ms (53.31%), Space: 19.4 MB (62.8%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.cpp
+++ 0212-word-search-ii/0212-word-search-ii.cpp
@@ -1,105 +1,86 @@
 class Solution {
-    class Trie{
-        bool isEnd = false;
-        int childCount = 0;
-        array<unique_ptr<Trie>, 26> children;
-        
-        public:
-        void insert(const string& word){
-            Trie* cur = this;
-            for(int i = 0; i < word.size(); i++){
-                int idx = word[i] - 'a';
-                if(cur->children[idx] == nullptr){
-                    cur->children[idx] = make_unique<Trie>();
-                    cur->childCount++;
-                }
-                cur = cur->children[idx].get();
-            }
-            cur->isEnd = true;
-        }
-        bool find(const string& word){
-            Trie* cur = this;
-            for(int i = 0; i < word.size(); i++){
-                int idx = word[i] - 'a';
-                if(cur->children[idx] == nullptr){
-                    return false;
-                }
-                cur = cur->children[idx].get();
-            }
-            return cur->isEnd;
-        }
-        Trie* get(const char c) const {
-            return children[c - 'a'].get();
-        }
-        bool isWord() const {
-            return isEnd;
-        }
-        void unWord() {
-            isEnd = false;
-        }
-        void unset(const char c) {
-            children[c - 'a'] = nullptr;
-            childCount--;
-        }
-        int count() const {
-            return childCount;
-        }
-    };
 
-    Trie trie;
-    vector<vector<char>>* board;
+
+
+private:
+    class Trie {
+        private:
+            bool _isWord = false;
+            array<unique_ptr<Trie>, 26> children;
+            int _count = 0;
+    
+        public:
+            void insert(const string& s){
+                auto cur = this;
+                for(int i = 0; i < s.size(); i++){
+                    const char c = s[i];
+                    if(cur->get(c) == nullptr){
+                        cur->children[c - 'a'] = make_unique<Trie>();
+                        _count++;
+                    }
+                    cur = cur->get(c);
+                }
+                cur->_isWord = true;
+            }
+            Trie* get(const char c) const {
+                return children[c - 'a'].get();
+            }
+            bool isWord() const {
+                return _isWord;
+            }
+            void unWord() {
+                _isWord = false;
+            }
+    };
     vector<string> res;
+    vector<vector<char>> board;
+    Trie trie = Trie();
     string curWord = "";
-    vector<vector<bool>> visited;
-    unordered_set<string> inRes;
 public:
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-        for(const string& word : words){
-            trie.insert(word);
+        for(const string & s : words){
+            trie.insert(s);
         }
-        this->board = &board;
-        visited = vector<vector<bool>>(board.size(), vector<bool>(board[0].size(), false));
+
+        this->board = board;
         for(int row = 0; row < board.size(); row++){
             for(int col = 0; col < board[row].size(); col++){
                 calc(&trie, row, col);
             }
         }
+
         return res;
     }
+    
     void calc(Trie* cur, int row, int col){
         if(cur == nullptr){
             return;
         }
-        if(row < 0 || row >= board->size() || col < 0 || col >= (*board)[row].size()){
+
+        if(cur->isWord()){
+            cur->unWord();
+            res.push_back(curWord);
+        }
+        if(row < 0 || col < 0 || row >= board.size() || col >= board[row].size()){
             return;
         }
-        if(visited[row][col]){
+        
+        if(board[row][col] < 'a'){
             return;
         }
-        char c = (*board)[row][col];
+
+        int c = board[row][col];
+        curWord.push_back(c);
+        board[row][col] -= 'a';
         Trie* next = cur->get(c);
-        if(!next){
-            return;
-        } else {
-            curWord.push_back(c);
-            visited[row][col] = true;
-            calc(next, row + 1, col);
-            calc(next, row - 1, col);
-            calc(next, row, col + 1);
-            calc(next, row, col - 1);
-            if(next->isWord()){
-                if(inRes.find(curWord) == inRes.end()){
-                    res.push_back(curWord);
-                    next->unWord();
-                    if(next->count() == 0){
-                        cur->unset(c);
-                    }
-                }   
-            }
-            curWord.pop_back();
-            visited[row][col] = false;
-            
-        }
+        calc(next, row + 1, col);
+        calc(next, row - 1, col);
+        calc(next, row, col + 1);
+        calc(next, row, col - 1);
+        board[row][col] += 'a';
+        curWord.pop_back();
+
+
 
     }
 };
