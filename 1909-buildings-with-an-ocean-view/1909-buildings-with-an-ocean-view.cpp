5d4ee158ce003a90b02fa17b9a283f329a7c2309
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-03-22 16:50:12 -0700

    Time: 0 ms (100%), Space: 73.1 MB (52.49%) - LeetHub

--- 1909-buildings-with-an-ocean-view/1909-buildings-with-an-ocean-view.cpp
+++ 1909-buildings-with-an-ocean-view/1909-buildings-with-an-ocean-view.cpp
@@ -2,20 +2,16 @@ class Solution {
 public:
     vector<int> findBuildings(vector<int>& heights) {
 
-        vector<int> largestAfter(heights.size());
-        largestAfter[heights.size() - 1] = INT_MIN;
         int largest = INT_MIN;
+        vector<int> res;
+        res.push_back(heights.size() - 1);
         for(int i = heights.size() - 1; i>=1 ; i--){
             largest = max(largest, heights[i]);
-            largestAfter[i - 1] = largest;
-        }  
-
-        vector<int> res;
-        for(int i = 0; i < heights.size(); i++){
-            if(heights[i] > largestAfter[i]){
-                res.push_back(i);
+            if(heights[i - 1] > largest){
+                res.push_back(i - 1);
             }
-        }
+        }  
+        reverse(res.begin(), res.end());
         return res;
     }
 };
