45b334f1d0517221fd30afefc202d412a15cf4b1
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-03-27 16:04:05 -0700

    Time: 207 ms (13.77%), Space: 180.9 MB (6.35%) - LeetHub

---
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
@@ -0,0 +1,72 @@
+class Solution {
+private:
+    class Trie {
+        bool isEnd = false;
+        array<unique_ptr<Trie>, 27> children;
+
+        int hash(char c) const {
+            if(c == '/'){
+                return 26;
+            }
+            return c - 'a';
+        }
+
+    public:
+        void insert(const string & s){
+            Trie* cur = this;
+            for(int i = 0; i < s.size(); i++){
+                char c = s[i];
+                int idx = hash(c);
+
+                if(cur->children[idx] == nullptr){
+                    cur->children[idx] = make_unique<Trie>();
+                }
+                cur = cur->children[idx].get();
+            }
+            cur->isEnd = true;
+            
+        }
+        bool isSub(const string & s){
+            Trie* cur = this;
+            for(int i = 0; i < s.size(); i++){
+                char c = s[i];
+                if(c == '/' && cur->isEnd){
+                    return true;
+                }
+                int idx = hash(c);
+                cur = cur->children[idx].get();
+            }
+            return false;
+        }
+
+    };
+
+public:
+    vector<string> removeSubfolders(vector<string>& folder) {
+        Trie trie;
+        for(auto & str : folder){
+            trie.insert(str);
+        }
+        vector<string> res;
+        for(const auto & str : folder){
+            if(trie.isSub(str)){
+                continue;
+            }
+            res.push_back(str);
+        }
+        return res;
+    }
+    vector<string> split(const string& s, char delilm) const {
+        string cur = "";
+        vector<string> res;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == '/'){
+                res.push_back(cur);
+                cur = "";
+            } else {
+                cur = "";
+            }
+        }
+        return res;
+    }
+};
