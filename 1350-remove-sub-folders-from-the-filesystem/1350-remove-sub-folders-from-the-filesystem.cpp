a32da909363ae1b6e052face4f05ee1f5794e317
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-03-27 16:16:41 -0700

    Time: 92 ms (38.54%), Space: 118.4 MB (11.12%) - LeetHub

--- 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
@@ -1,26 +1,61 @@
 class Solution {
+private:
+    class Trie {
+        bool isEnd = false;
+        array<unique_ptr<Trie>, 27> children;
+
+        int hash(char c) const {
+            if(c == '/'){
+                return 26;
+            }
+            return c - 'a';
+        }
+
+    public:
+        void insert(const string & s){
+            Trie* cur = this;
+            for(int i = 0; i < s.size(); i++){
+                char c = s[i];
+                int idx = hash(c);
+                if(c == '/' && cur -> isEnd){
+                    return;
+                }
+                if(cur->children[idx] == nullptr){
+                    cur->children[idx] = make_unique<Trie>();
+                }
+                cur = cur->children[idx].get();
+            }
+            cur->isEnd = true;
+            
+        }
+        bool isSub(const string & s){
+            Trie* cur = this;
+            for(int i = 0; i < s.size(); i++){
+                char c = s[i];
+                if(c == '/' && cur->isEnd){
+                    return true;
+                }
+                int idx = hash(c);
+                cur = cur->children[idx].get();
+            }
+            return false;
+        }
+
+    };
+
 public:
     vector<string> removeSubfolders(vector<string>& folder) {
-        sort(folder.begin(), folder.end());
-        vector<string> res;
-        unordered_set<string> seen;
-        
-        for (const string& f : folder) {
-            // Check if a valid parent folder exists
-            size_t pos = 0;
-            bool isSub = false;
-            while ((pos = f.find('/', pos + 1)) != string::npos) {
-                if (seen.count(f.substr(0, pos))) {
-                    isSub = true;
-                    break;
-                }
-            }
-            if (!isSub) {
-                res.push_back(f);
-                seen.insert(f);
-            }
+        Trie trie;
+        for(auto & str : folder){
+            trie.insert(str);
         }
-        
+        vector<string> res;
+        for(const auto & str : folder){
+            if(trie.isSub(str)){
+                continue;
+            }
+            res.push_back(str);
+        }
         return res;
     }
 };
