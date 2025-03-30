16262a4caba76399958a017815833b6d152c9cc9
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-03-29 17:35:50 -0700

    Time: 0 ms (100%), Space: 13.3 MB (44.79%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,70 @@
+class Solution {
+    class DJU{
+        struct item {
+            int root;
+            int size = 1;
+
+            item(int _root){
+                root = _root;
+            }
+        };
+
+        vector<item> items;
+
+    public:
+        DJU(int n){
+            items.resize(n, item(-1));
+            for(int i = 0; i < n; i++){
+                items[i].root = i;
+            }
+        }
+
+        int Find(int i) {
+            if(items[i].root == i){
+                return i;
+            }
+            items[i].root = Find(items[i].root);
+            return items[i].root;
+        }
+
+        bool Union(int i1, int i2) {
+            item &root1 = items[Find(i1)];
+            item &root2 = items[Find(i2)];
+
+            if(root1.root == root2.root){
+                return true;
+            }
+
+            if(root1.size > root2.size){
+                root2.size += root1.size;
+                root1.root = root2.root;
+            } else {
+                root1.size += root2.size;
+                root2.root = root1.root;
+            }
+
+            return false;
+        }
+
+    };
+
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        
+        int n = 0;
+        unordered_set<int> present;
+        for(const auto & edge : edges){
+            present.insert(edge[0]);
+            present.insert(edge[1]);
+        }
+
+        DJU dju(present.size() + 1);
+        for(const auto & edge : edges){
+            if(dju.Union(edge[0], edge[1])){
+                return edge;
+            }
+        }
+
+        return {};
+    }
+};
