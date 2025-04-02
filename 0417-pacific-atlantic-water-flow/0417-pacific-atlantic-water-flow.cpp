97b7fac11ba5cf8885444896eccb03ed2f4dd4aa
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-04-01 18:53:05 -0700

    Time: 6 ms (70.01%), Space: 24.7 MB (35.68%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,26 +1,27 @@
 class Solution {
-private:
-    struct Point{
-        bool pacific = false;
-        bool atlantic = false;
-        int height = 0;
-        Point(){}
-        Point(const int _height){
-            height = _height;
+private: vector<vector<bool>> visit;
+    vector<vector<int>> heights;
+public:
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        vector<vector<bool>> pacific(heights.size(), vector<bool>(heights[0].size(), false));
+        vector<vector<bool>> atlantic(heights.size(), vector<bool>(heights[0].size(), false));
+        visit = vector<vector<bool>>(heights.size(), vector<bool>(heights[0].size(), false));
+        this->heights = heights;
+
+        for(int c = 0; c < heights[0].size(); c++){
+            dfs(0, c, pacific, 0);
+            dfs(heights.size() - 1, c, atlantic, 0);
         }
 
-        bool isReachable() const {
-            return pacific && atlantic;
-        }
-
-    };
-    vector<vector<Point>> points;
-
-    vector<vector<int>> calcReachable() const {
+        for(int r = 0; r < heights.size(); r++){
+            dfs(r, 0, pacific, 0);
+            dfs(r, heights[r].size() - 1, atlantic, 0);
+        }        
+        
         vector<vector<int>> res;
-        for(int r = 0; r < points.size(); r++){
-            for(int c = 0; c < points[r].size(); c++){
-                if(points[r][c].isReachable()){
+        for(int r = 0; r < heights.size(); r++){
+            for(int c = 0; c < heights[r].size(); c++){
+                if(pacific[r][c] && atlantic[r][c]){
                     res.push_back({r,c});
                 }
             }
@@ -28,88 +29,31 @@ private:
         return res;
     }
 
-    void initPoints(const vector<vector<int>> & heights){
-        for(int r = 0; r < heights.size(); r++){
-            for(int c = 0; c < heights[r].size(); c++){
-                points[r][c] = Point(heights[r][c]);
-            }
-        }
-    }
-    void printPacific() {
-        for(int r = 0; r < points.size(); r++){
-            for(int c = 0; c < points[r].size(); c++){
-                if(points[r][c].pacific){
-                    cout<<points[r][c].height;
-                } else {
-                    cout<<" ";
-                }
-                cout<<" ";
-            }
-            cout<<endl;
-        }
-    }
-    void calcPacific() {
-        for(int r = 0; r < points.size(); r++){
-            dfsPacific(r, 0, 0);
-        }
-        for(int c = 0; c < points[0].size(); c++){
-            dfsPacific(0, c, 0);
-        }
-    }
-    void dfsPacific(int r, int c, int previous){
-       
-        if(r < 0 || c < 0 || r >= points.size() || c >= points[r].size()){
+    void dfs(int r, int c, vector<vector<bool>>& ocean, int prev){
+        if(r < 0 || c < 0 || r >= ocean.size() || c >= ocean[r].size()){
             return;
         }
-        if(points[r][c].pacific == true){
+        if(heights[r][c] < prev){
             return;
         }
-        if(points[r][c].height >= previous){
-            points[r][c].pacific = true;
-        } else {
+        if(visit[r][c]){
             return;
         }
-        dfsPacific(r + 1, c, points[r][c].height);
-        dfsPacific(r, c + 1, points[r][c].height);
-        dfsPacific(r - 1, c, points[r][c].height);
-        dfsPacific(r, c - 1, points[r][c].height);
-    }
-    void calcAtlantic() {
-        for(int r = 0; r < points.size(); r++){
-            dfsAtlantic(r, points[r].size() - 1, 0);
-        }
-        for(int c = 0; c < points[0].size(); c++){
-            dfsAtlantic(points.size() - 1, c, 0);
-        }
-    }
-    void dfsAtlantic(int r, int c, int previous){
-        if(r < 0 || c < 0 || r >= points.size() || c >= points[r].size()){
-            return;
-        }
-        if(points[r][c].atlantic == true){
-            return;
-        }
-        if(points[r][c].height >= previous){
-            points[r][c].atlantic = true;
-        } else {
-            return;
-        }
-        dfsAtlantic(r - 1, c, points[r][c].height);
-        dfsAtlantic(r, c - 1, points[r][c].height);
-        dfsAtlantic(r + 1, c, points[r][c].height);
-        dfsAtlantic(r, c + 1, points[r][c].height);
-    }
-public:
-    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        points = vector<vector<Point>>(heights.size(), vector<Point>(heights[0].size()));
-        initPoints(heights);
-        calcPacific();
-        //printPacific();
-        calcAtlantic();
-        return calcReachable();
-    }
 
+        //TODO if already set to true, exit
+        if(ocean[r][c] == true){
+            return;
+        }
 
+        ocean[r][c] = true;
 
-    
+        visit[r][c] = true;
+
+        prev = heights[r][c];
+        dfs(r - 1, c, ocean, prev);
+        dfs(r + 1 ,c, ocean, prev);
+        dfs(r, c - 1, ocean, prev);
+        dfs(r, c + 1, ocean, prev);
+        visit[r][c] = false;
+    }
 };
