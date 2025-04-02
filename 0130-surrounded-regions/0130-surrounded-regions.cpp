89cb634d5b284cb9fef7c939cd7f25735427fc11
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-04-02 08:34:30 -0700

    Time: 0 ms (100%), Space: 13.8 MB (90.84%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,40 +1,58 @@
 class Solution {
-private:
 public:
-    void solve(vector<vector<char>>& board) {
+    void solve(vector<vector<char>>& board) const {
+        //dfs all on the border. Any connected to it we will set to B for border
 
+        //loop through and set all O's to X and all B's to O
+
+        traceBorder(board);
+        resolve(board);
+    }
+
+    void traceBorder(vector<vector<char>>& board) const{
         for(int r = 0; r < board.size(); r++){
-            for(int c = 0; c < board[r].size(); c++){
-                if(r == 0 || r == board.size() - 1 || c == 0 || c == board[r].size() - 1){
-                    calc(r, c, board);
-                }                
-
+            if(board[r][0] == 'O'){
+                trace(board, r, 0);
+            }
+            if(board[r][board[r].size() - 1] == 'O'){
+                trace(board, r, board[r].size() - 1);
             }
         }
 
+        for(int c = 1; c < board[0].size() - 1; c++){
+            if(board[0][c] == 'O'){
+                trace(board, 0, c);
+            }
+            if(board[board.size() - 1][c] == 'O'){
+                trace(board, board.size() - 1, c);
+            }
+        }
+    }
+
+    void trace(vector<vector<char>> & board, int r, int c) const{
+        if(r < 0 || c < 0 || r >= board.size() || c >= board[r].size()){
+            return;
+        }
+        if(board[r][c] == 'B' || board[r][c] == 'X'){
+            return;
+        }
+        board[r][c] = 'B';
+        
+        trace(board, r + 1, c);
+        trace(board, r - 1, c);
+        trace(board, r, c - 1);
+        trace(board, r, c + 1);
+    }
+
+    void resolve(vector<vector<char>>& board) const{
         for(int r = 0; r < board.size(); r++){
             for(int c = 0; c < board[r].size(); c++){
-                if(board[r][c] == 'E'){
+                if(board[r][c] == 'B'){
                     board[r][c] = 'O';
-                } else {
+                } else if (board[r][c] == 'O'){
                     board[r][c] = 'X';
                 }
             }
         }
-    }  
-
-    void calc(int r, int c, vector<vector<char>>& board){
-        if(r < 0 || c < 0 || r >= board.size() || c >= board[r].size()){
-            return;
-        }
-        if(board[r][c] != 'O'){
-            return;
-        }
-        board[r][c] = 'E';
-        calc(r + 1, c, board);
-        calc(r - 1, c, board);
-        calc(r, c + 1, board);
-        calc(r, c - 1, board);
     }
-
 };
