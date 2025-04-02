a513c07c5b84c31cda481289b34c09ce6e4f84a0
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-04-02 12:53:57 -0700

    Time: 0 ms (100%), Space: 27.5 MB (88.39%) - LeetHub

---
+++ 3429-special-array-i/3429-special-array-i.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        for(int i = 1; i < nums.size(); i++){
+            if(parity(nums[i]) == parity(nums[i - 1])){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool parity(int n){
+        return n % 2 == 0;
+    }
+};
