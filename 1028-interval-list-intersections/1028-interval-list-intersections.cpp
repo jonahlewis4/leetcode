2a598c0c5779e4b8452742b4df1de114963861d1
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-04-05 16:14:35 -0700

    Time: 3 ms (52.05%), Space: 23 MB (35.37%) - LeetHub

--- 1028-interval-list-intersections/1028-interval-list-intersections.cpp
+++ 1028-interval-list-intersections/1028-interval-list-intersections.cpp
@@ -17,17 +17,6 @@ public:
         int i = 0;
         int j = 0;
         
-        // while(!pqA.empty() && !pqB.empty()){
-        //     auto & endsFirst = pqA.top()[1] < pqB.top()[1] ? pqA : pqB;
-        //     auto & startsSecond = pqA.top()[0] > pqB.top()[0] ? pqA : pqB;
-
-        //     if(endsFirst.top()[1] - startsSecond.top()[0] >= 0){
-        //         res.push_back({startsSecond.top()[0], endsFirst.top()[1]});
-        //     }
-
-        //     endsFirst.pop();
-        // }
-
         while(i < firstList.size() && j < secondList.size()){
             auto & endsFirst = firstList[i][1] < secondList[j][1] ? firstList : secondList;
             int &endFirstIdx = &endsFirst == &firstList ? i : j;
