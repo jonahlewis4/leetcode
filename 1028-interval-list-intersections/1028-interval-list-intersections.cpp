3f314b0c43849b98e11fe220e3dc6e604cbba794
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-04-05 16:14:24 -0700

    Time: 0 ms (100%), Space: 22.9 MB (86.11%) - LeetHub

--- 1028-interval-list-intersections/1028-interval-list-intersections.cpp
+++ 1028-interval-list-intersections/1028-interval-list-intersections.cpp
@@ -1,19 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
-        const auto cmp = [](const auto & a, const auto & b){
-            return a[0] > b[0];
-        };
-
-        priority_queue<vector<int>, vector<vector<int>>, decltype(cmp)> pqA;
-        priority_queue<vector<int>, vector<vector<int>>, decltype(cmp)> pqB;
-
-        for(const auto & interval : firstList){
-            pqA.push(interval);
-        }
-        for(const auto & interval : secondList){
-            pqB.push(interval);
-        }
+        
 
         //if b starts after or equal  a and ends after or equal, there is overlap.
         //if a starts after or equal b and ends after, or equal,  there is overlap.
@@ -26,16 +14,34 @@ public:
         //pop the one that ends first;
         vector<vector<int>> res;
 
-        while(!pqA.empty() && !pqB.empty()){
-            auto & endsFirst = pqA.top()[1] < pqB.top()[1] ? pqA : pqB;
-            auto & startsSecond = pqA.top()[0] > pqB.top()[0] ? pqA : pqB;
+        int i = 0;
+        int j = 0;
+        
+        // while(!pqA.empty() && !pqB.empty()){
+        //     auto & endsFirst = pqA.top()[1] < pqB.top()[1] ? pqA : pqB;
+        //     auto & startsSecond = pqA.top()[0] > pqB.top()[0] ? pqA : pqB;
 
-            if(endsFirst.top()[1] - startsSecond.top()[0] >= 0){
-                res.push_back({startsSecond.top()[0], endsFirst.top()[1]});
+        //     if(endsFirst.top()[1] - startsSecond.top()[0] >= 0){
+        //         res.push_back({startsSecond.top()[0], endsFirst.top()[1]});
+        //     }
+
+        //     endsFirst.pop();
+        // }
+
+        while(i < firstList.size() && j < secondList.size()){
+            auto & endsFirst = firstList[i][1] < secondList[j][1] ? firstList : secondList;
+            int &endFirstIdx = &endsFirst == &firstList ? i : j;
+            auto & startsSecond = firstList[i][0] > secondList[j][0] ? firstList : secondList;
+            int startSecondIdx = &startsSecond == &firstList ? i : j;
+
+            if(endsFirst[endFirstIdx][1] - startsSecond[startSecondIdx][0] >= 0){
+                res.push_back({startsSecond[startSecondIdx][0], endsFirst[endFirstIdx][1]});
             }
 
-            endsFirst.pop();
+            endFirstIdx++;
         }
+
+
         return res;
 
     }
