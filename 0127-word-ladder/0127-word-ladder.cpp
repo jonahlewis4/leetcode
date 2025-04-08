43760895df3168d40cac314363e4761f5e4ef38c
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-04-08 00:11:50 -0700

    Time: 1258 ms (8.67%), Space: 21.8 MB (34%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_map<string, bool> dict;
-
-        for(const auto & s : wordList){
-            dict[s] = true;
+        unordered_map<string, int> dict;
+        wordList.push_back(beginWord);
+        for(int i = 0; i < wordList.size(); i++){
+            dict[wordList[i]] = i;
         }
-        dict[beginWord] = true;
 
         if(dict.find(endWord) == dict.end()){
             return 0;
         }
-
-        queue<string> q;
-        q.push(beginWord);
+        int targetIdx = dict[endWord];
+        queue<int> q;
+        q.push(dict[beginWord]);
         int length = 1;
         while(!q.empty()){
             int n = q.size();
             for(int i = 0; i < n; i++){
-                string s = q.front();
+                int idx = q.front();
                 q.pop();
-                if(s == endWord){
+                if(idx == targetIdx){
                     return length;
                 }
-                dict[s] = false;
+                string s = wordList[idx];
+                dict[s] = -1;
                 for(int i = 0; i < s.size(); i++){
                     for(char c = 'a'; c <= 'z'; c++){
                         char oldC = s[i];
                         s[i] = c;
-                        if(dict.find(s) != dict.end() && dict[s] == true){
-                            q.push(s);
+                        if(dict.find(s) != dict.end() && dict[s] != -1){
+                            q.push(dict[s]);
                         }
                         s[i] = oldC;
                     }
