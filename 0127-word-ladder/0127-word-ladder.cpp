e9b50f7f7f33a69ab850b8969d1ac229adf9d2c4
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-04-08 00:06:19 -0700

    Time: 1239 ms (8.84%), Space: 34.1 MB (19.37%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,50 +1,45 @@
 class Solution {
 public:
-    int ladderLength(const string &beginWord, const string &endWord, vector<string>& wordList) {
-        unordered_map<string, int> dict;
-        wordList.push_back(beginWord);
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_map<string, bool> dict;
 
-        for(int i = 0; i < wordList.size(); i++){
-            dict[wordList[i]] = i;
+        for(const auto & s : wordList){
+            dict[s] = true;
         }
+        dict[beginWord] = true;
+
         if(dict.find(endWord) == dict.end()){
             return 0;
         }
-        int i = 1;
-        int goalI = dict[endWord];
-        queue<int> q;
-        q.push(dict[beginWord]);
 
+        queue<string> q;
+        q.push(beginWord);
         int length = 1;
-
         while(!q.empty()){
-            int n = q.size(); 
+            int n = q.size();
             for(int i = 0; i < n; i++){
-                int idx = q.front();
+                string s = q.front();
                 q.pop();
-                if(idx == goalI){
+                if(s == endWord){
                     return length;
                 }
-
-                string s = wordList[idx];
+                dict[s] = false;
                 for(int i = 0; i < s.size(); i++){
                     for(char c = 'a'; c <= 'z'; c++){
-                        char old = s[i];
+                        char oldC = s[i];
                         s[i] = c;
-                        if(dict.find(s) == dict.end() || dict[s] == -1){
-                            s[i] = old;
-                            continue;
+                        if(dict.find(s) != dict.end() && dict[s] == true){
+                            q.push(s);
                         }
-                        q.push(dict[s]);
-                        dict[s] = -1;
-                        s[i] = old;
+                        s[i] = oldC;
                     }
                 }
             }
             length++;
-        }
+        }    
+
+
 
         return 0;
-        
     }
 };
