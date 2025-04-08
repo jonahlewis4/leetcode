d40ae19253b1d41472dd3524ebecad9831fc2a36
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-04-07 20:26:26 -0700

    Time: 0 ms (100%), Space: 8.7 MB (99.8%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -3,12 +3,10 @@ public:
     vector<int> getRow(int rowIndex) {
         vector<int> row(rowIndex + 1, 1);
 
-        
-        for(int i = 0; i < rowIndex; i++){
-            int before = 1;
-            for(int j = i; j > 0; j--){
-                row[j] = row[j] + row[j - 1];
-            }  
+        long current = 1;
+        for(int i = 1; i <= rowIndex; i++){
+            current = current * (rowIndex - i + 1) / i;
+            row[i] = current;
         }
         return row;
     }
