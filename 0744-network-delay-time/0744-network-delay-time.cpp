a8c98d80edade2899f592ae1c33b97144b8658b9
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-04-09 22:02:34 -0700

    Time: 67 ms (97.57%), Space: 44 MB (93.94%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.cpp
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -1,49 +1,62 @@
 class Solution {
+private:
+    struct edgeDest{
+        int node;
+        int weight;
+    };
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<int> dst(n + 1, INT_MAX);
-
-        vector<vector<pair<int, int>>> adjList(n + 1);
+        unordered_map<int, vector<edgeDest>> adjList;
         for(const auto & time : times){
             int node1 = time[0];
             int node2 = time[1];
             int weight = time[2];
+            adjList[node1].push_back({
+                .node = node2,
+                .weight = weight
+            });
+        }  
 
-            adjList[node1].push_back({weight, node2});
-        }
+        auto cmp = [](const auto & a, const auto & b){
+            return a.weight < b.weight;
+        };
+        priority_queue<edgeDest, vector<edgeDest>, decltype(cmp)> pq;
 
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        pq.push({0, k});
-        vector<bool> visited(n + 1, false);
-        dst[k] = 0;
-        int visitCount = 0;
-        int ans = -1;
+        vector<int> dist(n + 1, INT_MAX);
+        dist[k] = 0;
+        pq.push({
+            .node = k,
+            .weight = 0
+        });
         while(!pq.empty()){
-            auto [weight, node] = pq.top();
+            edgeDest e = pq.top();
             pq.pop();
-            if(visited[node]){
+
+            if(e.weight > dist[e.node]){
                 continue;
             }
 
-            visited[node] = true;
-            visitCount++;
 
-            ans = weight;
+            for(auto const & neigh : adjList[e.node]){
+                int newWeight = neigh.weight + e.weight;
 
-            for(const auto & neigh : adjList[node]){
-                auto [neighWeight, neighNode] = neigh;
-                
-                int newWeight = weight + neighWeight;
-                //dst[neighNode] = newWeight;
-                if(!visited[neighNode]){
+                if(newWeight < dist[neigh.node]){
+                    dist[neigh.node] = newWeight;
                     pq.push({
-                        newWeight,
-                        neighNode
+                        .node = neigh.node,
+                        .weight = newWeight
                     });
                 }
             }
         }
-        return visitCount == n ? ans : -1;
 
+
+
+        int res = -1;
+        for(int i = 1; i < dist.size(); i++){
+            res = max(res, dist[i]);
+        }
+
+        return res == INT_MAX ? -1 : res;
     }
 };
