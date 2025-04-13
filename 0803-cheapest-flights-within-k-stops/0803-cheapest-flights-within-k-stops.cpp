d92022d431a39f2fb13e1e70d14a5757885eb987
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-04-13 01:58:20 -0700

    Time: 0 ms (100%), Space: 18 MB (64.18%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -1,30 +1,62 @@
 class Solution {
+    struct visit {
+        int id;
+        int weight;
+    };
+
+    struct edge {
+        int nextId;
+        int weight;
+    };
+
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        //bellman ford.
-        vector<int> dist(n, INT_MAX);
-        dist[src] = 0;
-        for(int i = 0; i <= k; i++){
-            vector<int> tmpPrices = dist;
-            for(const auto & flight : flights){
-                int src = flight[0];
-                int dst = flight[1];
-                int weight = flight[2];
+        //basic queue.  
 
-                if(dist[src] == INT_MAX){
+        vector<vector<edge>> adjList(n);
+        for(const auto & flight : flights){
+            int src = flight[0];
+            int dst = flight[1];
+            int weight = flight[2];
+            adjList[src].push_back({
+                .nextId = dst,
+                .weight = weight
+            });
+        }
+        
+        queue<visit> q;
+        q.push({
+            .id = src,
+            .weight = 0
+        });
+
+        vector<int> distances(n, INT_MAX);
+
+
+        int i = 0;
+        while(!q.empty() && i <= k + 1){
+            int n = q.size();
+            for(int j = 0; j < n; j++){
+                auto v = q.front();
+                q.pop();
+                if(v.weight > distances[v.id]){
                     continue;
                 }
+                distances[v.id] = v.weight;
 
-                if(dist[src] + weight < tmpPrices[dst]){
-                    tmpPrices[dst] = dist[src] + weight;
+                for(const auto & edge : adjList[v.id]){
+                    int newWeight = v.weight + edge.weight;
+                    if(newWeight <= distances[edge.nextId]){
+                        q.push({
+                            .id = edge.nextId,
+                            .weight = newWeight
+                        });
+                    }
                 }
             }
-
-            dist = tmpPrices;
-
+            i++;
         }
 
-        return dist[dst] == INT_MAX ? -1 : dist[dst];
-
+        return distances[dst] == INT_MAX ? -1 : distances[dst];
     }
 };
