d05c8cb6fd788c091e21d2d08987dd3ce46e620e
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-04-14 01:21:05 -0700

    Time: 15 ms (14.7%), Space: 20 MB (80.87%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -1,32 +1,32 @@
 class Solution {
+private:
+    unordered_map<string, multiset<string>> list;
+    vector<string> res;
 public:
     vector<string> findItinerary(vector<vector<string>>& tickets) {
         //push lowest in lexi order of given node to stack. 
         //if top of stack has none in lexi order, pop from stack and put in answer.
 
-        unordered_map<string, multiset<string>> list;
         for(auto const & t : tickets){
             const string & from = t[0];
             const string & to = t[1];
             list[from].insert(to);
         }
 
-        stack<string> stack;
-        stack.push("JFK");
-        vector<string> res;
-        while(!stack.empty()){
-            string city = stack.top();
-            if(list[city].empty()){
-                stack.pop();
-                res.push_back(city);
-            } else {
-                auto firstNeighItr = list[city].begin();
-                stack.push(*firstNeighItr);
-                list[city].erase(firstNeighItr);
-            }
-        }
+        dfs("JFK");
 
         reverse(res.begin(), res.end());
         return res;
+    }
+
+    void dfs(const string & city){
+        while(!list[city].empty()){
+            auto firstNeighItr = list[city].begin();
+            string next = *firstNeighItr;
+            list[city].erase(firstNeighItr);
+            dfs(next);
+        }
+        res.push_back(city);
+        return;
     }
 };
