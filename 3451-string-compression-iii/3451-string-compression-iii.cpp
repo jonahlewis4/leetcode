3efbc07619b0c00b2cdb271e34f1b81e9300fa8c
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-04-16 10:12:32 -0700

    Time: 10 ms (93.23%), Space: 28.1 MB (91.21%) - LeetHub

---
+++ 3451-string-compression-iii/3451-string-compression-iii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string compressedString(string word) {
+        string comp = "";
+        int prefixLength = 0;
+        char prefixChar = word[0];
+        for(int i = 0; i < word.size(); i++){
+            if(word[i] != prefixChar){
+                compress(comp, prefixLength, prefixChar);
+                prefixChar = word[i];
+                prefixLength = 0;
+            }
+            prefixLength++;
+        }
+        compress(comp, prefixLength, prefixChar);
+        return comp;
+    }
+
+    void compress(string &comp, int prefixLength, char prefixChar) const {
+        if(prefixLength == 0){
+            return;
+        }
+        while(prefixLength > 9){
+            comp.push_back('9');
+            comp.push_back(prefixChar);
+            prefixLength -= 9;
+        }
+        comp.push_back(prefixLength + '0');
+        comp.push_back(prefixChar);
+        prefixLength = 0;
+    }
+};
