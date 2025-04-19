e3e731c261e645a6b45aa6e59a9f9fb15f243c52
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-04-18 17:16:49 -0700

    Time: 83 ms (66.93%), Space: 44.3 MB (61.9%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.cpp
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -1,66 +1,60 @@
 class Solution {
-private:
-    struct edgeDest{
-        int node;
+    struct edge{
+        int dest;
         int weight;
     };
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        unordered_map<int, vector<edgeDest>> adjList;
-        for(const auto & time : times){
-            int node1 = time[0];
-            int node2 = time[1];
-            int weight = time[2];
-            adjList[node1].push_back({
-                .node = node2,
-                .weight = weight
-            });
-        }  
+        //MST.
 
-        auto cmp = [](const auto & a, const auto & b){
-            return a.weight > b.weight;
-        };
-        priority_queue<edgeDest, vector<edgeDest>, decltype(cmp)> pq;
+        //djiksta's ant return the max.
+        //if one of them is INT_MAX return -1;
 
         vector<int> dist(n + 1, INT_MAX);
         dist[k] = 0;
+
+        priority_queue<edge, vector<edge>, decltype([](const auto & a, const auto & b){
+            return a.weight > b.weight;
+        })> pq; 
+
         pq.push({
-            .node = k,
+            .dest = k,
             .weight = 0
         });
 
-        vector<int> visited(n + 1, false);
-        int vc = 0;
-        while(!pq.empty() && vc < n){
-            edgeDest e = pq.top();
-            pq.pop();
+        vector<vector<edge>> adjList(n + 1);
+        for(const auto & time : times){
+            adjList[time[0]].push_back({
+                .dest = time[1],
+                .weight = time[2],
+            });
+        }
 
-            if(visited[e.node]){
+        while(!pq.empty()){
+            edge e = pq.top();
+            pq.pop();
+            if(e.weight > dist[e.dest]){
                 continue;
             }
-            visited[e.node] = true;
-            vc++;
-
-            for(auto const & neigh : adjList[e.node]){
+            for(const auto & neigh : adjList[e.dest]){
                 int newWeight = neigh.weight + e.weight;
-
-                if(!visited[neigh.node] && newWeight < dist[neigh.node]){
-                    dist[neigh.node] = newWeight;
+                if(newWeight < dist[neigh.dest]){
+                    dist[neigh.dest] = newWeight;
                     pq.push({
-                        .node = neigh.node,
+                        .dest = neigh.dest,
                         .weight = newWeight
                     });
                 }
             }
         }
 
-
-
-        int res = -1;
+        int maxi = INT_MIN;
         for(int i = 1; i < dist.size(); i++){
-            res = max(res, dist[i]);
+            if(dist[i] == INT_MAX){
+                return -1;
+            } 
+            maxi = max(maxi, dist[i]);
         }
-
-        return res == INT_MAX ? -1 : res;
+        return maxi;
     }
 };
