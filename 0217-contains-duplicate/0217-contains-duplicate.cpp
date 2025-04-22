30772feb37d0d5ab0f548fde1b17f359b6b596d9
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-04-21 17:25:40 -0700

    Time: 21 ms (85.78%), Space: 70.9 MB (74.65%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        std::unordered_set<int> encountered;
-        for (const auto & num : nums){
-            if(encountered.find(num) != encountered.end()){
+        // std::unordered_set<int> encountered;
+        // for (const auto & num : nums){
+        //     if(encountered.find(num) != encountered.end()){
+        //         return true;
+        //     }
+        //     encountered.insert(num);
+        // }
+        // return false;
+        sort(nums.begin(), nums.end());
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i - 1] == nums[i]){
                 return true;
             }
-            encountered.insert(num);
         }
         return false;
     }
