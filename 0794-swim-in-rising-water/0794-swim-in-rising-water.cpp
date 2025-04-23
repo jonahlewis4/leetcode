6a11cfc467caf8464c305d53b65ddeb34e991124
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-04-23 00:31:40 -0700

    Time: 8 ms (52.93%), Space: 13.3 MB (55.62%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -1,63 +1,52 @@
 class Solution {
-    struct qItem {
-        int r;
-        int c;
-        int depth;
-    };
-    vector<tuple<int, int>> dirs = {
-        {0,1},
-        {1,0},
-        {0,-1},
-        {-1,0}
-    };
 public:
     int swimInWater(vector<vector<int>>& grid) {
-        //bfs with visited. 
-        //if water is not high enough push it and deal with it later.
-        priority_queue<qItem, vector<qItem>, decltype([](const auto & a, const auto & b){
-            return a.depth >= b.depth;
-        })> q;
-        q.push({
-            .r = 0,
-            .c = 0,
-            .depth = grid[0][0]
-        });
-        int t = 0;
-        while(!q.empty()){
-            qItem item = q.top();
-            q.pop();
-            
-            if(item.depth > t){
-                t = item.depth;
-            } 
-            if(grid[item.r][item.c] < 0){
-                continue;
-            }  
-
-            if(item.r == grid.size() - 1 && item.c == grid[0].size() - 1){
-                return t;
-            }
-            grid[item.r][item.c] = -1;
-
-            for(const auto & dir : dirs){
-                auto [rOff, cOff] = dir;
-                int newR = item.r + rOff;
-                int newC = item.c + cOff;
-                if(newR < 0 || newC < 0 || newR >= grid.size() || newC >= grid[newR].size()){
-                    continue;
-                }
-                if(grid[newR][newC] < 0){
-                    continue;
-                }
-                q.push({
-                    .r = newR,
-                    .c = newC,
-                    .depth = grid[newR][newC]
-                });
+        int n = grid.size();
+        int l = 0;
+        int r = n * n - 1;
+        int res = INT_MIN;
+        while(l <= r){
+            int m = (l + r) / 2;
+            if(canReach(m, grid)){
+                //if we can reach we need to try smaller options
+                res = m;
+                r = m - 1;
+            } else {
+                //if we cannot reach, we need to try larger options.
+                l = m + 1;
             }
         }
-
-        return -1;
-
+        return res;
     }
+    bool canReach(int maxDepth, const vector<vector<int>> &grid) {
+        vector<vector<bool>> visited(grid.size(), vector<bool>(grid.size(), false));
+        return dfs(maxDepth, grid, visited, 0, 0);
+    }
+    bool dfs(int maxDepth, const vector<vector<int>> &grid, vector<vector<bool>> &visited, int r, int c) const {
+        if(r < 0 || c < 0 || r >= grid.size() || c >= grid[r].size()){
+            return false;
+        }
+        if(grid[r][c] > maxDepth){
+            return false;
+        }
+        if(r == grid.size() - 1 && c == grid[r].size() - 1){
+            return true;
+        }
+        if(visited[r][c]){
+            return false;
+        }
+        visited[r][c] = true;
+        
+
+
+
+        bool below = dfs(maxDepth, grid, visited, r + 1, c);
+        bool above = dfs(maxDepth, grid, visited, r - 1, c);
+        bool left = dfs(maxDepth, grid, visited, r, c - 1);
+        bool right = dfs(maxDepth, grid, visited, r, c + 1);
+        
+        return below || above || left || right;
+        
+    }
+
 };
