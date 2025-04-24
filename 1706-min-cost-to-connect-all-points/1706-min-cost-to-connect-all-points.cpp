2097e4cb4382eff083972b3601dbb947a537e695
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-04-24 14:12:18 -0700

    Time: 19 ms (96.92%), Space: 14.7 MB (92.97%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.cpp
@@ -1,94 +1,57 @@
 class Solution {
-private:
-    class DSU {
-        vector<int> root;
-        vector<int> size;
-    public:
-        DSU(int n){
-            root.resize(n);
-            for(int i = 0; i < n; i++){
-                root[i] = i;
-            }
-            size.resize(n, 1);
-        }
-
-        int find(int i) {
-            if(root[i] == i){
-                return i;
-            }
-            root[i] = find(root[i]);
-            return root[i];
-        }
-
-        bool Union(int a, int b){
-            int aRoot = find(a);
-            int bRoot = find(b);
-            if(aRoot == bRoot){
-                return false;
-            }
-
-            if(size[aRoot] < size[bRoot]){
-                size[bRoot] += size[aRoot];
-                root[aRoot] = bRoot;
-            } else {
-                size[aRoot] += size[bRoot];
-                root[bRoot] = aRoot;
-            }
-
-            return true;
-        }
-    };
-private:
-    struct point {
+    struct point{
         int x;
         int y;
-        int dist(point p2){
-            return abs(p2.x - x) + abs(p2.y - y);
+        int distanceFrom(const point &p2){
+            return abs(x - p2.x) + abs(y - p2.y);
         }
-    };
-    struct distance {
-        int point1;
-        int point2;
-        int distance;
     };
 public:
     int minCostConnectPoints(vector<vector<int>>& _points) {
-        //MInimum spanning tree -
-        //kruskal's
-        vector<point> points;
-        vector<distance> distances;
-        for(const auto & point : _points) {
-            int x = point[0];
-            int y = point[1];
-            points.push_back({
+        //kurskal's or prim's.
+    
+        //use prim's based on vertices because there are many edges.
+        //this means the graph is dense.
+        //prim's based on vertices is best for dense graphs.
+
+
+        //convert points to list of points.
+        vector<point> points(_points.size());
+        for(int i = 0; i < _points.size(); i++){
+            int x = _points[i][0];
+            int y = _points[i][1];
+            points[i] = {
                 .x = x,
                 .y = y
-            });
+            };
         }
-        for(int i = 0; i < points.size(); i++){
-            for(int j = i + 1 ; j < points.size(); j++){
-                distances.push_back({
-                    .point1 = i,
-                    .point2 = j,
-                    .distance = points[i].dist(points[j])
-                });
+        //start with a vertex.
+        //use the verison without a priority queue because it is a dense graph.
+        int vertexI = 0;
+        vector<int> dist(points.size(), INT_MAX);
+        dist[vertexI] = 0;
+        vector<bool> visited(points.size(), false);
+        int visitCount = 0;
+        int res = 0;
+        while(visitCount < points.size() - 1){
+            visited[vertexI] = true;
+            visitCount++;
+            int nextVertexI = -1;
+            for(int i = 0; i < points.size(); i++){
+                if(visited[i]){
+                    continue;
+                }
+                int newDistance = points[i].distanceFrom(points[vertexI]);
+                dist[i] = min(dist[i], newDistance);
+                if(nextVertexI == -1 || dist[i] < dist[nextVertexI]){
+                    nextVertexI = i;
+                }
             }
+            vertexI = nextVertexI;
+            res += dist[vertexI];
         }
 
-        sort(distances.begin(), distances.end(), [](const auto & a, const auto & b){
-            return a.distance < b.distance;
-        });
-
-        DSU dsu(points.size());
-
-        int total = 0;
-        for(const auto & distance : distances){
-            if(dsu.Union(distance.point1, distance.point2)){
-                total += distance.distance;
-            }
-        }
-
-        return total;
+        return res;
         
     }
 };
