af4f25d4ae116537dcc080bfc2b80bf971ed724d
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-04-23 22:10:39 -0700

    Time: 0 ms (100%), Space: 8.8 MB (57%) - LeetHub

--- 3394-minimum-array-end/3394-minimum-array-end.cpp
+++ 3394-minimum-array-end/3394-minimum-array-end.cpp
@@ -3,18 +3,33 @@ public:
     long long minEnd(int n, int x) {
         //strictly increasing array of size n that ands to x.
 
-        //and not monotonic.
         
-        //only way
 
-        int found = 1;
-        long long num = x;
-        while(found < n){
-            num++;
-            num |= x;
-            found++;
-        }
-        return num;
+        //go from right to left in x
+        //go from right to left in n - 1;
+
+        n--;
         
+        //if x is 0 just move over
+        //if x digit is 1 continue
+
+        long long res = x;
+        long twoPow = 1;
+        while(n > 0){
+            int xDigit = x & 1;
+            if(x > 0 && xDigit == 1){
+                
+            } else {
+                int nDigit = n & 1;
+                n >>= 1;
+                if(nDigit == 1){
+                    res |= twoPow;
+                }
+            }
+            x >>= 1;
+            twoPow <<= 1;
+        }
+
+        return res;
     }
 };
