a1c796ebd54898a86aa401b4cf75ddb7b6a2299c
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-04-26 00:05:56 -0700

    Time: 4 ms (47.77%), Space: 17.8 MB (73.17%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -1,30 +1,60 @@
 class Solution {
+    struct edge{
+        int dest;
+        int weight;  
+    };
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<int> prices(n, INT_MAX);
-        prices[src] = 0;
-        vector<vector<pair<int, int>>> adj(n);
-        for (const auto& flight : flights) {
-            adj[flight[0]].emplace_back(flight[1], flight[2]);
+        //bfs of edges. 
+        //check if we've reached this dst in less time. Since we're going time by time
+        //we can guarantee each change will only be beneficial. 
+
+        vector<vector<edge>> adjList(n);
+        for(const auto & flight : flights) {
+            int from = flight[0];
+            int to = flight[1];
+            int price = flight[2];
+            adjList[from].push_back({
+                .dest = to,
+                .weight = price
+            });
         }
 
-        queue<tuple<int, int, int>> q;
-        q.push({0, src, 0});
+        queue<edge> q;
+        q.push({
+            .dest = src,
+            .weight = 0
+        });
 
-        while (!q.empty()) {
-            auto [cst, node, stops] = q.front();
-            q.pop();
-            if (stops > k) continue;
+        vector<int> distance(n, INT_MAX);
+        distance[src] = 0;
 
-            for (const auto& neighbor : adj[node]) {
-                int nei = neighbor.first, w = neighbor.second;
-                int nextCost = cst + w;
-                if (nextCost < prices[nei]) {
-                    prices[nei] = nextCost;
-                    q.push({nextCost, nei, stops + 1});
+        int stops = 0;
+        while(!q.empty()){
+            if(stops > k){
+                break;
+            }
+            int n = q.size();
+            for(int i  = 0; i < n; i++){
+                edge e = q.front();
+                q.pop();
+                
+                for(const auto & n : adjList[e.dest]){
+                    int newWeight = n.weight + e.weight;
+                    if(newWeight < distance[n.dest]){
+                        distance[n.dest] = newWeight;
+                        q.push({
+                            .dest = n.dest,
+                            .weight = newWeight
+                        });
+                    }
+                    
                 }
             }
+        
+            stops++;
         }
-        return prices[dst] == INT_MAX ? -1 : prices[dst];
+        
+        return distance[dst] == INT_MAX ? -1 : distance[dst];
     }
 };
