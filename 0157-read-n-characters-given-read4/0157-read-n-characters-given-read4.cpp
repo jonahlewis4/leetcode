f28820f0c36ba869e09b8ad239ebca4a79f6a2b9
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-04-25 18:45:05 -0700

    Time: 0 ms (100%), Space: 8.9 MB (18.23%) - LeetHub

--- 0157-read-n-characters-given-read4/0157-read-n-characters-given-read4.cpp
+++ 0157-read-n-characters-given-read4/0157-read-n-characters-given-read4.cpp
@@ -24,7 +24,7 @@ public:
                 if(totalRead == n){
                     break;
                 }
-                *(buf + totalRead) = *(readBuffer + i);
+                buf[totalRead] = readBuffer[i];
                 totalRead++;
             }
         }
