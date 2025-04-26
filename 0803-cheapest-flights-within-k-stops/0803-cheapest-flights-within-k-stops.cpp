fcbb54e6afdfb08293f9f12491dcbea779382782
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-04-26 00:05:40 -0700

    Time: 3 ms (63.85%), Space: 17.7 MB (73.17%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -1,41 +1,30 @@
 class Solution {
-    struct edge{
-        int src;
-        int dest;
-        int weight;
-    };
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<int> dist(n, INT_MAX);
-        dist[src] = 0;
-
-        vector<edge> edges;
-        for(auto const & flight : flights){
-            int src = flight[0];
-            int to = flight[1];
-            int price = flight[2];
-            edges.push_back({
-                .src = src,
-                .dest = to,
-                .weight = price,
-            });
+        vector<int> prices(n, INT_MAX);
+        prices[src] = 0;
+        vector<vector<pair<int, int>>> adj(n);
+        for (const auto& flight : flights) {
+            adj[flight[0]].emplace_back(flight[1], flight[2]);
         }
 
-        //k + 1 because we want to see which node we can reach after k + 1 # of edges (stops does not count the final edge)
-        for(int i = 0; i < k + 1; i++){
-            vector<int> newDist = dist;
-            for(const auto & e : edges){
-                if(dist[e.src] == INT_MAX){
-                    continue;
-                }
-                if(dist[e.src] + e.weight < newDist[e.dest]){
-                    newDist[e.dest] = dist[e.src] + e.weight;
+        queue<tuple<int, int, int>> q;
+        q.push({0, src, 0});
+
+        while (!q.empty()) {
+            auto [cst, node, stops] = q.front();
+            q.pop();
+            if (stops > k) continue;
+
+            for (const auto& neighbor : adj[node]) {
+                int nei = neighbor.first, w = neighbor.second;
+                int nextCost = cst + w;
+                if (nextCost < prices[nei]) {
+                    prices[nei] = nextCost;
+                    q.push({nextCost, nei, stops + 1});
                 }
             }
-            dist = newDist;
         }
-
-        return dist[dst] == INT_MAX ? -1 : dist[dst];
-        
+        return prices[dst] == INT_MAX ? -1 : prices[dst];
     }
 };
