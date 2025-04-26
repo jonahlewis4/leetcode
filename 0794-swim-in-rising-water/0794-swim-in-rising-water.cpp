bbeed2612c191e9dc0c79c7eff27b20cbcd32012
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-04-26 11:55:25 -0700

    Time: 10 ms (44.63%), Space: 12.9 MB (70.53%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -1,52 +1,110 @@
 class Solution {
+    struct item{
+        int r;
+        int c;
+        int weight;
+    };
+
+    class compareWeight{
+    public:
+        bool operator() (const auto & a, const auto & b) {
+            return a.weight < b.weight;
+        }
+    };
+    
+    vector<tuple<int, int>> dirs = {
+        {0,1},
+        {1,0},
+        {-1, 0},
+        {0, -1},
+    };
+
+    class DSU {
+        vector<int> size;
+        vector<int> root;
+    public:
+        DSU(int n){
+            size.resize(n, 1);
+            root.resize(n, -1);
+            iota(root.begin(), root.end(), 0);
+        }
+
+        int Find(int i){
+            if(root[i] == i){
+                return i;
+            }
+            root[i] = Find(root[i]);
+            return root[i];
+        }
+        void Union(int a, int b){
+            int aRoot = Find(a);
+            int bRoot = Find(b);
+
+            if(aRoot == bRoot){
+                return;
+            }
+
+            if(size[aRoot] < size[bRoot]){
+                swap(aRoot, bRoot);
+            }
+
+            //aRoot is the larger one so move b into a.
+
+            root[bRoot] = aRoot;
+            size[aRoot] += size[bRoot];
+        }
+    };
+    
+
+    int n;
 public:
     int swimInWater(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int l = 0;
-        int r = n * n - 1;
-        int res = INT_MIN;
-        while(l <= r){
-            int m = (l + r) / 2;
-            if(canReach(m, grid)){
-                //if we can reach we need to try smaller options
-                res = m;
-                r = m - 1;
-            } else {
-                //if we cannot reach, we need to try larger options.
-                l = m + 1;
+        //DSU, sort by weight.
+
+        //union the smaller adjecent squares with the current square. 
+        //if first in same as last return current weight;
+
+        n = grid.size();
+        vector<item> points(n * n);
+
+        for(int r = 0; r < n; r++){
+            for(int c = 0; c < n; c++){
+                points[flatten(r,c)] = {
+                    .r = r,
+                    .c = c,
+                    .weight = grid[r][c],
+                };
             }
         }
-        return res;
-    }
-    bool canReach(int maxDepth, const vector<vector<int>> &grid) {
-        vector<vector<bool>> visited(grid.size(), vector<bool>(grid.size(), false));
-        return dfs(maxDepth, grid, visited, 0, 0);
-    }
-    bool dfs(int maxDepth, const vector<vector<int>> &grid, vector<vector<bool>> &visited, int r, int c) const {
-        if(r < 0 || c < 0 || r >= grid.size() || c >= grid[r].size()){
-            return false;
-        }
-        if(grid[r][c] > maxDepth){
-            return false;
-        }
-        if(r == grid.size() - 1 && c == grid[r].size() - 1){
-            return true;
-        }
-        if(visited[r][c]){
-            return false;
-        }
-        visited[r][c] = true;
-        
 
+        //we can use counting sort??!?? (if I get it to work we gonna run this back with it)
+        sort(points.begin(), points.end(), compareWeight());
 
+        DSU dsu(n * n);
+        for(auto const & point : points){
+            //merge point with all of its neighboors which are smaller. 
+            for(auto const & dir : dirs){
+                auto const [rOff, cOff] = dir;
+                int newR = point.r + rOff;
+                int newC = point.c + cOff;
 
-        bool below = dfs(maxDepth, grid, visited, r + 1, c);
-        bool above = dfs(maxDepth, grid, visited, r - 1, c);
-        bool left = dfs(maxDepth, grid, visited, r, c - 1);
-        bool right = dfs(maxDepth, grid, visited, r, c + 1);
-        
-        return below || above || left || right;
-        
+                if(newR < 0 || newC < 0 || newR >= n || newC >= n){
+                    continue;
+                }
+                if(grid[newR][newC] > grid[point.r][point.c]){
+                    continue;
+                }
+                dsu.Union(flatten(newR, newC), flatten(point.r, point.c));
+            }
+            if(dsu.Find(flatten(0, 0)) == dsu.Find(flatten(n - 1, n - 1))){
+                return point.weight;
+            }
+        }
+        return n * n;
+
     }
 
+    int flatten(int r, int c) const {
+        return r * n + c;
+    }
 };
