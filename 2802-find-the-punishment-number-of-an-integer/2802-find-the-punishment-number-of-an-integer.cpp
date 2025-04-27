1e60c48128ac44c74859ea23db09e8aab9d975b1
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-04-27 16:28:34 -0700

    Time: 15 ms (72.86%), Space: 7.9 MB (96.62%) - LeetHub

--- 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.cpp
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.cpp
@@ -1,8 +1,5 @@
 class Solution {
 private:
-    unordered_map<int, unordered_map<int, bool>> memo;
-    //memo is accesed as [digits][total]
-
 
 public:
     int punishmentNumber(int n) {
@@ -15,6 +12,7 @@ public:
         return res;
     }
     bool selfSumming(int i) {
+       
         return recurse(i * i, i);
     }
 
@@ -27,10 +25,6 @@ public:
         if(total <= 0 || digits <= 0){
             return false;
         }
-        if(memo[digits].find(total) != memo[digits].end()){
-            bool res = memo[digits][total];
-            return res;
-        }
 
         int rightSum = 0;
         int tenPow = 1;
@@ -41,11 +35,9 @@ public:
             digits /= 10;
             
             if(recurse(digits, total - rightSum)){
-                memo[digits][total - rightSum] = true;
                 return true;
             }
         }
-        memo[digits][total] = false;
         return false;
     }
 
