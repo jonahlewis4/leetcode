410ad888b97dfff3eb16714fec1db1619c5b135d
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-04-28 16:59:33 -0700

    Time: 8 ms (89.02%), Space: 117.4 MB (95.1%) - LeetHub

--- 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.cpp
+++ 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.cpp
@@ -1,36 +1,21 @@
 class Solution {
-    struct node {
-        int id;
-        int indegree = 0;
-        int importance = 0;
-    };
 public:
     long long maximumImportance(int n, vector<vector<int>>& roads) {
-        vector<node> nodes(n);
+        vector<int> indegrees(n);
         for(auto const & road : roads){
             int src = road[0];
             int dest = road[1];
-            nodes[src].id = src;
-            nodes[src].indegree++;
-            nodes[dest].id = dest;
-            nodes[dest].indegree++;
+            indegrees[src]++;
+            indegrees[dest]++;
         }
-        auto oldNodes = nodes;
-        sort(nodes.begin(), nodes.end(), [](auto const & a, auto const & b){
-            return a.indegree < b.indegree;
-        });
+        sort(indegrees.begin(), indegrees.end());
 
         long long sum = 0;
 
-        for(int i = 0; i < n; i++){
-            oldNodes[nodes[i].id].importance = i + 1;
-        }
-
-        for(auto const & edge : roads){
-            int src = edge[0];
-            int dest = edge[1];
-
-            sum += oldNodes[src].importance + oldNodes[dest].importance;
+        int i = 1;
+        for(auto const & degree : indegrees){
+            sum += (long long)degree * i;
+            i++;
         }
 
         return sum;
