9ecf308d717bf76763a1160b9561998de38fbc27
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-04-28 21:42:34 -0700

    Time: 8 ms (21.53%), Space: 19.1 MB (19.58%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -158,22 +158,21 @@ public:
             .weight = 0,
         });
 
-        for(int i = 0; i < k + 1 && !q.empty(); i++){
+        for(int i = 0; i <= k + 1 && !q.empty(); i++){
             int n = q.size();
             for(int TwT = 0; TwT < n; TwT++){
                 edge e = q.front();
                 q.pop();
 
-                // if(e.weight > dist[e.dest]){
-                //     continue;
-                // }
+                if(e.weight > dist[e.dest]){
+                    continue;
+                }
 
-                //dist[e.dest] = e.weight;
+                dist[e.dest] = e.weight;
 
                 for(auto const & neigh : adjList[e.dest]){
                     int newWeight = neigh.weight + e.weight;
                     if(newWeight < dist[neigh.dest]){
-                        dist[neigh.dest] = newWeight;
                         q.push({
                             .dest = neigh.dest,
                             .weight = newWeight,
