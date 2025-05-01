09f421b45212f1dbf926acbad5ed541f65a70b76
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-05-01 13:52:24 -0700

    Time: 0 ms (100%), Space: 14.3 MB (5.38%) - LeetHub

---
+++ 1349-check-if-it-is-a-straight-line/1349-check-if-it-is-a-straight-line.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& coordinates) {
+        struct point{
+            int x;
+            int y;
+            float slope(const point &p2) const {
+                if(x - p2.x == 0){
+                    return numeric_limits<float>::infinity();
+                }
+                return float((y - p2.y)) / (x - p2.x);
+            }
+        };
+        vector<point> points;
+        for(const auto & coord : coordinates){
+            int x = coord[0];
+            int y = coord[1];
+            points.push_back({
+                .x = x,
+                .y = y,
+            });
+        }
+
+        float firstSlope = points[0].slope(points[1]); 
+
+        for(int i = 2; i < points.size(); i++){
+            if(points[i].slope(points[i - 1]) != firstSlope){
+                return false;
+            }
+        }
+        return true;
+
+        
+    }
+};
