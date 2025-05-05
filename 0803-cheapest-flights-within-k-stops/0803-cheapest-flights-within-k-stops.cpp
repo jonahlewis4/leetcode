60eb25695ba3d49a55128d9c37f199404d79fc40
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-05-05 00:34:03 -0700

    Time: 7 ms (28.41%), Space: 17.6 MB (81.46%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -39,7 +39,7 @@ public:
             for(int i  = 0; i < n; i++){
                 edge e = q.front();
                 q.pop();
-                if(e.weight < distance[e.dest]){
+                if(e.weight > distance[e.dest]){
                     continue;
                 }
                 
