f6a8ca46036fa2022dc5bf2a23b2b2189af7d4a7
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-05-04 22:31:11 -0700

    Time: 0 ms (100%), Space: 17.3 MB (42.38%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -4,96 +4,59 @@ public:
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
         return _isBipartite(n, graph);
-        // vector<bool> visited(n, false);
-
-        // queue<int> q;
-
-        // unordered_set<int> odd;
-        // unordered_set<int> even;
-        // int i = 0;
-        // for(int p = 0; p < n; p++){
-        
-        //     if(visited[p]){
-        //         continue;
-        //     }
-        //     if(q.empty()){
-        //         i = 0;
-        //         q.push(p);
-        //     }
-        //     int k = q.size();
-        //     for(int j = 0; j < k; j++){
-        //         unordered_set<int>* forbidden;
-        //         unordered_set<int>* aggregator;
-        //         if(i % 2 == 0){
-        //             forbidden = &odd;
-        //             aggregator = &even;
-        //         } else {
-        //             forbidden = &even;
-        //             aggregator = &odd;
-        //         }
-
-        //         int node = q.front();
-        //         q.pop();
-        //         if(forbidden->find(node) != forbidden->end()){
-        //             return false;
-        //         }
-        //         if(visited[node]){
-        //             continue;
-        //         }
-
-        //         aggregator->insert(node);
-        //         visited[node] = true;
-        //         for(auto const & nei : graph[node]){
-        //             if(aggregator->find(nei) != aggregator->end()){
-        //                 return false;
-        //             }
-        //             if(visited[nei]){
-        //                 continue;
-        //             }
-
-        //             q.push(nei);
-
-        //         }
-        //     }
-        //     i++;
-        // }   
-
-        // return true;
     }
 
     inline bool _isBipartite(int n, const vector<vector<int>> &adjList){
         vector<int8_t> colors(n, -1);
         for(int node = 1; node < n; node++){
-            if(colors[node] != -1){
-                continue;
-            }
-            queue<int> q;
-            q.push(node);
-            
-            int color = false;
-            while(!q.empty()){
-                int n = q.size();
-                for(int i = 0; i < n; i++){
-                    int currentNode = q.front();
-                    q.pop();
-                    if(colors[currentNode] != -1 && colors[currentNode] != color){
-                        return false;
-                    } else if (colors[currentNode] != -1){
-                        continue;
-                    }
-                    colors[currentNode] = color;
-                    for(int neigh : adjList[currentNode]){
-                        if(colors[neigh] != -1 && colors[neigh] == colors[currentNode]){
-                            return false;
-                        } else if (colors[neigh] != -1){
-                            continue;
-                        }
-                        q.push(neigh);
-                    }
-                }
-                color = !color;
+            if(!isBipartite(node, adjList, colors)){
+                return false;
             }
         }
+        
         return true;
     }
+
+    bool isBipartite(int node, const vector<vector<int>> &adjList, vector<int8_t> &colors){
+        if(colors[node] != -1){
+            return true;
+        }
+        queue<int> q;
+        q.push(node);
+
+        int color = false;
+
+        int edgeNode;
+        colors[node] = color;
+        while(!q.empty()){
+            int n = q.size();
+            for(int i = 0; i < n; i++){
+
+                int currentNode = q.front();
+
+                q.pop();
+                if(colors[currentNode] != color){
+                    return false;
+                } 
+
+                edgeNode = currentNode;
+
+                colors[currentNode] = color;
+                for(int neigh : adjList[currentNode]){
+                    if(colors[neigh] != -1 && colors[neigh] == colors[currentNode]){
+                        return false;
+                    } else if (colors[neigh] != -1){
+                        continue;
+                    }
+                    colors[neigh] = !color;
+
+                    q.push(neigh);
+                }
+            }
+            color = !color;
+        }
+
+        return true;
+    }
+
 };
