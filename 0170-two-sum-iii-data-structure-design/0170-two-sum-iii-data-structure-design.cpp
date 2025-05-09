8199dcb661b1331a1f952c56c8c8b5e2233ec8c3
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-05-08 21:01:35 -0700

    Time: 955 ms (11.42%), Space: 39.7 MB (5.02%) - LeetHub

--- 0170-two-sum-iii-data-structure-design/0170-two-sum-iii-data-structure-design.cpp
+++ 0170-two-sum-iii-data-structure-design/0170-two-sum-iii-data-structure-design.cpp
@@ -1,36 +1,21 @@
 class TwoSum {
-private:
-    unordered_map<int, int> set;
 public:
+    unordered_set<int> twosum_set;
+    unordered_set<int> s;
     TwoSum() {
         
     }
     
     void add(int number) {
-        set[number]++;
+        if(s.find(number)==s.end()) {
+            for(auto &i:s) twosum_set.insert(number+i);
+            s.insert(number);
+        } else {
+            twosum_set.insert(2*number);
+        }
     }
     
     bool find(int value) {
-        for(auto const &num : set){
-            long number = num.first;
-            long comp = value - number;
-            if(comp == number){
-                if(set[number] >= 2){
-                    return true;
-                }
-            } else {
-                if(set.find(comp) != set.end()){
-                    return true;
-                }
-            }
-        }
-        return false;
+        return twosum_set.find(value)!=twosum_set.end();
     }
 };
-
-/**
- * Your TwoSum object will be instantiated and called as such:
- * TwoSum* obj = new TwoSum();
- * obj->add(number);
- * bool param_2 = obj->find(value);
- */
