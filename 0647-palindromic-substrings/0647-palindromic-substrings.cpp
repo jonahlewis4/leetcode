5a16132d331bf149dcd86f18fe76b9c711eb45ef
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-05-10 14:00:45 -0700

    Time: 4 ms (74.69%), Space: 8.6 MB (81.82%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        //count all palindromes originating with 1 center 
+        //add with all palindromes originating with 2 center.
+
+        int total = 0;
+
+        for(int i = 0; i + 1 < s.size(); i++){
+            total += originatingFrom(i, i, s);
+            total += originatingFrom(i, i + 1, s);
+        }
+        total += originatingFrom(s.size() - 1, s.size() - 1, s);
+        return total;
+    }   
+
+    int originatingFrom(int l, int r, const string &s) const {
+        int created = 0;
+        while(l >= 0 && r < s.size()){
+            if(s[l] != s[r]){
+                return created;
+            }
+            created++;
+            l--;
+            r++;
+        }
+        return created;
+    }
+};
