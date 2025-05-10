d47a37eb5d5f2f11029ee46c9b717019e88cd47f
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-05-09 18:55:43 -0700

    Time: 0 ms (100%), Space: 14.2 MB (59.45%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> set1;
-        unordered_set<int> set2;
+        unordered_set<int> set;
 
         for(const auto & num : nums1){
-            set1.insert(num);
-        }
-        for(const auto & num : nums2){
-            set2.insert(num);
+            set.insert(num);
         }
         vector<int> res;
 
-        unordered_set<int> &larger = set2.size() > set1.size() ? set2 : set1;
-        unordered_set<int> &smaller = set2.size() > set1.size() ? set1 : set2;
-        for(const auto & num : larger){
-            if(smaller.find(num) != smaller.end()){
+        for(const auto & num : nums2){
+            if(set.find(num) != set.end()){
                 res.push_back(num);
             }
+            set.erase(num);
         }
+
+        
 
 
         return res;
-
     }
 };
