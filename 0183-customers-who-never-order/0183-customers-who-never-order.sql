7371f8345a017efdb42f6d2c6bc3b13b7a578957
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-05-15 22:24:28 -0700

    Time: 544 ms (79.28%), Space: 0B (100%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,9 +1,4 @@
-# Write your MySQL query statement below
-WITH order_counts AS (
-    SELECT customerId as id, COUNT(customerId) as num
-    FROM Orders
-    GROUP BY customerId
-)
 
 
-SELECT name as Customers FROM Customers WHERE id NOT IN (SELECT id FROM order_counts);
+
+SELECT name as Customers FROM Customers WHERE id NOT IN (SELECT customerid FROM Orders);
