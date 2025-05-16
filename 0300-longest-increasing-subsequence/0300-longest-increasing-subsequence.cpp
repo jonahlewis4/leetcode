4f420bab53aa9dc747be18a384b149dae23dc908
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-05-15 22:09:50 -0700

    Time: 1 ms (83.06%), Space: 14.1 MB (78.43%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -112,6 +112,22 @@ public:
     }
 };
 
+class BottomUp {
+    public:
+    int lengthOfLIS(vector<int>& nums) const{
+        vector<int> dp(nums.size(), 1);
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = 0; j < i; j++){
+                if(nums[j] < nums[i]){
+                    dp[i] = max(dp[j] + 1, dp[i]);
+                }
+            }
+        }
+        return *max_element(dp.begin(), dp.end());
+;
+    }
+};
+
 class Solution {
     public:
     int lengthOfLIS(vector<int>& nums) {
