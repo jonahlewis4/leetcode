c538f0a9319d84dd25a185c8bd017ea9cdcc8015
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-05-17 00:07:55 -0700

    Time: 0 ms (100%), Space: 8.7 MB (37.57%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -0,0 +1,95 @@
+class Divider {
+    protected: 
+    bool normalize(int &dividend, int &divisor) const{
+        bool positive;
+
+        if(dividend < 0 && divisor < 0){
+            positive = true;
+        } else if (dividend > 0 && divisor > 0){
+            positive = true;
+        } else {
+            positive = false;
+        }
+
+        if(dividend > 0){
+            dividend = 0 - dividend;
+        }
+        if(divisor > 0){
+            divisor = 0 - divisor;
+        }
+        return positive;
+    }
+};
+
+class Sub :  Divider{
+public:
+    int divide(int dividend, int divisor){
+        int quotient = 0;
+
+        int positive = normalize(dividend, divisor);
+
+        while(dividend <= divisor){
+            quotient--;
+            dividend -= divisor;
+        }
+
+        if(positive){
+            quotient = 0 - quotient;
+        }
+
+        return quotient;
+    }
+};
+
+class PowerSub : Divider {
+public:
+    int divide(int dividend, int divisor){
+        int quotient = 0;
+
+        int positive = normalize(dividend, divisor);
+
+        int curFits = -1;
+        int baseDivisor = divisor;
+        while(dividend <= divisor){
+            if(divisor < (INT_MIN >> 1)){
+                quotient += curFits;
+                curFits = -1;
+                dividend -= divisor;
+                divisor = baseDivisor;
+                continue;
+            }
+            int nextDivisor = divisor + divisor;
+            if(nextDivisor < dividend){       
+                quotient += curFits;
+                curFits = -1;
+                dividend -= divisor; 
+                divisor = baseDivisor;
+            } else {
+                curFits += curFits;
+                divisor = nextDivisor;
+            }
+        }
+
+        if(positive) {
+            quotient = 0 - quotient;
+        }
+
+        return quotient;
+    }
+};
+
+
+class Solution {
+    
+public:
+    int divide(int dividend, int divisor) {
+        //represent number in base divisor and extrapolate. 
+        
+        if(dividend == INT_MIN && divisor == -1){
+            return INT_MAX;
+        }
+
+        return PowerSub().divide(dividend, divisor);
+    }
+};
+
