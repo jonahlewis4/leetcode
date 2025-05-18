68fab26842aad14921f95f15256f435dfae34630
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-05-17 22:35:59 -0700

    Time: 488 ms (9.35%), Space: 39.2 MB (12.69%) - LeetHub

--- 0689-maximum-sum-of-3-non-overlapping-subarrays/0689-maximum-sum-of-3-non-overlapping-subarrays.cpp
+++ 0689-maximum-sum-of-3-non-overlapping-subarrays/0689-maximum-sum-of-3-non-overlapping-subarrays.cpp
@@ -14,49 +14,45 @@ public:
                 kSum -= nums[i + 1 - k];
             }
         }
-        vector<int> largestFromI(sizes.size());
-        largestFromI[largestFromI.size() - 1] = sizes.size() - 1;
-        for(int i = largestFromI.size() - 2; i >= 0; i--){
-            int thisSize = sizes[i];
-            int runningSize = sizes[largestFromI[i + 1]];
-            int largestIdx = -1;
-            if(thisSize >= runningSize){
-                largestIdx = i;
+
+        vector<vector<int>> dp(nums.size() - k + 1, vector<int>(3, -1));
+
+
+        function<int(int, int)> maxSumFrom;
+        maxSumFrom = [sizes, nums, k, &maxSumFrom, &dp](int start, int splitNumber) -> int {
+            if(start + k > nums.size() || splitNumber >= 3){
+                return 0;
+            }
+            
+            if(dp[start][splitNumber] != -1){
+                return dp[start][splitNumber];
+            }
+            //use this one or skip it
+
+            int use = sizes[start] + maxSumFrom(start + k, splitNumber + 1);
+            int skip = maxSumFrom(start + 1, splitNumber);
+            dp[start][splitNumber] = max(use, skip);
+            return max(use, skip);
+        };
+
+
+        vector<int> starts;
+        int i = 0; 
+        while(i <= nums.size() - k && starts.size() < 3){
+            int use = sizes[i] + maxSumFrom(i + k, starts.size() + 1);
+            int skip = maxSumFrom(i + 1, starts.size());
+
+            if(use >= skip) {
+                starts.push_back(i);
+                i += k;
             } else {
-                largestIdx = largestFromI[i + 1];
+                i++;
             }
-            largestFromI[i] = largestIdx;
-        }
-
-        //find the 2 closest for every non overlapping
-
-        int maxSum = INT_MIN;
-        vector<int> res;
-        for(int firstStart = 0; firstStart < sizes.size(); firstStart++){
-            int firstSize = sizes[firstStart];
-            for(int secondStart = firstStart + k; secondStart < sizes.size(); secondStart++){
-                int secondSize = sizes[secondStart];
 
 
-                int firstThirdStart = secondStart + k;
-                if(firstThirdStart >= sizes.size()){
-                    continue;
-                }
-                int thirdStart = largestFromI[firstThirdStart];
-                if(thirdStart >= sizes.size()){
-                    continue;
-                }
-                int thirdSize = sizes[thirdStart];
-                int sum = firstSize + secondSize + thirdSize;
-                if(sum > maxSum){
-                    res = {firstStart, secondStart, thirdStart};
-                    maxSum = sum;
-                }
-            }
-        }
-
-        return res;
-
+        }        
+        
+        return starts;
 
     }
 };
