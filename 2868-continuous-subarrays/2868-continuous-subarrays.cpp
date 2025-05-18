a8b8f6cc3667ee619bff7732f66eefec16ea288a
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-05-17 23:56:51 -0700

    Time: 125 ms (38.24%), Space: 115.9 MB (57.82%) - LeetHub

--- 2868-continuous-subarrays/2868-continuous-subarrays.cpp
+++ 2868-continuous-subarrays/2868-continuous-subarrays.cpp
@@ -32,14 +32,14 @@ class Solution {
             return total;
         }
     };
-    class SW{
+    class Multiset{
     private:
         const vector<int> &nums;
         int dist(int a, int b) const {
             return abs(a - b);
         }
     public:
-        SW(const vector<int> &nums) : nums(nums) {};
+        Multiset(const vector<int> &nums) : nums(nums) {};
         long long solution() const {
             int l = 0;
             int r = 0;
@@ -78,8 +78,73 @@ class Solution {
             return total;
         }
     };
+    class Monotonic {
+    private:
+        const vector<int>& nums;
+    public:
+        Monotonic(const vector<int>& nums) : nums(nums){}
+        long long solution() const {
+            auto dist = [](int a, int b) {
+                return abs(a - b);
+            };
+
+            deque<int> smallest;
+            deque<int> largest;
+
+            int l = 0;
+            int r = 0;
+            long long total = 0;
+            while(r < nums.size()){
+                int lValue = nums[l];
+                int rValue = nums[r];
+
+                auto accept = [&total, &r, &l, rValue, &smallest, &largest, this](){
+                    total += r - l + 1;
+                    while(!smallest.empty() && nums[smallest.back()] >= rValue){
+                        smallest.pop_back();
+                    }
+                    smallest.push_back(r);
+                    while(!largest.empty() && nums[largest.back()] <= rValue) {
+                        largest.pop_back();
+                    }
+                    largest.push_back(r);
+                    r++;
+                };
+                
+                if(smallest.empty() || largest.empty()){
+                    accept();
+                    continue;
+                }
+                
+                int smallestVal = nums[smallest.front()];
+                int largestVal = nums[largest.front()];
+
+                if(dist(rValue, smallestVal) <= 2 && dist(rValue, largestVal) <= 2){
+                    accept();
+                    continue;
+                }
+
+                //move until smallest and largest are within r value.
+                while(!smallest.empty() && dist(nums[smallest.front()], rValue) > 2){
+                    l = smallest.front() + 1;
+                    smallest.pop_front();
+                }
+
+                while(!largest.empty() && dist(nums[largest.front()], rValue) > 2){
+                    l = max(l, largest.front() + 1);
+                    largest.pop_front();
+                }
+
+                
+
+
+            }
+            return total;
+        }
+
+    };
 public:
     long long continuousSubarrays(vector<int>& nums) {
-        return SW(nums).solution();
+        return Monotonic(nums).solution();
     }
 };
