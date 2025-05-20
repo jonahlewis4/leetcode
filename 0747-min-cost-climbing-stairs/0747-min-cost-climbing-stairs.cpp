0b7dd6c88ff1e30ec1f86c85a55f33379e83f942
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-05-20 01:29:18 +0000

    Time: 0 ms (100%), Space: 17.5 MB (88.5%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -1,89 +1,15 @@
+#include <algorithm>
 class Solution {
-    class recursive{
-        vector<int> cost;
-    public:
-        recursive(const vector<int> & _cost)  : cost(_cost){
-        }
-        int Solution() {
-            this->cost.push_back(0);
-            return recurse(cost.size() - 1);
-        }
-        int recurse(int n) const {
-            if(n < 0){
-                return INT_MAX; 
-            }
-
-            if(n == 0){
-                return cost[0];
-            }
-            if(n == 1){
-                return cost[1];
-            }
-
-            int oneStep = recurse(n - 1);
-            int twoStep = recurse(n - 2);
-
-            return cost[n] + min(oneStep, twoStep);
-        }
-    };
-    class memo{
-        vector<int> cache;
-        const vector<int> &cost;
-    public:
-        memo(const vector<int> & _cost)  : cost(_cost){
-            cache.resize(_cost.size() + 1, -1);
-        }
-        int Solution() {
-            return recurse(cost.size());
-        }
-        int recurse(int n){
-            if(n < 0){
-                return INT_MAX; 
-            }
-
-            if(n == 0){
-                return cost[0];
-            }
-            if(n == 1){
-                return cost[1];
-            }
-
-            if(cache[n] != -1){
-                return cache[n];
-            }
-
-            int oneStep = recurse(n - 1);
-            int twoStep = recurse(n - 2);
-
-            int curCost = n < cost.size() ? cost[n] : 0;
-            int res = curCost + min(oneStep, twoStep);
-            cache[n] = res;
-            return res;
-        }
-
-    };
-
-    class dp{
-        public:
-        const vector<int> &cost ;
-        dp(const vector<int> & cost) : cost(cost) {
-
-        }
-        int Solution(){
-            int twoAgo = cost[0];
-            int oneAgo = cost[1];
-            int cur;
-            for(int i = 2; i <= cost.size(); i++){
-                int curCost = i < cost.size() ? cost[i] : 0;
-                cur = min(twoAgo, oneAgo) + curCost;
-                twoAgo = oneAgo;
-                oneAgo = cur;
-            }
-            return cur;
-        }
-    };
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        return dp(cost).Solution();
+        int  oneAgo = 0;
+        int twoAgo = 0;
+        for(int i = cost.size() - 1; i >= 0; i--){
+            int lessExpensive = std::min(oneAgo, twoAgo);
+            int localCost = cost[i] + lessExpensive;
+            twoAgo = oneAgo;
+            oneAgo = localCost;
+        }
+        return std::min(oneAgo, twoAgo);
     }
 };
