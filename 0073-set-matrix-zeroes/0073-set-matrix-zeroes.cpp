184afede8b25d38a32e2431bd35a0b2a7a685537
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-05-21 00:06:51 +0000

    Time: 0 ms (100%), Space: 18.8 MB (12.91%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,39 +1,27 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        bool firstCol = false;
+        unordered_set<int> zeroRows;
+        unordered_set<int> zeroCols;
+
         for(int r = 0; r < matrix.size(); r++){
             for(int c = 0; c < matrix[r].size(); c++){
                 if(matrix[r][c] == 0){
-                    if(c == 0){
-                        firstCol = true;
-                    } else {
-                        matrix[0][c] = 0;
-                    }
-                    matrix[r][0] = 0;
+                    zeroRows.insert(r);
+                    zeroCols.insert(c);
                 }
             }
         }
 
-        for(int c = 1; c < matrix[0].size(); c++){
-            if(matrix[0][c] == 0){
-                for(int r = 1; r < matrix.size(); r++){
-                    matrix[r][c] = 0;
-                }
-            }
+
+        for(const auto & r : zeroRows) {
+            fill(matrix[r].begin(), matrix[r].end(), 0);
         }
-        for(int r = 0; r < matrix.size(); r++){
-            if(matrix[r][0] == 0){
-                for(int c = 1; c < matrix[r].size(); c++){
-                    matrix[r][c] = 0;
-                }
-            }
-        }
-        if(firstCol){
+
+        for(const auto & c : zeroCols) {
             for(int r = 0; r < matrix.size(); r++){
-                matrix[r][0] = 0;
+                matrix[r][c] = 0;
             }
         }
-
     }
 };
