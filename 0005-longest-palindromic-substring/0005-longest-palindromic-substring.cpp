ca46ccc84b22c25662449e9f3fe77ec5eaa8734f
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-05-22 00:41:15 +0000

    Time: 8 ms (78.93%), Space: 9.1 MB (99.14%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,43 +1,37 @@
 class Solution {
 public:
-    string longestPalindrome(const string &s) {
-        int oL = 0;
-        int oR = 0;
+    string longestPalindrome(const string& s) {
+        string longest = "";
+
+        int bestR = 0;
+        int bestL = 0;
+        const auto  find = [&longest, &s, &bestR, &bestL](int l, int r){
+            while(l >= 0 && r < s.size()){
+                if(s[l] != s[r]){
+                    break;
+                }
+                l--;
+                r++;
+            }
+
+            int palL = l + 1;
+            int palR = r - 1;
+
+            if(palR - palL > bestR - bestL){
+                bestR = palR;
+                bestL = palL;
+            }
+        };
 
         for(int i = 0; i < s.size(); i++){
-            const auto [l, r] = calc(i, i, s);
-
-            int len = r - l + 1;
-            if(len > oR - oL + 1){
-                oR = r;
-                oL = l;
-            } 
+            find(i, i);
         }
 
-        for(int i = 0; i + 1 < s.size(); i++){
-            const auto [l, r] = calc(i, i + 1, s);
-            int len = r - l + 1;
-            if(len > oR - oL + 1){
-                oR = r;
-                oL = l;
-            }
+        for(int i = 1; i < s.size(); i++){
+            find(i - 1, i);
         }
 
-        return s.substr(oL, oR - oL + 1);
-    }
-    tuple<int, int> calc(int l, int r, const string &s) const {
-        int oldL = 0;
-        int oldR = 0;
-        while(l >= 0 && r < s.size()){
-            if(s[l] != s[r]){
-                return {oldL, oldR};
-            }
-            oldL = l;
-            oldR = r;
+        return s.substr(bestL, bestR - bestL + 1);
 
-            l--;
-            r++;
-        }
-        return {oldL, oldR};
     }
 };
