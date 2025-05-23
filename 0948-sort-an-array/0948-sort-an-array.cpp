cd5b7e617a53154d6e6e2d4529ad6ef54ba4111a
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-05-23 22:55:24 +0000

    Time: 213 ms (55.61%), Space: 146 MB (44.68%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,16 +1,65 @@
 class Solution {
-public:
-    vector<int> sortArray(vector<int>& nums) {
-        std::priority_queue<int, vector<int>, std::greater<int>> pq;
-        for( const auto & num: nums){
-            pq.push(num);
+private:
+    //T is a random access iterator
+    template <typename T>
+    void mergesort(T begin, T end){
+        
+        if(next(begin) == end){
+            return;
         }
 
-        vector<int> res;
-        while(!pq.empty()){
-            res.push_back(pq.top());
-            pq.pop();
+
+        T back = prev(end);
+        int n = end - begin;
+        T m = begin + (end - begin) / 2;
+        int mOffset = end - m;
+        mergesort(begin, m);
+        mergesort(m, end);
+
+        //aggregate 
+        T l = end - n;
+        T lEnd = end - mOffset;
+        T r = lEnd;
+        T rEnd = end;
+
+        //iterator_traits<T>::value_type returns the type that the given iterator points to. 
+        using ValueType = iterator_traits<T>::value_type;
+        vector<ValueType> merged(end - begin);
+        
+        T mergeItr = merged.begin();
+
+        while(l != lEnd || r != rEnd) {
+            if(l == lEnd){
+                *mergeItr = *r;
+                r = next(r);
+            } else if(r == rEnd){
+                *mergeItr = *l;
+                l = next(l);
+            } else if (*l < *r){
+                *mergeItr = *l;
+                l = next(l);
+            } else {
+                *mergeItr = *r;
+                r = next(r);
+            }
+            mergeItr = next(mergeItr);
         }
-        return res;
+
+        //update original array to match the merged array;
+        T mergeBegin = merged.begin();
+        T destBegin = end - n;
+
+        while(mergeBegin != merged.end()){
+            *destBegin = *mergeBegin;
+            mergeBegin = next(mergeBegin);
+            destBegin = next(destBegin);
+        }
+
+        
+    }
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        mergesort(nums.begin(), nums.end());
+        return nums;
     }
 };
