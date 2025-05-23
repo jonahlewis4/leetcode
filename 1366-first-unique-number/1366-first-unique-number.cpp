e20b29eef55b4c35cf429168f81c75c8487362bc
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-05-23 21:58:36 +0000

    Time: 237 ms (31.79%), Space: 143.3 MB (18.93%) - LeetHub

---
+++ 1366-first-unique-number/1366-first-unique-number.cpp
@@ -0,0 +1,48 @@
+class FirstUnique {
+private:
+    list<int> unique;
+    unordered_map<int, list<int>::iterator> map;
+    bool isUnique(int value) const {
+        if(map.find(value) == map.end()){
+            return true;
+        }
+        return false;
+    }
+    bool occursOnce(int value) {
+        if(map.find(value)->second != unique.end()){
+            return true;
+        }
+        return false;
+    }
+public:
+    FirstUnique(vector<int>& nums) {
+        for(const auto & num : nums){
+            add(num);
+        }
+    }
+    
+    int showFirstUnique() {
+        if(unique.begin() == unique.end()){
+            return -1;
+        }
+        return *unique.begin();
+    }
+    
+    void add(int value) {
+        if(isUnique(value)){
+            unique.push_back(value);
+            map[value] = prev(unique.end());
+        } else if (occursOnce(value)) {
+            list<int>::iterator it = map[value];
+            unique.erase(it);
+            map[value] = unique.end();
+        }   
+    }
+};
+
+/**
+ * Your FirstUnique object will be instantiated and called as such:
+ * FirstUnique* obj = new FirstUnique(nums);
+ * int param_1 = obj->showFirstUnique();
+ * obj->add(value);
+ */
