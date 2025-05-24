32750941d32b3ff30c65c3c6ae3b780af2ca1d3c
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-05-24 03:18:38 +0000

    Time: 160 ms (57.21%), Space: 123.6 MB (45.24%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,50 +1,57 @@
 class Solution {
 private:
-    //T is a random access iterator
-    void mergesort(int l, int r, vector<int>& nums){
-        if(l == r){
-            return;
-        }  
-
-        int m = (l + r) / 2;
-        mergesort(l, m, nums);
-        mergesort(m + 1, r, nums);
-
-        vector<int> merged(r - l + 1);
-        int mergeI = 0;
-
-        int rightI = m + 1;
-        int leftI = l;
-
-        while(leftI <= m || rightI <= r){
-            if(leftI > m){
-                merged[mergeI] = nums[rightI];
-                rightI++;
-            } else if(rightI > r){
-                merged[mergeI] = nums[leftI];
-                leftI++;
-            } else if (nums[leftI] <= nums[rightI]){
-                merged[mergeI] = nums[leftI];
-                leftI++;
-            } else {
-                merged[mergeI] = nums[rightI];
-                rightI++;
-            }
-            mergeI++;
-        }
-
-        int ogI = l;
-        for(int i = 0; i < merged.size(); i++){
-            nums[ogI] = merged[i];
-            ogI++;
-        }
-
-
-
-    }
+    
 public:
     vector<int> sortArray(vector<int>& nums) {
-        mergesort(0, nums.size() - 1, nums);
+        for(int size = 1; size <= nums.size(); size *= 2){
+            int sub1 = 0;
+            int sub2 = sub1 + size;
+
+            int sub1End = sub1 + size - 1;
+            int sub2End = sub2 + size - 1;
+
+
+            while(sub1 < nums.size()){
+                if(sub1End >= nums.size()){
+                    sub1End = nums.size() - 1;
+                }
+                if(sub2End >= nums.size()){
+                    sub2End = nums.size() - 1;
+                }
+                int sub1Size = sub1End - sub1 + 1;
+                int sub2Size = max(0, sub2End - sub2 + 1);
+                vector<int> merged(sub1Size + sub2Size);
+
+                int mergeI = 0;
+                int l = sub1;
+                int r = sub2;
+                while(l <= sub1End || r <= sub2End) {
+                    if(l > sub1End){
+                        merged[mergeI] = nums[r];
+                        r++;
+                    } else if (r > sub2End) {
+                        merged[mergeI] = nums[l];
+                        l++;
+                    } else if (nums[l] < nums[r]){
+                        merged[mergeI] = nums[l];
+                        l++;
+                    } else {
+                        merged[mergeI] = nums[r];
+                        r++;
+                    }
+                    mergeI++;
+                }
+
+                for(int i = 0; i < merged.size(); i++){
+                    nums[sub1 + i] = merged[i];
+                }
+
+                sub1 += size * 2;
+                sub2 += size * 2;
+                sub1End += size * 2;
+                sub2End += size * 2;
+            }
+        }
         return nums;
     }
 };
