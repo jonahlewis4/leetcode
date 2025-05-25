6f3420ee28c6b9f2ac2b9745c14586c58ffa079d
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-05-25 16:15:17 -0700

    Time: 44 ms (87.5%), Space: 55.3 MB (75%) - LeetHub

--- 3860-resulting-string-after-adjacent-removals/3860-resulting-string-after-adjacent-removals.cpp
+++ 3860-resulting-string-after-adjacent-removals/3860-resulting-string-after-adjacent-removals.cpp
@@ -22,46 +22,15 @@ public:
             return s;
         }
 
-        std::list<char> list;
-        for(const auto & c : s){
-            list.push_back(c);
-        }
-
-        using It = decltype(list.begin());
-
-        It l = list.begin();
-        It r = next(list.begin());  
-        
-        while(r != list.end()){
-            if(consec(*l, *r)){
-                It newL;
-                It newR;
-                if(l == list.begin()){
-                    newL = next(r);
-                    if(newL == list.end()){
-                        newR = list.end();
-                    } else {
-                        newR = next(newL);
-                    }
-                } else {
-                    newL = prev(l);
-                    newR = next(r);
-                }
-                list.erase(l);
-                list.erase(r);
-                l = newL;
-                r = newR;
+        string res = "";
+        for(int i = 0; i < s.size(); i++){
+            if(res.empty() || !consec(res.back(), s[i])){
+                res.push_back(s[i]);
             } else {
-                l = next(l);
-                r = next(r);
+                res.pop_back();
             }
         }
-        string res = "";
-
-        for(It itr = list.begin(); itr != list.end(); itr++){
-            res.push_back(*itr);
-        }
-
         return res;
+
     }
 };
