655602fd81c4852bbc855fd5ee0e3dc156308fbf
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-05-24 22:32:19 -0700

    Time: 190 ms (11.93%), Space: 189 MB (5.76%) - LeetHub

---
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+        int longest = 0;
+
+
+        unordered_map<string, int> map;
+
+
+
+        //if pair in map : 
+            //decrease pair count and DON'T add this guy.
+            //also increase the size of longest by 4
+        // otherwise increase count of the current word.
+
+        for(const auto & word : words){
+            string pair = word;
+            reverse(pair.begin(), pair.end());
+
+            if(map.find(pair) != map.end()){
+                map[pair]--;
+                if(map[pair] == 0){
+                    map.erase(pair);
+                }
+                longest += 4;
+            } else {
+                map[word]++;
+            }
+
+        }
+
+
+
+        //final loop through and see if any of the words which have NOT been used yet are palindromes. If there is one,
+        //it can go at the center of the result and so we increase longest by 2. 
+
+        for(auto [key, _] : map) {
+            string reversed = key;
+            reverse(reversed.begin(), reversed.end());
+            if(key == reversed){
+                longest += 2;
+                break;
+            }
+        }
+
+        return longest;
+    }
+};
