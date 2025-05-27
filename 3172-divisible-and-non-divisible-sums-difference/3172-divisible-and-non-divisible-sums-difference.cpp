6b2c4f1250db3ec8181aef00e06850cc6f8205fa
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-05-26 17:11:55 -0700

    Time: 0 ms (100%), Space: 8.4 MB (36.04%) - LeetHub

--- 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.cpp
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.cpp
@@ -1,14 +1,7 @@
 class Solution {
 public:
     int differenceOfSums(int n, int m) {
-        int diff = 0;
-        for(int i = 1; i <= n; i++){
-            if(i % m == 0){
-                diff -= i;
-            } else {
-                diff += i;
-            }
-        }
-        return diff;
+        int k = n / m;
+        return n * (n + 1) / 2 - k * (k + 1) * m;
     }
 };
