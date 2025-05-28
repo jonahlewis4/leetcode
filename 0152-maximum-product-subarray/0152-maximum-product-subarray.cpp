52c2fd8a8d3087bf59f8aa812eab833dfadac241
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-05-27 18:35:28 -0700

    Time: 0 ms (100%), Space: 17.8 MB (57.04%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int smallestNeg = 1;
-        int largestPos = 1;
+        int negative = 1;
+        int positive = 1;
 
         int res = INT_MIN;
-
         for(const auto & num : nums){
-            int oldLargest = largestPos;
-            largestPos = max({smallestNeg * num, largestPos * num, num});
-            smallestNeg = min({smallestNeg * num, oldLargest * num, num});
-            res = max(largestPos, res);
-
-    
+            int oldPos = positive;
+            positive = max({num * oldPos, num * negative, num});
+            negative = min({num * oldPos, num * negative, num});
+            res = max(res, positive);
         }
-
         return res;
-
-
     }
 };
