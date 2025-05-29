681e543f58bc1d71978089dd9aaeabb5c5d6acba
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-05-28 18:20:10 -0700

    Time: 299 ms (79.59%), Space: 341.9 MB (54.42%) - LeetHub

--- 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii.cpp
+++ 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii.cpp
@@ -1,70 +1,40 @@
 class Solution {
-private:
-    enum color{
-        NOT_YET_COLORED = -1,
-        BLACK = 0,
-        WHITE = 1,
-    };
-    tuple<vector<color>, int, int> nonAdjacent(const vector<vector<int>>& adjList) const {
-        //returns two sets.
-        //the sets contain all nodes which are not adjacent to each other based on the adjacency list.
-
-        
-
-        bool first = true;
-        vector<color> colors(adjList.size(), NOT_YET_COLORED);
-        int blackCount = 0;
-        int whiteCount = 0;
-        dfs(adjList, true, colors, 0, blackCount, whiteCount);
-        return {colors, blackCount, whiteCount};
-    }
-    void dfs(const vector<vector<int>>&adjList, bool black, vector<color>& colors, int node, int& blackCount, int& whiteCount) const {
-        if(colors[node] != NOT_YET_COLORED){
-            return;
-        }
-
-        color color = black ? BLACK : WHITE;
-        blackCount = black ? blackCount + 1 : blackCount;
-        whiteCount = !black ? whiteCount + 1 : whiteCount;
-        colors[node] = color;
-        
-        for(const auto & neigh : adjList[node]){
-            dfs(adjList, !black, colors, neigh, blackCount, whiteCount);
-        }
-    }
-    vector<vector<int>> getAdjList(vector<vector<int>>& edges) const {
-        int n = edges.size() + 1;
-        vector<vector<int>> adjList(n);
-        for(const auto edge : edges) {
-            int src = edge[0];
-            int dest = edge[1];
-            adjList[src].push_back(dest);
-            adjList[dest].push_back(src);
-        }
-        return adjList;
-    }
 public:
-    vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2) const {
-        const auto adjList1 = getAdjList(edges1);
-        const auto adjList2 = getAdjList(edges2);
-        
-        vector<int> res(edges1.size() + 1);
-
-        auto const& [colors, blackCount, whiteCount] = nonAdjacent(adjList1);
-        auto const& [_, blackCount2, whiteCount2] = nonAdjacent(adjList2);
-
-        int bestBranchAcceptor = max(blackCount2, whiteCount2);
-
-        for(int i = 0; i < adjList1.size(); i++){
-            int nonAdjacent;
-            if(colors[i] == BLACK){
-                nonAdjacent = blackCount;
-            } else {
-                nonAdjacent = whiteCount;
+    int dfs(int node, int parent, int depth,
+            const vector<vector<int>>& children, vector<int>& color) {
+        int res = 1 - depth % 2;
+        color[node] = depth % 2;
+        for (int child : children[node]) {
+            if (child == parent) {
+                continue;
             }
-            res[i] = bestBranchAcceptor + nonAdjacent;
+            res += dfs(child, node, depth + 1, children, color);
         }
+        return res;
+    }
 
+    vector<int> build(const vector<vector<int>>& edges, vector<int>& color) {
+        int n = edges.size() + 1;
+        vector<vector<int>> children(n);
+        for (const auto& edge : edges) {
+            children[edge[0]].push_back(edge[1]);
+            children[edge[1]].push_back(edge[0]);
+        }
+        int res = dfs(0, -1, 0, children, color);
+        return {res, n - res};
+    }
+
+    vector<int> maxTargetNodes(vector<vector<int>>& edges1,
+                               vector<vector<int>>& edges2) {
+        int n = edges1.size() + 1, m = edges2.size() + 1;
+        vector<int> color1(n);
+        vector<int> color2(m);
+        vector<int> count1 = build(edges1, color1);
+        vector<int> count2 = build(edges2, color2);
+        vector<int> res(edges1.size() + 1);
+        for (int i = 0; i < n; i++) {
+            res[i] = count1[color1[i]] + max(count2[0], count2[1]);
+        }
         return res;
     }
 };
