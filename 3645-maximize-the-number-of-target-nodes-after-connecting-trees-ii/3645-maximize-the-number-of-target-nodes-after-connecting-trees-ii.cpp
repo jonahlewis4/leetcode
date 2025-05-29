218c6c17f2c2b078dfeeca0d1deab1b345bb114b
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-05-28 18:09:39 -0700

    Time: 414 ms (37.41%), Space: 368.1 MB (30.61%) - LeetHub

--- 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii.cpp
+++ 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii.cpp
@@ -1,33 +1,37 @@
 class Solution {
 private:
-    tuple<unordered_set<int>, unordered_set<int>> nonAdjacent(const vector<vector<int>>& adjList) const {
+    enum color{
+        NOT_YET_COLORED = -1,
+        BLACK = 0,
+        WHITE = 1,
+    };
+    tuple<vector<color>, int, int> nonAdjacent(const vector<vector<int>>& adjList) const {
         //returns two sets.
         //the sets contain all nodes which are not adjacent to each other based on the adjacency list.
 
-        unordered_set<int> firstNonAdjacent;
-        unordered_set<int> secondNonAdjacent;
+        
 
         deque<int> q;
         q.push_back(0);
 
         bool first = true;
+        vector<color> colors(adjList.size(), NOT_YET_COLORED);
         while(!q.empty()){
-            int n = q.size();
-            unordered_set<int>& group = first ? firstNonAdjacent : secondNonAdjacent;
-            
+            int n = q.size();            
+            color color = first ? BLACK : WHITE;
             for(int i = 0; i < n; i++){
                 int node = q.front();
                 q.pop_front();
 
+                
 
-
-                if(firstNonAdjacent.count(node) || secondNonAdjacent.count(node)){
+                if(colors[node] != NOT_YET_COLORED){
                     continue;
                 }
 
-                group.insert(node);
+                colors[node] = color;
                 for(const auto & neigh : adjList[node]){
-                    if (firstNonAdjacent.count(neigh) || secondNonAdjacent.count(neigh)) {
+                    if (colors[neigh] != NOT_YET_COLORED) {
                         continue;  
                     }
                     q.push_back(neigh);
@@ -36,7 +40,16 @@ private:
             first = !first;
 
         }
-        return {firstNonAdjacent, secondNonAdjacent};
+        int whiteCount = 0;
+        int blackCount = 0;
+        for(const auto & color : colors ){
+            if(color == BLACK){
+                blackCount++;
+            } else {
+                whiteCount++;
+            }
+        }
+        return {colors, blackCount, whiteCount};
     }
 
     vector<vector<int>> getAdjList(vector<vector<int>>& edges) const {
@@ -57,23 +70,21 @@ public:
         
         vector<int> res(edges1.size() + 1);
 
-        auto const& [first1, second1] = nonAdjacent(adjList1);
-        auto const& [first2, second2] = nonAdjacent(adjList2);
+        auto const& [colors, blackCount, whiteCount] = nonAdjacent(adjList1);
+        auto const& [_, blackCount2, whiteCount2] = nonAdjacent(adjList2);
 
-        int bestBranchAcceptor = max(first2.size(), second2.size());
+        int bestBranchAcceptor = max(blackCount2, whiteCount2);
 
         for(int i = 0; i < adjList1.size(); i++){
             int nonAdjacent;
-            if(first1.count(i)){
-                nonAdjacent = first1.size();
+            if(colors[i] == BLACK){
+                nonAdjacent = blackCount;
             } else {
-                nonAdjacent = second1.size();
+                nonAdjacent = whiteCount;
             }
             res[i] = bestBranchAcceptor + nonAdjacent;
         }
 
         return res;
-        
-        
     }
 };
