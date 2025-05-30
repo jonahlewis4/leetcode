c416db9e4452a8b75cac5ebcb232dda942c3d9fc
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-05-30 00:49:16 -0700

    Time: 0 ms (100%), Space: 94.5 MB (99.5%) - LeetHub

--- 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.cpp
+++ 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.cpp
@@ -1,86 +1,49 @@
 class Solution {
 public:
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
-        vector<vector<int>> adjList(edges.size());
 
-        for(int i = 0; i < edges.size(); i++){
-            if(edges[i] == -1){
-                continue;
-            }
-            adjList[i].push_back(edges[i]);
-        }
-
-        queue<int> q1;
-        queue<int> q2;
+        int p1;
+        int p2;
         vector<bool> visit1(edges.size(), false);
         vector<bool> visit2(edges.size(), false);
-        q1.push(node1);
-        q2.push(node2);
-        while(!q1.empty() || !q2.empty()){
-            vector<int> neigh1;
-            vector<int> neigh2;
-
-            while(!q1.empty() || !q2.empty()){
-                if(!q1.empty()){
-                    int top = q1.back();
-                    q1.pop();
-                    //might not need this check
-                    if(visit1[top]){
-                        continue;
-                    }
-                    if(visit2[top]){
-                        if(!q2.empty()){
-                            int top2 = q2.back();
-                            if(visit1[top2]){
-                                if(top2 < top){
-                                    return top2;
-                                }
+        p1 = node1;
+        p2 = node2;
+        while(p1 != -1 || p2 != -1){
+            if(p1 != -1){
+                int top = p1;
+                //might not need this check
+                if(visit1[top]){
+                    p1 = -1;
+                    continue;
+                }
+                if(visit2[top]){
+                    if(p2 != -1){
+                        int top2 = p2;
+                        if(visit1[top2]){
+                            if(top2 < top){
+                                return top2;
                             }
                         }
-                        return top;
                     }
-                    visit1[top] = true;
-                    for(const auto & neigh : adjList[top]){
-                        if(visit1[neigh]){
-                            continue;
-                        }
-                        neigh1.push_back(neigh);
-                    }
+                    return top;
                 }
-
-
-                if(!q2.empty()){
-                    int top = q2.back();
-                    q2.pop();
-
-                    if(visit2[top]){
-                        continue;
-                    }
-                    if(visit1[top]){
-                        return top;
-                    }
-                    visit2[top] = true;
-                    for(const auto & neigh : adjList[top]){
-                        if(visit2[neigh]){
-                            continue;
-                        }
-                        neigh2.push_back(neigh);
-                    }
-                }
+                visit1[top] = true;
+                p1 = edges[top];
             }
 
-            for(const auto & queued : neigh1 ){
-                if(visit1[queued]){
-                    continue;
-                }
-                q1.push(queued);
-            }
 
-            for(const auto & queued : neigh2) {
-                if(visit2[queued]) {
+            if(p2 != -1){
+                int top = p2;
+
+                if(visit2[top]){
+                    p2 = -1;
                     continue;
                 }
-                q2.push(queued);
+                if(visit1[top]){
+                    return top;
+                }
+                visit2[top] = true;
+                p2 = edges[top];
             }
         }
         return -1;
