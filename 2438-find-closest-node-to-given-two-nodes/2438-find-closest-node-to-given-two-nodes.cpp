88040bfc83574b1b20a5eac65c1ba57d3f8014a1
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-05-30 00:38:32 -0700

    Time: 169 ms (15.83%), Space: 155.3 MB (18%) - LeetHub

---
+++ 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.cpp
@@ -0,0 +1,88 @@
+class Solution {
+public:
+    int closestMeetingNode(vector<int>& edges, int node1, int node2) {
+        vector<vector<int>> adjList(edges.size());
+
+        for(int i = 0; i < edges.size(); i++){
+            if(edges[i] == -1){
+                continue;
+            }
+            adjList[i].push_back(edges[i]);
+        }
+
+        priority_queue<int, vector<int>, greater<int>> q1;
+        priority_queue<int, vector<int>, greater<int>> q2;
+        vector<bool> visit1(edges.size(), false);
+        vector<bool> visit2(edges.size(), false);
+        q1.push(node1);
+        q2.push(node2);
+        while(!q1.empty() || !q2.empty()){
+            vector<int> neigh1;
+            vector<int> neigh2;
+
+            while(!q1.empty() || !q2.empty()){
+                if(!q1.empty()){
+                    int top = q1.top();
+                    q1.pop();
+                    //might not need this check
+                    if(visit1[top]){
+                        continue;
+                    }
+                    if(visit2[top]){
+                        if(!q2.empty()){
+                            int top2 = q2.top();
+                            if(visit1[top2]){
+                                if(top2 < top){
+                                    return top2;
+                                }
+                            }
+                        }
+                        return top;
+                    }
+                    visit1[top] = true;
+                    for(const auto & neigh : adjList[top]){
+                        if(visit1[neigh]){
+                            continue;
+                        }
+                        neigh1.push_back(neigh);
+                    }
+                }
+
+
+                if(!q2.empty()){
+                    int top = q2.top();
+                    q2.pop();
+
+                    if(visit2[top]){
+                        continue;
+                    }
+                    if(visit1[top]){
+                        return top;
+                    }
+                    visit2[top] = true;
+                    for(const auto & neigh : adjList[top]){
+                        if(visit2[neigh]){
+                            continue;
+                        }
+                        neigh2.push_back(neigh);
+                    }
+                }
+            }
+
+            for(const auto & queued : neigh1 ){
+                if(visit1[queued]){
+                    continue;
+                }
+                q1.push(queued);
+            }
+
+            for(const auto & queued : neigh2) {
+                if(visit2[queued]) {
+                    continue;
+                }
+                q2.push(queued);
+            }
+        }
+        return -1;
+    }
+};
