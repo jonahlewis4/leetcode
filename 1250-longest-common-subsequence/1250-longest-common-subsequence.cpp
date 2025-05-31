6fa741ad095683925259743ae134f834782a4799
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-05-30 18:08:40 -0700

    Time: 91 ms (5.05%), Space: 28.9 MB (6.36%) - LeetHub

---
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -0,0 +1,75 @@
+class Solution {
+
+    class Recursive{
+        const string& text1;
+        const string& text2;
+    public:
+        Recursive(const string& text1, const string& text2) : text1(text1), text2(text2) {
+
+        }
+
+        int Solution(){
+            return recurse(0, 0);
+        }
+        int recurse(int i1, int i2){
+            if(i1 >= text1.size() || i2 >= text2.size()){
+                return 0;
+            }
+
+            if(text1[i1] == text2[i2]){
+                int use = 1 + recurse(i1 + 1, i2 + 1);
+                return use;
+            }
+
+            int skip1 = recurse(i1 + 1, i2);
+            int skip2 = recurse(i1, i2 + 1);
+            return max({skip1, skip2});
+        }
+    };
+
+    class Memo {
+        const string& text1;
+        const string& text2;
+
+        vector<vector<int>> dp;
+    public:
+        Memo(const string& text1, const string& text2) : text1(text1), text2(text2) {
+            dp.resize(text1.size(), vector<int>(text2.size(), -1));
+        }
+
+        int Solution() {
+            return recurse(0, 0);
+        }
+
+        int recurse(int i1, int i2) {
+            if(i1 >= text1.size() || i2 >= text2.size()){
+                return 0;
+            }
+
+            if(dp[i1][i2] != -1){
+                return dp[i1][i2];
+            }
+
+
+
+            if(text1[i1] == text2[i2]){
+                int use = 1 + recurse(i1 + 1, i2 + 1);
+                dp[i1][i2] = use;
+                return use;
+            }
+
+            int skip1 = recurse(i1 + 1, i2);
+            int skip2 = recurse(i1, i2 + 1);
+            dp[i1][i2] = max({skip1, skip2});
+            return max({skip1, skip2});
+        }
+    };
+
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        return Memo(text1, text2).Solution();
+    }
+
+
+
+};
