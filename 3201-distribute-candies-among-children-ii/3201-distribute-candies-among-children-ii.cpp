a5c9cd6a8c15c3d8c85c27045a7f0994b8c1ccb7
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-05-31 21:26:11 -0700

    Time: 36 ms (12.35%), Space: 8.8 MB (84.71%) - LeetHub

--- 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.cpp
+++ 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.cpp
@@ -5,10 +5,6 @@ public:
         for(int i = 0; i <= min(limit, n); i++){
             int remaining = n - i;
 
-            if(remaining < 0){
-                break;
-            }
-
             int combs = remaining + 1 - max(0, remaining - limit) * 2;
             ans += max(combs, 0);
         }
