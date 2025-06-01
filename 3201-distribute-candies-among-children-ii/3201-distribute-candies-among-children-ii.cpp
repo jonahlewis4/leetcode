fafcb5741f3029d3ef0ef193a8913d6e72f57c67
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-05-31 21:51:42 -0700

    Time: 25 ms (52.35%), Space: 9.1 MB (18.82%) - LeetHub

--- 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.cpp
+++ 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.cpp
@@ -3,10 +3,28 @@ public:
     long long distributeCandies(int n, int limit) {
         long long ans = 0;
         for(int i = 0; i <= min(limit, n); i++){
-            int remaining = n - i;
+            int m = n - i;
 
-            int combs = remaining + 1 - max(0, remaining - limit) * 2;
-            ans += max(combs, 0);
+            if(m <= limit) {
+                ans += m + 1;
+                continue;
+            }
+
+            int upperRemoved = m - limit;
+            int lowerRemoved = m - limit;
+            if(upperRemoved > (m + 1) / 2) {
+                upperRemoved = (m + 1) / 2;
+            }
+            if(lowerRemoved > (m + 1) / 2){
+                if((m + 1) % 2 == 1){
+                    lowerRemoved = (m + 1) / 2 + 1;
+                } else {
+                    lowerRemoved = (m + 1) / 2;
+                }
+            }
+            int additions = m + 1 - lowerRemoved - upperRemoved;
+            ans += additions;
+            
         }
         return ans;
     }
