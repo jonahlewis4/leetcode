056925fb3e161b07457426ffad829c854dd77ab9
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-06-01 16:51:41 -0700

    Time: 6 ms (82.85%), Space: 9.8 MB (99.33%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,32 +1,21 @@
 class Solution {
-    private:
-
-    int calc(int amount,const vector<int> &coins, const vector<vector<int>> &dp, int curAmount, int coinI) const {
-            if(amount == curAmount){
-                return 1;
-            }
-            if(curAmount > amount){
-                return 0;
-            }
-            if(coinI >= coins.size()){
-                return 0;
-            }
-            return dp[curAmount][coinI];
-        };
 public:
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>> dp(amount + 1, vector<int>(coins.size(), 0));
-        for(int amountI = dp.size() - 1; amountI >= 0; amountI--){
-            for(int coinI = dp[0].size() - 1; coinI >= 0; coinI--){
-                int useCoin = calc(amount, coins, dp, amountI + coins[coinI], coinI);
-                int skipCoin = calc(amount, coins, dp, amountI, coinI + 1);
-                if((long long) useCoin + skipCoin > INT_MAX){
-                    continue;
+        vector<int> dp(amount + 1, 0);
+        dp.back() = 1;
+        for(const auto & coin : coins) {
+            for(int amt = amount - coin; amt >= 0; amt--){
+                int cur = dp[amt];
+                int after = dp[amt + coin];
+
+                long long sum = (long long)cur + after;
+                if(sum > INT_MAX){
+                    sum = 0;
                 }
-                dp[amountI][coinI] = useCoin + skipCoin;
+                dp[amt] = sum;
             }
         }
 
-        return dp[0][0];
+        return dp[0];
     }
 };
