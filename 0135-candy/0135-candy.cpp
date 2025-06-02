b69f8bceecb57bf6a3e4ebe1485f31deda85b62e
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-06-01 20:48:09 -0700

    Time: 0 ms (100%), Space: 22.7 MB (73.25%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,50 +1,23 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        struct child {
-            int indegree = 0;
-            vector<int> outgoing;
-            int candies = 1;
-        };
-
-        vector<child> children(ratings.size());
-        for(int i = 0; i < ratings.size(); i++){
-            //outgoing are the ones which are larger
-            if(i - 1 >= 0 && ratings[i - 1] > ratings[i]) {
-                children[i].outgoing.push_back(i - 1);
-                children[i - 1].indegree++;
-            }
-
-            if(i + 1 < ratings.size() && ratings[i + 1] > ratings[i]){
-                children[i].outgoing.push_back(i + 1);
-                children[i + 1].indegree++;
+        vector<int> candies(ratings.size(), 1);
+        for(int i = 1; i < candies.size(); i++){
+            if(ratings[i] > ratings[i - 1]){
+                candies[i] = candies[i - 1] + 1;
             }
         }
 
-        queue<int> q;
-        for(int i = 0; i < children.size(); i++){
-            if(children[i].indegree == 0){
-                q.push(i);
+        for(int i = candies.size() - 2; i >= 0; i--){
+            if(ratings[i] > ratings[i + 1]){
+                candies[i] = max(candies[i + 1] + 1, candies[i]);
             }
         }
 
-        while(!q.empty()){
-            int i = q.front();
-            q.pop();
-            for(const auto & neigh : children[i].outgoing) {
-                children[neigh].candies = max(children[neigh].candies, children[i].candies + 1);
-                children[neigh].indegree--;
-                if(children[neigh].indegree == 0){
-                    q.push(neigh);
-                }
-            }
+        int sum = 0;
+        for(const auto & candy : candies){
+            sum += candy;
         }
-
-        int totalCandies = 0;
-        for(const auto & child : children) {
-            totalCandies += child.candies;
-        }
-
-        return totalCandies;
+        return sum;
     }
 };
