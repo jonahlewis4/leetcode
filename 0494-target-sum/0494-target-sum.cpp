b91ceebb1720ff148cc5f84437edd65e8e45b5b9
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-06-02 21:06:50 -0700

    Time: 13 ms (58.69%), Space: 25.1 MB (35.51%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -2,29 +2,9 @@ class Solution {
 private:
     vector<int> nums;
     int target;
-    vector<vector<int>> dp;
+    vector<int> dp;
     int offset;
-    int recurse(int i, int cur) {
-        if(i == nums.size()){
-            if(cur == target){
-                return 1;
-            }
-            return 0;
-        }
-
-        
-        auto &_dp = dp;
-        if(dp[cur + offset][i] != INT_MAX){
-            return dp[cur + offset][i];
-        }
-
-        int minus = recurse(i + 1, cur - nums[i]);
-        int pos = recurse(i + 1, cur + nums[i]);
-
-
-        dp[cur + offset][i] = minus + pos;
-        return minus + pos;
-    }
+    
 public:
     int findTargetSumWays(const vector<int>& _nums, int target) {
         
@@ -41,32 +21,34 @@ public:
         }
 
         offset = sum;
-        dp.resize(nums.size() + 1, vector<int>(2 * sum + 1, 0));
+        dp.resize(2 * sum + 1, 0);
 
 
 
-        dp[nums.size()][target + sum] = 1;
+        dp[target + sum] = 1;
         auto &_dp = dp;
         for(int r = nums.size(); r >= 1; r--){
+            vector<int> newDp(dp.size(), 0);
             for(int c = -sum; c <= sum; c++){
-                if(dp[r][c + sum] > 0){
+                if(dp[c + sum] > 0){
                     int num = nums[r - 1];
 
                     int left = c - num;
                     int right = c + num;
 
                     if(left >= -sum && left <= sum){
-                        dp[r - 1][left + sum] = dp[r][c + sum] + dp[r - 1][left + sum];
+                        newDp[left + sum] = dp[c + sum] + newDp[left + sum];
                     }
 
                     if(right >= -sum && right <= sum){
-                        dp[r - 1][right + sum] = dp[r][c + sum] + dp[r - 1][right + sum];
+                        newDp[right + sum] = dp[c + sum] + newDp[right + sum];
                     }
                 }
             }
+            dp = newDp;
         }
 
 
-        return dp[0][sum];
+        return dp[sum];
     }
 };
