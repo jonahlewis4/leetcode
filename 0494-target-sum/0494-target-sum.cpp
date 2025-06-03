1e753ee9b66c5b4d81620dfa886e5bffdbbc29df
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-06-02 21:18:06 -0700

    Time: 11 ms (62.59%), Space: 25.1 MB (35.51%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,21 +1,54 @@
 class Solution {
+private:
+    vector<int> nums;
+    int target;
+    vector<int> dp;
+    int offset;
+    
 public:
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int cnt = 0;
-        solve(nums, target, 0, 0, cnt);
-        return cnt;
-    }
-
-    void solve(vector<int>& nums, int target, int idx, int sum, int& cnt) {
-        if (idx == nums.size()) {
-            if (sum == target) {
-                cnt++;
-            }
-            return;
+    int findTargetSumWays(const vector<int>& _nums, int target) {
+        
+        this->target = target;
+        nums = _nums;
+        
+        int sum = 0;
+        for(const auto & num : nums){
+            sum += num;
+        }
+        
+        if(abs(target) > sum) {
+            return 0;
         }
 
-        solve(nums, target, idx + 1, sum + nums[idx], cnt); 
-        solve(nums, target, idx + 1, sum - nums[idx], cnt);
+        offset = sum;
+        dp.resize(2 * sum + 1, 0);
 
+
+
+        dp[target + sum] = 1;
+        auto &_dp = dp;
+        for(int r = nums.size(); r >= 1; r--){
+            vector<int> newDp(dp.size(), 0);
+            for(int c = -sum; c <= sum; c++){
+                if(dp[c + sum] > 0){
+                    int num = nums[r - 1];
+
+                    int left = c - num;
+                    int right = c + num;
+
+                    if(left >= -sum && left <= sum){
+                        newDp[left + sum] = dp[c + sum] + newDp[left + sum];
+                    }
+
+                    if(right >= -sum && right <= sum){
+                        newDp[right + sum] = dp[c + sum] + newDp[right + sum];
+                    }
+                }
+            }
+            dp = newDp;
+        }
+
+
+        return dp[sum];
     }
 };
