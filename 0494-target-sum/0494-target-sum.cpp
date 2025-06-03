07917c43669a4b16c9707d97edc433f27f4375e3
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-06-02 20:14:19 -0700

    Time: 43 ms (45.18%), Space: 33.2 MB (24.49%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -2,7 +2,9 @@ class Solution {
 private:
     vector<int> nums;
     int target;
-    int recurse(int i, int cur) const {
+    vector<vector<int>> dp;
+    int offset;
+    int recurse(int i, int cur) {
         if(i == nums.size()){
             if(cur == target){
                 return 1;
@@ -10,15 +12,31 @@ private:
             return 0;
         }
 
+        
+        auto &_dp = dp;
+        if(dp[cur + offset][i] != INT_MAX){
+            return dp[cur + offset][i];
+        }
+
         int minus = recurse(i + 1, cur - nums[i]);
         int pos = recurse(i + 1, cur + nums[i]);
 
+
+        dp[cur + offset][i] = minus + pos;
         return minus + pos;
     }
 public:
     int findTargetSumWays(const vector<int>& _nums, int target) {
+        
         this->target = target;
         nums = _nums;
+        
+        int sum = 0;
+        for(const auto & num : nums){
+            sum += num;
+        }
+        offset = sum;
+        dp.resize(2 * sum + 1, vector<int>(nums.size(), INT_MAX));
 
         return recurse(0, 0);
     }
