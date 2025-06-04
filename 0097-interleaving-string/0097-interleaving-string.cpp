0062eb9ee87ad2f1af7473c829e2df76cbc6515d
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-06-03 20:08:12 -0700

    Time: 4 ms (55.84%), Space: 8.5 MB (87.8%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        if(s1.size() + s2.size() != s3.size()){
+            return false;
+        }
+
+        if(s1.size() < s2.size()){
+            swap(s1, s2);
+        }
+
+        //s1 is row;
+        //s2 is col;
+        //col is the smaller of the two. Space has been minimized.
+
+        if(s2.size() == 0){
+            return s1 == s3;
+        }
+
+        vector<bool> dp(s2.size() + 1, false);
+        dp.back() = true;
+
+        for(int r = s1.size(); r >= 0; r--){
+            int right = 0;
+            for(int c = s2.size(); c >= 0; c--){
+                //k is the index in string3
+                int k = r + c;
+                if(k >= s3.size()){
+                    right = dp[c];
+                    continue;
+                }
+
+                int below = dp[c];
+
+                bool cMatch = c < s2.size() && s2[c] == s3[k];
+                bool rMatch = r < s1.size() && s1[r] == s3[k];
+
+                if(cMatch && right) {
+                    dp[c] = true;
+                } else if (rMatch && below) {
+                    dp[c] = true;
+                } else {
+                    dp[c] = false;
+                }
+                right = dp[c];
+            }
+        }
+
+        return dp[0];
+    }
+};
