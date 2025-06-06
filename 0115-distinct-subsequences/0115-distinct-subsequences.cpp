0d8038e847240d1d09d3122d950244c18d84985f
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-06-06 13:39:22 -0700

    Time: 7 ms (97.01%), Space: 8.9 MB (91.66%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        if(t.size() > s.size()){
+            return 0;
+        }
+
+        vector<int> dp(t.size(), 0);
+        int cc = t.size();
+        int rc = s.size();
+
+        for(int r = rc - 1; r >= 0; r--){
+            int c = cc - 1;
+            int bottomRight = 1;
+            for(int i = 0; i < min(cc, rc - r); i++){
+                int bottom = dp[c];
+
+                long long ways = bottom;
+                if(s[r] == t[c]){
+                    ways += bottomRight;
+                }
+
+                bottomRight = bottom;
+                dp[c] = ways > INT_MAX ? -1 : ways;
+                c--;
+            }
+        }
+        return dp[0];
+    }
+};
