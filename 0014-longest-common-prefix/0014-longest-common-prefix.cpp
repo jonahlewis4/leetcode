fedb842804889465e1090d422d9c61e7a383de2e
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-06-06 23:29:28 -0700

    Time: 0 ms (100%), Space: 11.8 MB (67.21%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -4,19 +4,14 @@ public:
         int i = 0;
         string res;
         while(true){
-            bool noMatch = false;
-            bool end = false;
             for(const auto & s : strs) {
                 if(i >= s.size() || s[i] != strs[0][i]){
-                    noMatch = true;
-                    break;
+                    return res;
                 }
             }
-            if(noMatch) {
-                return res;
-            } else {
-                res += strs[0][i];
-            }
+            
+            res += strs[0][i];
+            
             i++;
         }
         return res;
