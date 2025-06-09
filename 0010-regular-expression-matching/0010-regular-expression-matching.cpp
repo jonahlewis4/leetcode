6fc0dbe7a292d875d4e973d06e2fd13856b72f45
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-06-08 17:26:23 -0700

    Time: 0 ms (100%), Space: 8.3 MB (72.06%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -4,7 +4,10 @@ class Solution {
 
     vector<vector<int>> dp;
     bool starAfter(int pI) const {
-        if(pI + 1 < p.size() && p[pI + 1] == '*'){
+        if(pI + 1 >= p.size()){
+            return false;
+        }
+        if(p[pI + 1] == '*'){
             return true;
         }
         return false;
@@ -12,48 +15,85 @@ class Solution {
     bool match(int sI, int pI) const {
         return s[sI] == p[pI] || p[pI] == '.';
     }
-    bool calc(int sI, int pI) {
 
-        if(sI == s.size() && pI == p.size()){
-            return true;
-        }
-
-        if(sI > s.size()){
-            return false;
-        }
-
-        if(pI >= p.size()){
-            return false;
-        }
-
-        if(dp[sI][pI] != -1){
-            return dp[sI][pI];
-        }
-
-
-        if(starAfter(pI)){
-            bool res = false;
-            if(match(sI, pI)){
-                res |= calc(sI + 1, pI);
-            }
-            res |= calc(sI, pI + 2);
-            dp[sI][pI] = res;
-            return res;
-        } else {
-            if(match(sI, pI)){
-                int res = calc(sI + 1, pI + 1);
-                dp[sI][pI] = res;
-                return res;
-            }
-        }
-        dp[sI][pI] = false;
-        return false;
+    void update(
+        int res,
+        int& botRight,
+        int& twoRight,
+        int& right,
+        std::vector<bool>& dp,
+        int below,
+        int c
+    ) {
+        twoRight = right;
+        botRight = below;
+        right = res;
+        dp[c] = res;
     }
+
 public:
     bool isMatch(string s, string p) {
-        dp.resize(s.size() + 1, vector<int>(p.size(), -1));
         this->s = s;
         this->p = p;
-        return calc(0, 0);
+        vector<bool> dp(p.size() + 1, false);
+        //need to track below, right two, bottom right
+        dp.back() = true;
+
+        for(int c = dp.size() - 2; c >= 0; c--){
+            if(p[c] == '*'){
+                continue;
+            }
+
+            if(starAfter(c)){
+                dp[c] = dp[c + 2];
+            }
+        }
+
+        for(int r = s.size() - 1; r >= 0; r--) {
+            int botRight = dp.back();
+            int twoRight = false;
+            dp.back() = false;
+            int right = dp.back();
+
+            
+
+            for(int c = p.size() - 1; c >= 0; c--) {
+                int below = dp[c];
+
+                if(p[c] == '*'){
+                    update(false, botRight, twoRight, right, dp, below, c);
+                    continue;
+                }
+
+
+                
+
+
+                if(starAfter(c)) {
+                    bool res = false;
+                    if(match(r, c)){
+                        res |= below;
+                    }
+                    res |= twoRight;
+
+                    update(res, botRight, twoRight, right, dp, below, c);
+                    continue;
+                } else {
+                    if (match(r, c)) {
+                        bool res = botRight;
+                        update(res, botRight, twoRight, right, dp, below, c);
+                        continue;
+                    }
+                    update(false, botRight, twoRight, right, dp, below, c);
+
+
+                }
+            }
+
+        }
+
+
+        return dp[0];
+
     }
 };
