0bba37a209e4b7e74ed401c9abd5d9bac38afada
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-06-09 18:25:15 -0700

    Time: 4 ms (16.34%), Space: 10 MB (27.18%) - LeetHub

--- 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.cpp
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -6,18 +6,14 @@ public:
             freq[c]++;
         }
 
-        int smallestOdd = INT_MAX;
         int largestOdd = INT_MIN;
         int smallestEven = INT_MAX;
-        int largestEven = INT_MIN;
 
         for(const auto & p : freq) {
             int count = p.second;
             if(count % 2 == 0){
                 smallestEven = min(count, smallestEven);
-                largestEven = max(count, largestEven);
             } else {
-                smallestOdd = min(count, smallestOdd);
                 largestOdd = max(count, largestOdd);
             }
         }
