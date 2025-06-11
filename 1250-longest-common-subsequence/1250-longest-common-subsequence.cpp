963277d459c03bf0eaf403e94d3e58ff0d467e88
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-06-10 20:02:19 -0700

    Time: 15 ms (91.53%), Space: 9.2 MB (92.86%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,105 +1,27 @@
 class Solution {
-
-    class Recursive{
-        const string& text1;
-        const string& text2;
-    public:
-        Recursive(const string& text1, const string& text2) : text1(text1), text2(text2) {
-
-        }
-
-        int Solution(){
-            return recurse(0, 0);
-        }
-        int recurse(int i1, int i2){
-            if(i1 >= text1.size() || i2 >= text2.size()){
-                return 0;
-            }
-
-            if(text1[i1] == text2[i2]){
-                int use = 1 + recurse(i1 + 1, i2 + 1);
-                return use;
-            }
-
-            int skip1 = recurse(i1 + 1, i2);
-            int skip2 = recurse(i1, i2 + 1);
-            return max({skip1, skip2});
-        }
-    };
-
-    class Memo {
-        const string& text1;
-        const string& text2;
-
-        vector<vector<int>> dp;
-    public:
-        Memo(const string& text1, const string& text2) : text1(text1), text2(text2) {
-            dp.resize(text1.size(), vector<int>(text2.size(), -1));
-        }
-
-        int Solution() {
-            return recurse(0, 0);
-        }
-
-        int recurse(int i1, int i2) {
-            if(i1 >= text1.size() || i2 >= text2.size()){
-                return 0;
-            }
-
-            if(dp[i1][i2] != -1){
-                return dp[i1][i2];
-            }
-
-
-
-            if(text1[i1] == text2[i2]){
-                int use = 1 + recurse(i1 + 1, i2 + 1);
-                dp[i1][i2] = use;
-                return use;
-            }
-
-            int skip1 = recurse(i1 + 1, i2);
-            int skip2 = recurse(i1, i2 + 1);
-            dp[i1][i2] = max({skip1, skip2});
-            return max({skip1, skip2});
-        }
-    };
-
-    class Tabulation{
-        const string& text1;
-        const string& text2;
-
-        vector<int> dp;
-    public:
-        Tabulation(const string& text1, const string& text2) : text1(text1), text2(text2) {
-            dp.resize(text2.size() + 1, 0);
-        }
-
-        int Solution() {
-            for(int r = text1.size() - 1; r >= 0; r--){
-                int right = 0;
-                int bottomRight = 0;
-                for(int c = text2.size() - 1; c >= 0; c--) {
-                    int bottom = dp[c];
-                    if(text1[r] == text2[c]){
-                        dp[c] = bottomRight + 1;
-                        bottomRight = bottom;
-                        right = dp[c];
-                        continue;
-                    }
-                    
-                    bottomRight = bottom;
-                    dp[c] = max(right, bottom);
-
-                }
-            }
-
-            return dp[0];
-        }
-    };
-
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        return Tabulation(text1, text2).Solution();
+        if(text1.size() < text2.size()){
+            swap(text1, text2);
+        }
+
+        vector<int> dp(text2.size() + 1, 0);
+        for(int r = text1.size() - 1; r >= 0; r--){
+            int bottomRight = 0;
+            for(int c = text2.size() - 1; c >= 0; c--){
+                int bottom = dp[c];
+                int right = dp[c + 1];
+
+                int best = 0;
+                if(text1[r] == text2[c]){
+                    best = max(best, 1 + bottomRight);
+                }
+                best = max(best, right);
+                best = max(best, bottom);
+                bottomRight = bottom;
+                dp[c] = best;
+            }
+        }
+        return dp[0];
     }
 };
