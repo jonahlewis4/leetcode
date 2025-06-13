b7a63b2c19a9070e74ea0d988d46fd50ef6d5a78
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-06-12 22:37:20 -0700

    Time: 23 ms (47.87%), Space: 25.9 MB (27.97%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,47 +1,72 @@
 class Solution {
-private:
-    
-public:
-    int findTargetSumWays(const vector<int>& nums, int target) {
-        
-        
-        int sum = 0;
-        for(const auto & num : nums){
-            sum += num;
-        }
-        
-        if(abs(target) > sum) {
-            return 0;
-        }
+    class Base {
+        protected:
+            vector<int>& nums;
+            int target;
+            Base(vector<int>& nums, int target) : nums(nums), target(target){}
 
-        vector<int> dp(2 * sum + 1, 0);
+        public:
+            virtual int findTargetSumWays() = 0;
+    };
 
-
-
-        dp[target + sum] = 1;
-        auto &_dp = dp;
-        for(int r = nums.size(); r >= 1; r--){
-            vector<int> newDp(dp.size(), 0);
-            for(int c = -sum; c <= sum; c++){
-                if(dp[c + sum] > 0){
-                    int num = nums[r - 1];
-
-                    int left = c - num;
-                    int right = c + num;
-
-                    if(left >= -sum && left <= sum){
-                        newDp[left + sum] = dp[c + sum] + newDp[left + sum];
-                    }
-
-                    if(right >= -sum && right <= sum){
-                        newDp[right + sum] = dp[c + sum] + newDp[right + sum];
-                    }
-                }
+    class Recursion : Base{
+        public:
+            Recursion(vector<int>& nums, int target) : Base(nums, target){}
+            int findTargetSumWays() override  {
+                return dfs(0, 0);
             }
-            dp = newDp;
+            int dfs (int i, int val) const {
+                if(i == nums.size() && val == target) {
+                    return 1;
+                }
+                if(i >= nums.size()) {
+                    return 0;
+                }
+                int add = dfs(i + 1, val + nums[i]);
+                int sub = dfs(i + 1, val - nums[i]);
+
+                return add + sub;
+            }
+    };
+
+    class Memoization : Base {
+        private:
+            vector<vector<int>> dp;
+            int sum;
+        public:
+        Memoization(vector<int>& nums, int target) : Base(nums, target) {
+            sum = 0;
+            for(const auto & num : nums){
+                sum += num;
+            }
+            dp.resize(nums.size(), vector<int>(sum * 2 + 1, -1));
         }
-
-
-        return dp[sum];
+        int findTargetSumWays() {
+            if(sum < abs(target)){
+                return false;
+            }
+            return dfs(0, 0);
+        }
+        int dfs(int i, int val) {
+            if(i == nums.size() && val == target) {
+                return 1;
+            }
+            if(i >= nums.size()) {
+                return 0;
+            }
+            if(dp[i][val + sum] != -1){
+                return dp[i][val + sum];
+            }
+            
+            int add = dfs(i + 1, val + nums[i]);
+            int sub = dfs(i + 1, val - nums[i]);
+            int res = add + sub;
+            dp[i][val + sum] = res;
+            return res;
+        }
+    };
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        return Memoization(nums, target).findTargetSumWays();
     }
 };
