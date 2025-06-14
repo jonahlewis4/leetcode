5beb4e9ddf509f6d9e050db0fa89c7ae0c3c15e5
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-06-14 14:25:13 -0700

    Time: 0 ms (100%), Space: 10.2 MB (97.7%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,46 +1,36 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        
-
         if(word1.size() < word2.size()){
-            swap(word1, word2); 
+            swap(word1, word2);
         }
 
-        if(word2.size() == 0){
-            return word1.size();
-        }
-
-        vector<int> dp(word2.size());
-        for(int c = 0; c < dp.size(); c++){
-            dp[c] = word2.size() - c;
+        vector<int> dp(word2.size() + 1, 0);
+        for(int c = dp.size() - 1; c >= 0; c--){
+            int ways = dp.size() - 1 - c;
+            dp[c] = ways;
         }
 
         for(int r = word1.size() - 1; r >= 0; r--){
-            int bottomRight = word1.size() - r - 1;
-            int right = bottomRight + 1;
+            int bottomRight = dp.back();
+            dp.back() = dp.back() + 1;
             for(int c = word2.size() - 1; c >= 0; c--){
-                int below = dp[c];
+                int bottom = dp[c];
+                int right = dp[c + 1];
+
+                int res = INT_MAX;
                 if(word1[r] == word2[c]){
-                    dp[c] = bottomRight;
-                } else {
-                    int replace = bottomRight;
-                    int _delete = below;
-                    int add = right;
-                    
-                    int best = min({replace, _delete, add});
-                    dp[c] = best + 1;
-                }
+                    res = min(res, bottomRight);
+                }   
 
+                res = min(res, right + 1);
+                res = min(res, bottom + 1);
+                res = min(res, bottomRight + 1);
 
-                bottomRight = below;
-                right = dp[c];
+                bottomRight = bottom;
+                dp[c] = res;
             }
         }
-
-        return dp[0];
+        return dp.front();
     }
-    
-    
 };
-    
