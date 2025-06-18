880fc840b0fd7601849bdf7c9d7ffb652c45cef3
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-06-17 20:01:34 -0700

    Time: 12 ms (87.07%), Space: 8.5 MB (99.96%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,30 +1,63 @@
 class Solution {
+private:
+    class Recursive {
+    private:
+        const string& s;
+        const string& t;
+    public:
+        Recursive(const string& s, const string& t) : s(s), t(t) {}
+        
+        int Solve() const {
+            return calc(0, 0);
+        }
+        
+        int calc(int i, int j) const {
+            if(j >= t.size()){
+                return 1;
+            }
+            if(i >= s.size()){
+                return 0;
+            }
+
+            if(s[i] == t[j]) {
+                int use = calc(i + 1, j + 1);
+                int skip = calc(i + 1, j);
+                return use + skip;
+            }
+
+            return calc(i + 1, j);
+        }
+    };
+    class Tabulation {
+        private:
+        const string& s;
+        const string& t;
+    public:
+        Tabulation(const string& s, const string& t) : s(s), t(t) {}
+        int Solve() {
+            vector<unsigned int> dp(t.size() + 1);
+            dp.back() = 1;
+            for(int i = s.size() - 1; i >= 0; i--) {
+                int bottomRight = dp.back();
+                for(int j = t.size() - 1; j >= 0; j--) {
+                    int bottom = dp[j];
+                    unsigned int res = bottom;
+                    if(s[i] == t[j]){
+                        res = min(res + bottomRight, (unsigned int)INT_MAX);
+                    }
+                    bottomRight = bottom;
+                    dp[j] = res;
+                }
+            }
+            return dp[0];
+        }
+    };
 public:
     int numDistinct(string s, string t) {
-        if(t.size() > s.size()){
+        if(t.size() > s.size()) {
             return 0;
         }
-
-        vector<unsigned int> dp(t.size(), 0);
-        int cc = t.size();
-        int rc = s.size();
-
-        for(int r = rc - 1; r >= 0; r--){
-            unsigned int c = cc - 1;
-            unsigned int bottomRight = 1;
-            for(int i = 0; i < min(cc, rc - r); i++){
-                int bottom = dp[c];
-
-                unsigned int ways = bottom;
-                if(s[r] == t[c]){
-                    ways += bottomRight;
-                }
-
-                bottomRight = bottom;
-                dp[c] = ways > INT_MAX ? -1 : ways;
-                c--;
-            }
-        }
-        return dp[0];
+        return Tabulation(s, t).Solve();
+        
     }
 };
