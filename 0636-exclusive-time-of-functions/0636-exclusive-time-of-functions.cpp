cbf7afb70435562e21659f738a3ab731bb81d986
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-06-20 01:40:55 -0700

    Time: 0 ms (100%), Space: 18.3 MB (68.74%) - LeetHub

---
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.cpp
@@ -0,0 +1,56 @@
+class Solution {
+
+public:
+    vector<int> exclusiveTime(int n, vector<string>& logs) {
+        vector<int> res(n, 0);
+        struct Log {
+            int id;
+            int start = -1;
+            int end = -1;
+        };
+        const auto & stoiL = [](const string& s) {
+            Log res;
+            int i = 0;
+            while(s[i] != ':'){
+                i++;
+            }
+            string idStr = s.substr(0, i);
+            i++;
+            res.id = stoi(idStr);
+
+            int* dest = &res.start;
+            if(s[i] == 'e'){
+                dest = &res.end;
+            }
+
+            while(s[i] != ':'){
+                i++;
+            }
+            i++;
+
+            *dest = stoi(s.substr(i, s.size() - i));
+            return res;
+        };
+        stack<Log> stack;
+        int lastSwitch = 0;
+        for(const auto & log : logs) {
+            Log l = stoiL(log);
+            if(l.start != -1){
+                if(!stack.empty()){
+                    Log running = stack.top();
+                    res[running.id] += l.start - lastSwitch;
+
+                }
+                lastSwitch = l.start;
+                stack.push(l);
+
+            } else {
+                Log running = stack.top();
+                stack.pop();
+                res[running.id] += l.end + 1 - lastSwitch;
+                lastSwitch = l.end + 1;
+            }
+        } 
+        return res;
+    }
+};
