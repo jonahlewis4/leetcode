d59e3e59c935c642bec5f9fed8c8c62249a55c64
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-06-20 01:44:28 -0700

    Time: 5 ms (65.32%), Space: 18.2 MB (73.76%) - LeetHub

--- 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.cpp
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.cpp
@@ -31,23 +31,23 @@ public:
             *dest = stoi(s.substr(i, s.size() - i));
             return res;
         };
-        stack<Log> stack;
+        stack<int> stack;
         int lastSwitch = 0;
         for(const auto & log : logs) {
             Log l = stoiL(log);
             if(l.start != -1){
                 if(!stack.empty()){
-                    Log running = stack.top();
-                    res[running.id] += l.start - lastSwitch;
+                    int running = stack.top();
+                    res[running] += l.start - lastSwitch;
 
                 }
                 lastSwitch = l.start;
-                stack.push(l);
+                stack.push(l.id);
 
             } else {
-                Log running = stack.top();
+                int running = stack.top();
                 stack.pop();
-                res[running.id] += l.end + 1 - lastSwitch;
+                res[running] += l.end + 1 - lastSwitch;
                 lastSwitch = l.end + 1;
             }
         } 
