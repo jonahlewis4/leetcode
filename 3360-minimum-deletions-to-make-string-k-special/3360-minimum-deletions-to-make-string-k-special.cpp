5a9337c3a909cb54d7bb88867556417be88eff2f
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-06-20 23:20:44 -0700

    Time: 25 ms (18.48%), Space: 19.6 MB (41.3%) - LeetHub

---
+++ 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minimumDeletions(string word, int k) {
+        //count and sort
+        //find largest window where they're k apart.
+        //find distance of all not in window and add together.
+        unordered_map<char, int> freq;
+        for(const auto & c : word){
+            freq[c]++;
+        }
+
+        int best = INT_MAX;
+        for(const auto & [_, count] : freq){
+            int removals = 0;
+            for(const auto & [_, count2] : freq) {
+                if(count2 < count) {
+                    removals += count2;
+                } else if (count2 > count + k) {
+                    removals += count2 - (count + k);
+                }
+            }
+            best = min(best, removals);
+        }
+        return best;
+    }
+};
