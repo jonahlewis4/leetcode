375751bb5855946c0ba518e0206ddf133706c6e8
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-06-20 23:22:24 -0700

    Time: 8 ms (56.88%), Space: 19.2 MB (86.59%) - LeetHub

--- 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.cpp
+++ 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    int minimumDeletions(string word, int k) {
-        unordered_map<char, int> freq;
-        for(const auto & c : word){
-            freq[c]++;
+   int minimumDeletions(string word, int k) {
+    vector<int> freq(26, 0);
+    int deleted = 0, ans = word.size();
+    for(auto c: word) freq[c-'a']++;             // find frequency of each chars
+    sort(freq.begin(), freq.end());              // sort the freq array
+    for(int i = 0; i < freq.size(); ++i){        // Iterate over freq 
+        int res = deleted, minFreq = freq[i];    // consider the current freq as the min freq after deletion. Also add the already deleted freq to temporary result
+        for(int j = freq.size() - 1; j >= 0; --j){  // iterate over freq array to evaluate the chars to be deleted from the higher freq side of the freq array
+            if(freq[j] - minFreq <= k) break;    // Once condition is satisfied then exit from the loop
+            res += freq[j] - minFreq - k;        // accumulate on the result - number of items to be deleted
         }
-
-        int best = INT_MAX;
-        for(const auto & [_, count] : freq){
-            int removals = 0;
-            for(const auto & [_, count2] : freq) {
-                if(count2 < count) {
-                    removals += count2;
-                } else if (count2 > count + k) {
-                    removals += count2 - (count + k);
-                }
-            }
-            best = min(best, removals);
-        }
-        return best;
+        ans = min(ans, res);                     // keep tracking the min possible value
+        deleted += freq[i];                      // update the deleted freq with current freq
     }
+    return ans;
+}
 };
