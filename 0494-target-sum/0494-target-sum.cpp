bd00e3a935bebbe229fcedeb383b6fc95a8ee7bc
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-06-22 00:58:27 -0700

    Time: 6 ms (72.75%), Space: 15.3 MB (56.06%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,72 +1,65 @@
 class Solution {
-    class Base {
-        protected:
-            vector<int>& nums;
-            int target;
-            Base(vector<int>& nums, int target) : nums(nums), target(target){}
-
-        public:
-            virtual int findTargetSumWays() = 0;
-    };
-
-    class Recursion : Base{
-        public:
-            Recursion(vector<int>& nums, int target) : Base(nums, target){}
-            int findTargetSumWays() override  {
-                return dfs(0, 0);
-            }
-            int dfs (int i, int val) const {
-                if(i == nums.size() && val == target) {
-                    return 1;
-                }
-                if(i >= nums.size()) {
-                    return 0;
-                }
-                int add = dfs(i + 1, val + nums[i]);
-                int sub = dfs(i + 1, val - nums[i]);
-
-                return add + sub;
-            }
-    };
-
-    class Memoization : Base {
-        private:
-            vector<vector<int>> dp;
-            int sum;
-        public:
-        Memoization(vector<int>& nums, int target) : Base(nums, target) {
-            sum = 0;
-            for(const auto & num : nums){
-                sum += num;
-            }
-            dp.resize(nums.size(), vector<int>(sum * 2 + 1, -1));
+    class SubsetSumCount{
+        static constexpr int MOD = 1e9 + 7;
+        const vector<int>& nums;
+        int target;
+    public:
+        SubsetSumCount(const vector<int>& nums, int target) : nums(nums), target(target) {}
+        int Recursive() const {
+            return recurse(0, 0);
         }
-        int findTargetSumWays() {
-            if(sum < abs(target)){
-                return false;
-            }
-            return dfs(0, 0);
-        }
-        int dfs(int i, int val) {
-            if(i == nums.size() && val == target) {
+        int recurse(int i, int sum) const {
+            if(i >= nums.size() && sum == target){
                 return 1;
             }
-            if(i >= nums.size()) {
+            if(i >= nums.size() && sum != target) {
                 return 0;
             }
-            if(dp[i][val + sum] != -1){
-                return dp[i][val + sum];
+
+            int use = recurse(i + 1, sum + nums[i]);
+            int skip = recurse(i + 1, sum);
+            return (use + skip) % MOD;
+        }
+
+        int Memo() {
+            vector<vector<int>> cache(nums.size(), vector<int>(target + 1, -1));
+            return recurseMemo(0, 0, cache);
+        }
+        int recurseMemo(int i, int sum, vector<vector<int>>& cache){
+            if(sum > target) {
+                return 0;
             }
-            
-            int add = dfs(i + 1, val + nums[i]);
-            int sub = dfs(i + 1, val - nums[i]);
-            int res = add + sub;
-            dp[i][val + sum] = res;
-            return res;
+            if(i >= nums.size() && sum == target){
+                return 1;
+            }
+            if(i >= nums.size() && sum != target) {
+                return 0;
+            }
+            if(cache[i][sum] != -1){
+                return cache[i][sum];
+            }
+            int use = recurseMemo(i + 1, sum + nums[i], cache);
+            int skip = recurseMemo(i + 1, sum, cache);
+            cache[i][sum] = (use + skip) % MOD;
+            return cache[i][sum];
         }
     };
 public:
-    int findTargetSumWays(vector<int>& nums, int target) {
-        return Memoization(nums, target).findTargetSumWays();
+    int findTargetSumWays(vector<int>& nums, int target) {  
+        int sum = 0;
+        for(const auto & num : nums) {
+            sum += num;
+        }
+        if(sum < target) {
+            return 0;
+        }
+
+        if((sum + target) % 2 == 1 || (sum + target) < 0) {
+            return 0;
+        }
+
+        int newSum = (sum + target) / 2; 
+
+        return SubsetSumCount(nums, newSum).Memo();
     }
 };
