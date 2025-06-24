5313b5e37c4cdd388b4a9056675ad2483692d5d3
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-06-23 20:51:23 -0700

    Time: 0 ms (100%), Space: 8.2 MB (97.47%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,41 +1,35 @@
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
-    
-        if (s2.size() + s1.size() != s3.size()){
+        if(s1.size() + s2.size() != s3.size()){
             return false;
         }
-
         if(s1.size() < s2.size()){
             swap(s1, s2);
-        }    
+        }
 
-
+        //if 1 match 
+            //i + 1, j
+        //if 2 match
+            //i, j + 1
+        
         vector<bool> dp(s2.size() + 1, false);
         dp.back() = true;
 
-        for(int c = dp.size() - 1; c >= 0; c--){
-            if(s3[s1.size() + c] == s2[c] && dp[c + 1]){
-                dp[c] = true;
-            }
+        for(int c = dp.size() - 2; c >= 0; c--){
+            dp[c] = s2[c] == s3[s1.size() + c] && dp[c + 1];
         }
 
-        for(int r = s1.size() - 1; r>=0; r--){
-            dp.back() = s3[s2.size() + r] == s1[r] && dp.back();
-            for(int c = s2.size() - 1; c>=0; c--){
-                bool res = false;
-                bool right = dp[c + 1];
-                bool below = dp[c];
-
-                if(s3[r + c] == s1[r] && below) {
-                    res = true;
-                } else if (s3[r + c] == s2[c] && right) {
-                    res = true;
-                }
-
-                right = res;
+        for(int r = s1.size() - 1; r >= 0; r--) {
+            dp.back() = dp.back() && s3[s2.size() + r] == s1[r];
+            for(int c = s2.size() - 1; c >= 0; c--){
+                int below = dp[c];
+                int right = dp[c + 1];
+                
+                int res = false;
+                res |= s1[r] == s3[r + c] && below;
+                res |= s2[c] == s3[r + c] && right;
                 dp[c] = res;
-
             }
         }
         return dp.front();
