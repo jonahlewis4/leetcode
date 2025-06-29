ad1cbf288bbddd14f9371d829fe33eb3cd268430
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-06-29 01:31:12 -0700

    Time: 46 ms (15.11%), Space: 54.3 MB (34.63%) - LeetHub

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -4,6 +4,14 @@ public:
         sort(nums.begin(), nums.end());
         int ans = 0;
         int MOD = 1e9 + 7;
+
+        vector<int> powers(nums.size(), 0);
+        powers[0] = 1;
+        for(int i = 1; i < powers.size(); i++) {
+            powers[i] = (powers[i - 1] * 2) % MOD;
+        }
+
+
         for(int i = 0; i < nums.size(); i++){
             int num = nums[i];
             int comp = target - num;
@@ -14,19 +22,9 @@ public:
             if(maxLen < 1) {
                 continue;
             }
-            ans += shiftLeftMod1e9(maxLen - 1);
+            ans += powers[maxLen - 1];
             ans %= MOD;
         }
         return ans;
-    }
-    int shiftLeftMod1e9(int i) {
-        long long res = 1;
-        while(i > 0) {
-            res <<= min(31, i);
-            res %= (int)1e9 + 7;
-            i -= 31;
-        }
-        return (int) res;
-
     }
 };
