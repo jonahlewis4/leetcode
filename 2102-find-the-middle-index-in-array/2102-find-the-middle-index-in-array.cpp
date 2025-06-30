8d82cdd4c26ed29cfd10af40e7ea25acdc814042
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-06-30 02:22:59 -0700

    Time: 0 ms (100%), Space: 16 MB (58.15%) - LeetHub

--- 2102-find-the-middle-index-in-array/2102-find-the-middle-index-in-array.cpp
+++ 2102-find-the-middle-index-in-array/2102-find-the-middle-index-in-array.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int findMiddleIndex(vector<int>& nums) {
-        unordered_map<int, int> sumsWhichCouldSatisfy;
+        int sum = 0;
+        for(const auto & num : nums) {
+            sum += num;
+        }
+
         int leftSum = 0;
-        for(int i = 0; i < nums.size(); i++) {
-            int num = nums[i];
-            int sumWhichCouldSatisfy = leftSum * 2 + num;
-            if(sumsWhichCouldSatisfy.find(sumWhichCouldSatisfy) == sumsWhichCouldSatisfy.end()) {
-                sumsWhichCouldSatisfy[sumWhichCouldSatisfy] = i;
+        int i = 0;
+        for(const auto & num : nums ) {
+            if(2 * leftSum + num == sum){
+                return i;
             }
+            i++;
             leftSum += num;
         }
-        if(sumsWhichCouldSatisfy.find(leftSum) == sumsWhichCouldSatisfy.end()) {
-            return -1;
-        }
-        return sumsWhichCouldSatisfy[leftSum];
+        return -1;
     }
 };
