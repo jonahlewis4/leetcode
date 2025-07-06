e8158afcf4fdbe38ed06742921a51d1e4da09f08
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-07-05 20:08:50 -0700

    Time: 2295 ms (5.01%), Space: 266 MB (5.01%) - LeetHub

---
+++ 1995-finding-pairs-with-a-certain-sum/1995-finding-pairs-with-a-certain-sum.cpp
@@ -0,0 +1,43 @@
+class FindSumPairs {
+    unordered_map<int, int> freq1;
+    unordered_map<int, int> freq2;
+    vector<int>& nums2;
+public:
+    FindSumPairs(vector<int>& nums1, vector<int>& nums2) : nums2(nums2) {
+        for(const auto & num : nums1) {
+            freq1[num]++;
+        }
+        for(const auto & num : nums2) {
+            freq2[num]++;
+        }
+    }
+    
+    void add(int index, int val) {
+        int num = nums2[index];
+        freq2[num] = max(0, freq2[num] - 1);
+        int newNum = num + val;
+        freq2[newNum]++;
+        nums2[index] = newNum;
+        
+    }
+    
+    int count(int tot) {
+        //optimization: use size of freq1 or freq2 to dettermine which to 
+        //iterate over
+        int ans = 0;
+        for(const auto & keyValPair : freq2) {
+            int occur1 = keyValPair.second;
+            int num = keyValPair.first;
+            int occur2 = freq1[tot - num];
+            ans += occur1 * occur2;
+        }
+        return ans;
+    }
+};
+
+/**
+ * Your FindSumPairs object will be instantiated and called as such:
+ * FindSumPairs* obj = new FindSumPairs(nums1, nums2);
+ * obj->add(index,val);
+ * int param_2 = obj->count(tot);
+ */
