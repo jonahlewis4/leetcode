27e141e8257e390795275a9270781da461d3e0bf
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-07-11 15:24:10 -0700

    Time: 0 ms (100%), Space: 7.9 MB (82.38%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
@@ -1,20 +1,7 @@
 class Solution {
 private:
-    int log(int i) {
-        return 31 - __builtin_clz(i);
-    }
 public:
-    char kthCharacter(int k) {
-        return get(k - 1);
+    static char kthCharacter(int k) {
+        return 'a' + __builtin_popcount(k - 1);
     }
-    
-    char get(int k) {
-        if(k == 0) {
-            return 'a';
-        }
-        int logarithm = log(k);
-        int segLength = 1 << logarithm;
-        return get(k - segLength) + 1;
-    }
-
 };
