d6595573f07778ed22d23e96da362b38fac51909
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-07-13 02:13:38 -0700

    Time: 0 ms (100%), Space: 10.8 MB (93.47%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,40 +1,62 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        if(numRows == 1){
+        if(numRows == 1) {
             return s;
         }
-        int colSize = ceil((double)s.size() / (2 * numRows - 2)) * (numRows - 1);
-        vector<vector<char>> grid(numRows, vector<char>(colSize, ' '));
-
-        int r = 0;
-        int c = 0;
-        int i   = 0;
-        while(i < s.size())
-        {
-            while(i < s.size() && r < numRows && c < colSize){ 
-                
-                grid[r][c] = s[i];
-                r++;
-                i++;
-            }
-            r -= 2;
-            c++;
-            while(i < s.size() && r > 0 && c < colSize){
+        int numCharsInSection = numRows + numRows - 2;
+        int numSections = (int)std::ceil((double)s.size() / numCharsInSection); 
+        int sectionWidth = numRows - 1;
         
-                grid[r][c] = s[i];
-                r--;
-                c++;
+        string res(s.size(), '.');
+        int sectionNumber = 0;
+        int i = 0;
+        while(i < s.size()) {
+            int resI = sectionNumber;
+            int curRow = 0;
+            int remainingSections = numSections - sectionNumber - 1; 
+            for(int curRow = 0; curRow < numRows; curRow++) {
+                if(i >= s.size()) {
+                    return res;
+                }
+                res[resI] = s[i];
+                resI++;
+                bool dualRow = curRow != 0 && curRow != numRows - 1;
+                int gap = (numRows - curRow - 1) * 2;
+                if(dualRow) {
+                    //add bonus character
+                    if(i + gap < s.size()) {
+                        res[resI] = s[i + gap];
+                        resI++;
+                    }
+                }
+                //calculate number of chars to right to skip.
+                int numToSkipRight = 0;
+                if(remainingSections > 1) {
+                    int numPerSectionInThisRow = dualRow ? 2 : 1;
+                    numToSkipRight += numPerSectionInThisRow * (remainingSections - 1); 
+                }
+                if(remainingSections > 0) {
+                    int iInFinalSection = numCharsInSection * (numSections - 1);
+                    int iInThisRowFinalSection = iInFinalSection + curRow;
+                    if(iInThisRowFinalSection < s.size()){
+                        numToSkipRight++;
+                        if(dualRow && (iInThisRowFinalSection + gap < s.size())) {
+                            numToSkipRight++;
+                        }
+                    }
+                }
+                
+                //calculate number of chars left to skip
+                int numPerSectionInNextRow = (curRow + 1 == numRows - 1) ? 1 : 2;
+                int numToSkipLeft = numPerSectionInNextRow * sectionNumber;
+
+                int numToSkip = numToSkipRight + numToSkipLeft;
+                resI += numToSkip;
                 i++;
             }
-        }
-        string res = "";
-        for(int    r = 0; r < numRows; r++){
-            for(int c = 0; c < grid[r].size(); c++){
-                if(grid[r][c] != ' '){
-                    res += grid[r][c];
-                }
-            }
+            sectionNumber++;
+            i += numCharsInSection - numRows;
         }
         return res;
     }
