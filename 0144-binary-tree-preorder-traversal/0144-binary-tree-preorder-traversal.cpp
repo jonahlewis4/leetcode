3af54731a86ef5c80477066f075a1a87daeb7505
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-07-15 20:11:10 -0700

    Time: 0 ms (100%), Space: 10.8 MB (71.69%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -1,31 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-vector<int> res;
-TreeNode* rightMostInLeft(TreeNode* root) {
-        TreeNode* predecessor = root->left;
-        while(predecessor->right != nullptr && predecessor->right != root) {
-            predecessor = predecessor->right;
+private:
+    static TreeNode* predecessor(TreeNode* root) {
+        TreeNode* cur = root->left;
+        while(cur->right != nullptr && cur->right != root) {
+            cur = cur->right;
         }
-        return predecessor;
+        return cur;
     }
 public:
-
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> res;
         TreeNode* cur = root;
+        vector<int> res;
         while(cur != nullptr) {
             if(cur->left == nullptr) {
                 res.push_back(cur->val);
                 cur = cur->right;
                 continue;
             }
-            TreeNode* predecessor = rightMostInLeft(cur);
-            if(predecessor->right == nullptr) {
-                res.push_back(cur->val);
-                predecessor->right = cur;
-                cur = cur->left;
-            } else {
-                predecessor->right = nullptr;
+            TreeNode* pred = predecessor(cur);
+            if(pred->right == cur) {
+                pred->right = nullptr;
                 cur = cur->right;
+            } else {
+                pred->right = cur;
+                res.push_back(cur->val);
+                cur = cur->left;
             }
         }
         return res;
