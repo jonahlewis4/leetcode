e8d90d707245253d32ed8818fb886917b5c1eec9
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-07-16 20:09:48 -0700

    Time: 0 ms (100%), Space: 7.8 MB (67.73%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.cpp
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int hammingDistance(int x, int y) {
-        const auto & xSet = bitset<32>(x);
-        const auto & ySet = bitset<32>(y);
         int diff = 0;
-        for(int i = 0; i < xSet.size(); i++) {
-            if(xSet[i] != ySet[i]){
+        while(max(x,y) > 0) {
+            int xDig = x % 2;
+            int yDig = y % 2;
+            if(xDig != yDig) {
                 diff++;
             }
+            x >>=1;
+            y >>=1;
         }
         return diff;
     }
