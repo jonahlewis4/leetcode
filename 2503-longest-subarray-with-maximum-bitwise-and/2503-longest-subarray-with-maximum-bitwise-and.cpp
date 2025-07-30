a1a145a672723fdfe7cb0acce3a84b0af5eef2b8
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-07-29 19:50:10 -0700

    Time: 0 ms (100%), Space: 86.1 MB (38.1%) - LeetHub

--- 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
+++ 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
@@ -3,7 +3,6 @@ public:
     int longestSubarray(vector<int>& nums) {
         int maxElem = *max_element(nums.begin(), nums.end());
         int longestConsec = 0;
-        int prev = maxElem - 1;
         int streak = 0;
         for(const auto & num : nums) {
             if(num != maxElem) {
