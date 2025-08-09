507d526ceb776a194adea3cc4dea20dbc2749d6f
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-08-09 12:27:15 -0700

    Time: 0 ms (100%), Space: 12.8 MB (5%) - LeetHub

---
+++ 0762-find-anagram-mappings/0762-find-anagram-mappings.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> anagramMappings(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, stack<int>> iList;
+        for(int i = 0; i < nums2.size(); i++) {
+            iList[nums2[i]].push(i);
+        }
+        vector<int> ans(nums1.size());
+        for(int i = 0; i < nums1.size(); i++) {
+            ans[i] = iList[nums1[i]].top();
+            iList[nums1[i]].pop();
+        }
+        return ans;
+    }
+};
