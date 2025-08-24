50f503d427877d050d0c2d9efa3caf27e0bcc65b
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-08-24 15:05:34 -0700

    Time: 3 ms (87.78%), Space: 12.7 MB (65.88%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,76 +1,34 @@
 class Solution {
-    class SubsetSumCount{
-        static constexpr int MOD = 1e9 + 7;
-        const vector<int>& nums;
-        int target;
-    public:
-        SubsetSumCount(const vector<int>& nums, int target) : nums(nums), target(target) {}
-        int Recursive() const {
-            return recurse(0, 0);
-        }
-        int recurse(int i, int sum) const {
-            if(i >= nums.size() && sum == target){
-                return 1;
-            }
-            if(i >= nums.size() && sum != target) {
-                return 0;
-            }
 
-            int use = recurse(i + 1, sum + nums[i]);
-            int skip = recurse(i + 1, sum);
-            return (use + skip) % MOD;
-        }
-
-        int Memo() {
-            vector<vector<int>> cache(nums.size(), vector<int>(target + 1, -1));
-            return recurseMemo(0, 0, cache);
-        }
-        int recurseMemo(int i, int sum, vector<vector<int>>& cache){
-            if(sum > target) {
-                return 0;
-            }
-            if(i >= nums.size() && sum == target){
-                return 1;
-            }
-            if(i >= nums.size() && sum != target) {
-                return 0;
-            }
-            if(cache[i][sum] != -1){
-                return cache[i][sum];
-            }
-            int use = recurseMemo(i + 1, sum + nums[i], cache);
-            int skip = recurseMemo(i + 1, sum, cache);
-            cache[i][sum] = (use + skip) % MOD;
-            return cache[i][sum];
-        }
-        int Tabulate() {
-            vector<int> dp(target + 1, 0);
-            dp.back() = 1;
-            for(int r = nums.size() - 1; r >= 1; r--){
-                int num = nums[r];
-                for(int c = 0; c + num <= target; c++){
-                    dp[c] += dp[c + num];
-                }
-            }
-            return dp[0] + (nums[0] < dp.size() ? dp[nums[0]] : 0);
-        }
-    };
 public:
-    int findTargetSumWays(vector<int>& nums, int target) {  
-        int sum = 0;
-        for(const auto & num : nums) {
-            sum += num;
+
+    int subSumCount(vector<int>& nums, int target) {
+        vector<int> dp(target + 1, 0);
+        dp.back() = 1;
+        for(int i = nums.size() - 1; i >= 0; i--) {
+            for(int sum = 0; sum <= target; sum++) {
+                int below = dp[sum];
+                int newSum = sum + nums[i];
+                int withNewSum = newSum > target ? 0 : dp[newSum];
+
+                dp[sum] = below + withNewSum;
+            }
         }
-        if(sum < target) {
+        return dp.front();
+}
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+
+        int remaining = sum - target;
+        if(remaining % 2 != 0) {
             return 0;
         }
-
-        if((sum + target) % 2 == 1 || (sum + target) < 0) {
-            return 0;
+        int sumTarget = remaining / 2;
+        if(sumTarget < 0) {
+            return false;
         }
+    
 
-        int newSum = (sum + target) / 2; 
-
-        return SubsetSumCount(nums, newSum).Tabulate();
+        return subSumCount(nums, sumTarget);
     }
 };
