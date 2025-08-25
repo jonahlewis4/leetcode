77855ca36263502f9495ff2e12c0f4c352f084c3
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-08-25 15:03:21 -0700

    Time: 2 ms (81.03%), Space: 10.7 MB (97.06%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -4,59 +4,33 @@ public:
         if(numRows == 1) {
             return s;
         }
-        int numCharsInSection = numRows + numRows - 2;
-        int numSections = (int)std::ceil((double)s.size() / numCharsInSection); 
-        int sectionWidth = numRows - 1;
-        
-        string res(s.size(), '.');
-        int sectionNumber = 0;
-        int i = 0;
-        while(i < s.size()) {
-            int resI = sectionNumber;
-            int curRow = 0;
-            int remainingSections = numSections - sectionNumber - 1; 
-            for(int curRow = 0; curRow < numRows; curRow++) {
-                if(i >= s.size()) {
-                    return res;
-                }
-                res[resI] = s[i];
-                resI++;
-                bool dualRow = curRow != 0 && curRow != numRows - 1;
-                int gap = (numRows - curRow - 1) * 2;
-                if(dualRow) {
-                    //add bonus character
-                    if(i + gap < s.size()) {
-                        res[resI] = s[i + gap];
-                        resI++;
-                    }
-                }
-                //calculate number of chars to right to skip.
-                int numToSkipRight = 0;
-                if(remainingSections > 1) {
-                    int numPerSectionInThisRow = dualRow ? 2 : 1;
-                    numToSkipRight += numPerSectionInThisRow * (remainingSections - 1); 
-                }
-                if(remainingSections > 0) {
-                    int iInFinalSection = numCharsInSection * (numSections - 1);
-                    int iInThisRowFinalSection = iInFinalSection + curRow;
-                    if(iInThisRowFinalSection < s.size()){
-                        numToSkipRight++;
-                        if(dualRow && (iInThisRowFinalSection + gap < s.size())) {
-                            numToSkipRight++;
-                        }
-                    }
-                }
-                
-                //calculate number of chars left to skip
-                int numPerSectionInNextRow = (curRow + 1 == numRows - 1) ? 1 : 2;
-                int numToSkipLeft = numPerSectionInNextRow * sectionNumber;
 
-                int numToSkip = numToSkipRight + numToSkipLeft;
-                resI += numToSkip;
+        string res(s.size(), '.');
+        int numCharsInSection = numRows * 2 - 2;
+
+        int i = 0;
+        for(int r = 0; r < numRows; r++) {
+            int position = r;
+            while(true){
+                if(position >= s.size()) {
+                    break;
+                }
+                res[i] = s[position];
                 i++;
+                int nextSection = position + numCharsInSection;
+
+                if(r != 0 && r != numRows - 1) {
+                    int gap = numCharsInSection - r * 2;
+                    position += gap;
+                    if(position >= s.size()) {
+                        break;
+                    }
+                    res[i] = s[position];
+                    i++;
+                }
+                position = nextSection;
             }
-            sectionNumber++;
-            i += numCharsInSection - numRows;
+        
         }
         return res;
     }
