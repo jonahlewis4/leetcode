661836fa723ddb25fb6fa88b72ff077401a31edf
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-08-26 11:46:31 -0700

    Time: 0 ms (100%), Space: 10.7 MB (58.65%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.cpp
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -1,34 +1,62 @@
 class Solution {
 public:
     int calculate(string s) {
-        int total = 0;
-        int currentNumber = 0;
-        int sign = 1;
-        std::stack<int> stk;
+        
+        //(1 + (4 + 5 + 2) - 3) - (6 - (-4))
+        //9
+        //- 10
 
-        for(const char c : s){
-            if(isdigit(c)){
-                currentNumber *= 10;
-                currentNumber += c - '0';
-            } else if(c == '+' || c == '-'){
-                total += currentNumber * sign;
-                currentNumber = 0;
-                sign = c == '+' ? 1 : -1;
-            } else if (c == '('){
-                stk.push(total);
-                stk.push(sign);
-                total = 0;
-                sign = 1;
-            } else if (c == ')'){
-                total += currentNumber * sign;
-                currentNumber = 0;
-                total *= stk.top();
-                stk.pop();
-                total += stk.top();
-                stk.pop();
+
+        stack<int> stk;
+        int sign = 1;
+        int total = 0;
+        int curVal = 0;
+        stk.push(0);
+        for(const auto & c : s) {
+            switch (c) {
+                case ' ':{
+                    break;
+                }
+                case '(': {
+                    stk.push(total);
+                    stk.push(sign);
+                    sign = 1;
+                    total = 0;
+                    break;
+                }
+                    
+                case ')': {
+                    total += curVal;
+                    curVal = 0;
+                    int localSign = stk.top();
+                    stk.pop();
+                    total = stk.top() + total * localSign;
+                    stk.pop();
+                    break;
+                }
+                
+                case '+': {
+                    total += curVal;
+                    curVal = 0;
+                    sign = 1;
+                    break;
+                }
+                    
+                case '-': {
+                    total += curVal;
+                    curVal = 0;
+                    sign = -1;
+                    break;  
+                }
+                    
+                default: {
+                    int val = c - '0';
+                    curVal *= 10;
+                    curVal += val * sign;
+                }
             }
         }
-        total += currentNumber * sign;
+        total += curVal;
         return total;
     }
 };
