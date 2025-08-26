ca0e458e6f651b9be16e65809ad53d939a20f12e
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-08-26 12:47:18 -0700

    Time: 2 ms (65.28%), Space: 43 MB (11.96%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,6 +1,7 @@
 class Solution {
     String _s;
     String _p;
+    HashMap<Integer, HashMap<Integer, Boolean>> cache = new HashMap<>();
     private boolean starAfter(int pI) {
         if(pI + 1 >= _p.length()) {
             return false;
@@ -30,16 +31,29 @@ class Solution {
             return false;
         }
         
+        HashMap<Integer, Boolean> in = cache.get(sI);
+        if(in == null) {
+            cache.put(sI, new HashMap<Integer, Boolean>());
+            in = cache.get(sI);
+        }
+
+        Boolean in2 = in.get(pI);
+        if(in2 != null) {
+            return in2;
+        }
+
         if(starAfter(pI)){
             boolean res = false;
             if(matches(sI, pI)){
                 res |= match(sI + 1, pI);
             }
             res |= match(sI, pI + 2);
+            in.put(pI, res);
             return res;
         }
         if(matches(sI, pI)) {
-            return match(sI + 1, pI + 1);
+            in.put(pI, match(sI + 1, pI + 1));
+            return in.get(pI);
         }
         return false;
 
