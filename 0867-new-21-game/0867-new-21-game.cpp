54aed4f722e6056cdbf61291f364f2b536e7c2cc
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-08-27 12:26:32 -0700

    Time: 3 ms (61.11%), Space: 13 MB (8.7%) - LeetHub

--- 0867-new-21-game/0867-new-21-game.cpp
+++ 0867-new-21-game/0867-new-21-game.cpp
@@ -3,30 +3,24 @@ class Solution {
     
 public:
     double new21Game(int n, int k, int maxPts) {
-        vector<double> dp(n + maxPts);
-        for(int i = n + 1; i < dp.size(); i++) {
-            dp[i] = 0;
-        }
-        for(int i = k; i <= n; i++) {
-            dp[i] = 1;
-        }
-        for(int i = k - 1; i >= 0; i--) {
-            double total = 0;
-            if(i == k - 1) {
-                for(int card = 1; card <= maxPts; card++) {
-                    int next = i + card;
-                    double prob = dp[next];
-                    total += prob;
-                }
-                total /= maxPts;
-                dp[i] = total;
+        vector<double> dp(n + 1, 0);
+        dp.back() = 0;
+        double sum = 0;
+
+        for(int i = n; i >= 0; i--) {
+            if(i >= k) {
+                dp[i] = 1;
+            } else if(i + maxPts > n){
+                dp[i] = sum / maxPts;
             } else {
-                total = dp[i + 1] * maxPts;
-                total -= dp[i + maxPts + 1];
-                total += dp[i + 1];
-                total /= maxPts;
-                dp[i] = total;
+                dp[i] = sum / maxPts;
             }
+
+            sum += dp[i];
+            if(i + maxPts <= n) {
+                sum -= dp[i + maxPts];
+            }
+
         }
         return dp.front();
     }
