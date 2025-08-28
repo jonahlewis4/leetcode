7a14137beefcfc86ae2dd6c0503f8869260d4fcf
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-08-28 11:58:53 -0700

    Time: 0 ms (100%), Space: 8.5 MB (86.77%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
-        //do not forget this check!!!!!
         if(s1.size() + s2.size() != s3.size()) {
             return false;
         }
+
         if(s1.size() > s2.size()) {
             swap(s1, s2);
         }
-        //s1 is the samller of the two.
 
-        vector<bool> dp(s1.size() + 1, 0);
+        vector<bool> dp(s1.size() + 1);
         dp.back() = true;
-        for(int i = s1.size() - 1; i >= 0; i--) {
-            if(s1[i] == s3[s2.size() + i]){
-                dp[i] = dp[i] | dp[i + 1];
+        for(int i = dp.size() - 2; i >= 0; i--) {
+            dp[i] = dp[i + 1] && s1[i] == s3[s2.size() + i];
+        }
+
+        for(int i2 = s2.size(); i2 >= 0; i2--) {
+            dp.back() = s2[i2] == s3[i2 + s1.size()] && dp.back();
+            for(int i1 = s1.size(); i1 >= 0; i1--) {
+                bool res = false;
+                char target = s3[i1 + i2];
+                if(s1[i1] == target){
+                    res |= dp[i1 + 1];
+                }
+
+                if(s2[i2] == target) {
+                    res |= dp[i1];
+                }
+
+                dp[i1] = res;
             }
         }
 
-        for(int j = s2.size() - 1; j >= 0; j--) {
-            dp.back() = (s3[s1.size() + j] == s2[j]) && dp.back();
-            for(int i = s1.size() - 1; i >= 0; i--) {
-                char interC = s3[i + j];
-                bool res = false;
-                if(interC == s2[j]) {
-                    res |= dp[i];
-                }
-                if(interC == s1[i]) {
-                    res |= dp[i + 1];
-                }
-                dp[i] = res;
-            }
-        }
         return dp.front();
     }
 };
