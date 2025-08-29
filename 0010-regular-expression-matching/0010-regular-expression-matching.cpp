24035eee01ecb54896103d58731899bc46432750
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-08-29 13:52:17 -0700

    Time: 0 ms (100%), Space: 8 MB (89.34%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,60 +1,47 @@
 class Solution {
-    class Regex{
-    private:
-        const string& s;
-        const string& p;
-    public:
-        Regex(const string& s, const string& p) : s(s), p(p) {}
-        bool match(int sI, int pI) const {
-            if(p[pI] == '.' || s[sI] == p[pI]) {
-                return true;
-            }
+    string _s;
+    string _p;
+    bool starAfter(int pI) const {
+        if(pI + 1 >= _p.size()) {
             return false;
         }
-
-        bool starAfter(int pI) const {
-            if(pI + 1 < p.size() && p[pI + 1] == '*'){
-                return true;
-            }
-            return false;
-        }
-    };
+        return _p[pI + 1] == '*';
+    }
+    bool match(int sI, int pI) const {
+        return _s[sI] == _p[pI] || _p[pI] == '.';
+    }
 public:
     bool isMatch(string s, string p) {
+        _s = s;
+        _p = p;
+
         vector<bool> dp(p.size() + 1, false);
         dp.back() = true;
-        
-        Regex reg(s, p);
-        //initialize final row
-        //this represents if we've completed the s string.
-        //if the s string is completed that means the only successful patterns
-        //would be stars, which can be empty (ie ones that don't have s)
 
-        for(int i = dp.size() - 3; i >= 0; i--) {
-            dp[i] = reg.starAfter(i) && dp[i + 2];
+        for(int pI = _p.size() - 2; pI >= 0; pI--) {
+            if(starAfter(pI)){
+                dp[pI] = dp[pI + 2];
+            }
         }
-
         for(int sI = s.size() - 1; sI >= 0; sI--) {
             bool bottomRight = dp.back();
             dp.back() = false;
-
-            for(int pI = p.size() - 1; pI >= 0; pI--) {
+            for(int pI = _p.size() - 1; pI >= 0; pI--) {
                 bool res = false;
-                bool below = dp[pI];
-                if(reg.starAfter(pI)) {
-                    if(reg.match(sI, pI)){ 
-                        res |= below;
+                if(starAfter(pI)){
+                    if(match(sI, pI)) {
+                        res |= dp[pI];
                     }
                     res |= dp[pI + 2];
-                } else if (reg.match(sI, pI)) {
-                    res = bottomRight;
-                } else {
-                    res = false;
-                }
+                } else if (match(sI, pI)) {
+                    res |= bottomRight;
+                }   
+                
+                bottomRight = dp[pI];
                 dp[pI] = res;
-                bottomRight = below;
             }
         }
+
         return dp.front();
     }
 };
