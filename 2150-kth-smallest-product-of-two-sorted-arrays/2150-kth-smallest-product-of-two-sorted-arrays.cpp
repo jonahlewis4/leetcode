7f7a1a2bbc127d9bc3da89144c48de35f0427486
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-09-01 16:51:29 -0700

    Time: 452 ms (81.65%), Space: 98.1 MB (23.6%) - LeetHub

--- 2150-kth-smallest-product-of-two-sorted-arrays/2150-kth-smallest-product-of-two-sorted-arrays.cpp
+++ 2150-kth-smallest-product-of-two-sorted-arrays/2150-kth-smallest-product-of-two-sorted-arrays.cpp
@@ -1,41 +1,71 @@
 class Solution {
 public:
     long long kthSmallestProduct(vector<int>& nums1, vector<int>& nums2, long long k) {
-        if(nums1.size() > nums2.size()){
+        //find largest product which is rank k or less.
+        if(nums1.size() > nums2.size()) {
             swap(nums1, nums2);
         }
-        long long left = -1e10, right = 1e10;
-        while (left < right) {
-            long long mid = left + (right - left) / 2;
-            if (countProducts(nums1, nums2, mid) < k) left = mid + 1;
-            else right = mid;
+        long long front1 = nums1.front();
+        long long front2 = nums2.front();
+        long long back1 = nums1.back();
+        long long back2 = nums2.back();
+
+        long long corner1 = front1 * front2;
+        long long corner2 = front1 * back2;
+        long long corner3 = front2 * back1;
+        long long corner4 = back1 * back2;
+
+        long long minProduct = min({
+            corner1, corner2, corner3, corner4
+        });
+        long long maxProduct = max({
+            corner1, corner2, corner3, corner4
+        });
+
+        long long l = minProduct;
+        long long r = maxProduct;
+
+        long long best = minProduct;
+        
+        while(l <= r) {
+            long long m = (l + r) / 2;
+            long long rank = Rank(nums1, nums2, m);
+            if(rank > k){
+                r = m - 1;
+            } else {
+                best = m;
+                l = m + 1;
+            }
         }
-        return left;
+        return best;   
     }
 
-    long long countProducts(vector<int>& nums1, vector<int>& nums2, long long target) {
-        long long count = 0;
-        for (int num1 : nums1) {
-            if (num1 == 0) {
-                if (target >= 0) count += nums2.size();
-                continue;
-            }
-
-            int low = 0, high = nums2.size();
-            while (low < high) {
-                int mid = (low + high) / 2;
-                long long prod = 1LL * num1 * nums2[mid];
-                if (prod <= target) {
-                    if (num1 > 0) low = mid + 1;
-                    else high = mid;
-                } else {
-                    if (num1 > 0) high = mid;
-                    else low = mid + 1;
-                }
-            }
-
-            count += (num1 > 0) ? low : (nums2.size() - low);
-        }
-        return count;
+    long long Rank(vector<int>& nums1, vector<int>& nums2, long long prod) const {
+        long long rank = 1;
+        for(const auto & num : nums1){
+            int numSmaller = numElementsSmaller(nums2, num, prod);
+            rank += numSmaller;
+        }  
+        return rank;
     }
+
+    static int numElementsSmaller(vector<int>& nums2, int num, long long prod) {
+        if(num == 0) {
+            if(prod <= 0) {
+                return 0;
+            }
+            return nums2.size();
+        }
+
+        if(num < 0) {
+            long long target = floor((double) prod / num);
+            const auto & itr = upper_bound(nums2.begin(), nums2.end(), target);
+            return nums2.end() - itr;
+        }
+
+        long long target = ceil((double) prod / num);
+        const auto & itr = lower_bound(nums2.begin(), nums2.end(), target);
+        return itr - nums2.begin();
+    }
+
 };
