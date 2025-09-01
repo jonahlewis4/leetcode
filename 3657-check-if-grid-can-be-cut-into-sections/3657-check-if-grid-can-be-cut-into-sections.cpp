e8bc5a47425efdb62cdd9181e682c4eef269d96c
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-08-31 18:35:01 -0700

    Time: 51 ms (98.5%), Space: 208.2 MB (66.82%) - LeetHub

---
+++ 3657-check-if-grid-can-be-cut-into-sections/3657-check-if-grid-can-be-cut-into-sections.cpp
@@ -0,0 +1,54 @@
+class Solution {
+    class Checker {
+        struct Interval {
+            int start = 0;
+            int end = 0;
+        };
+    public:
+
+        vector<Interval> intervals;
+        Checker(const vector<vector<int>>& rects, bool vertical){
+            for(const auto & rect : rects) {
+                int start = rect[0 + vertical];
+                int end = rect[2 + vertical];
+
+                intervals.push_back({
+                    .start = start,
+                    .end = end
+                });
+            }
+        }
+
+        bool Check() const {
+            vector<Interval> startCopy = intervals;
+            sort(startCopy.begin(), startCopy.end(), [](const Interval& a, const Interval& b){
+                return a.start < b.start;
+            });
+            
+            Interval ivl = startCopy[0];
+            int cuts = 0;
+            for(const auto & newIvl : startCopy){
+                if(newIvl.start >= ivl.end){
+                    cuts++;
+                    if(cuts == 2){
+                        return true;
+                    }
+                    ivl = newIvl;
+                } else if (newIvl.end > ivl.end) {
+                    ivl.end = newIvl.end;
+                }
+            }
+            return false;
+        }
+    };
+
+public:
+    static bool checkValidCuts(int n, const vector<vector<int>>& rectangles) {
+        bool horiSliceable = Checker(rectangles, false).Check();
+        if(horiSliceable) {
+            return true;
+        }
+        bool vertiSliceable = Checker(rectangles, true).Check();
+        return vertiSliceable;
+    }
+};
