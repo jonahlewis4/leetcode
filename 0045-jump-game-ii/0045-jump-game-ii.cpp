a3145075a669189a3f9b69033c4bb4d644a3c3e2
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-09-03 13:14:42 -0700

    Time: 0 ms (100%), Space: 20.5 MB (37.22%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,37 +1,18 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        if(nums.size() == 1) {
-            return 0;
-        }
-        
-        queue<int> q;
-        vector<bool> enqueued(nums.size() - 1, false);
-
-        q.push(0);
-        enqueued[0] = true;
         int jumps = 0;
-        while(!q.empty()) {
-            int n = q.size();
-            for(int i = 0; i < n; i++) {
-                int idx = q.front();
-                q.pop();
+        int nextStart = 0;
+        int nextEnd = 0;
+        for(int i = 0; i < nums.size() - 1; i++) {
+            nextStart = max(nextStart, i + nums[i]);
 
-                for(int j = idx + 1; j <= idx + nums[idx] && j < nums.size(); j++){
-                    if(j == nums.size() - 1){
-                        return jumps + 1;
-                    }
-                    if(enqueued[j]) {
-                        continue;
-                    }
-                    q.push(j);
-                    enqueued[j] = true;
-                }
+
+            if(i == nextEnd) {
+                nextEnd = nextStart;
+                jumps++;
             }
-            jumps++;
         }
-
-        return -1;
-
+        return jumps;
     }
 };
