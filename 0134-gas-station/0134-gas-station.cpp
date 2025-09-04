170576e070f4328afba9cb6cb0774c03d2a3e544
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-09-04 12:37:40 -0700

    Time: 0 ms (100%), Space: 112.4 MB (42.84%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -13,21 +13,14 @@ public:
 
         //find start of maximum continuous sum
 
-        int maxSum = -1;
-        int curSum = 0;
-        int bestStart = -1;
-        int curStart = 0;
-        for(int i = 0; i < gas.size() * 2; i++) {
+        int curTank = 0;
+        int bestStart = 0;
+        for(int i = 0; i < gas.size(); i++) {
             int idx = i % gas.size();
-            curSum += gas[idx];
-            if(curSum < 0) {
-                curSum = 0;
-                curStart = idx + 1;
-            }
-
-            if(curSum > maxSum) {
-                bestStart = curStart;
-                maxSum = curSum;
+            curTank += gas[idx];
+            if(curTank < 0) {
+                bestStart = i + 1;
+                curTank = 0;
             }
         }
 
