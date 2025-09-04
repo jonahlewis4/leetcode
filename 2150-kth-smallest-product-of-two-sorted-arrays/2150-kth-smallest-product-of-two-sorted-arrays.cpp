0d0a3e2aeccd8a8ddb1d0ebdfe521ef74dcf1c79
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-09-04 14:46:09 -0700

    Time: 100 ms (88.59%), Space: 103.5 MB (11.82%) - LeetHub

--- 2150-kth-smallest-product-of-two-sorted-arrays/2150-kth-smallest-product-of-two-sorted-arrays.cpp
+++ 2150-kth-smallest-product-of-two-sorted-arrays/2150-kth-smallest-product-of-two-sorted-arrays.cpp
@@ -1,14 +1,35 @@
 class Solution {
-public:
-    long long kthSmallestProduct(vector<int>& nums1, vector<int>& nums2, long long k) {
-        //find largest product which is rank k or less.
-        if(nums1.size() > nums2.size()) {
-            swap(nums1, nums2);
+private:
+    int _nums1FirstPos;
+    int _nums2FirstPos;
+    vector<int> _nums1;
+    vector<int> _nums2;
+    int _k;
+    long long _smallestProd;
+    long long _largestProd;
+
+    void SetupSplit() {
+        _nums1FirstPos = _nums1.size();
+        for(int i = 0; i < _nums1.size(); i++) {
+            if(_nums1[i] >= 0){
+                _nums1FirstPos = i;
+                break;
+            }
         }
-        long long front1 = nums1.front();
-        long long front2 = nums2.front();
-        long long back1 = nums1.back();
-        long long back2 = nums2.back();
+        _nums2FirstPos = _nums2.size();
+        for(int i = 0; i < _nums2.size(); i++){
+            if(_nums2[i] >= 0) {
+                _nums2FirstPos = i;
+                break;
+            }
+        }
+    }
+
+    void SetSearchBounds() {
+        long long front1 = _nums1.front();
+        long long front2 = _nums2.front();
+        long long back1 = _nums1.back();
+        long long back2 = _nums2.back();
 
         long long corner1 = front1 * front2;
         long long corner2 = front1 * back2;
@@ -21,51 +42,93 @@ public:
         long long maxProduct = max({
             corner1, corner2, corner3, corner4
         });
-
-        long long l = minProduct;
-        long long r = maxProduct;
-
-        long long best = minProduct - 1;
-        
-        while(l <= r) {
-            long long m = (l + r) / 2;
-            long long rank = Rank(nums1, nums2, m);
-            if(rank > k){
-                r = m - 1;
-            } else {
-                best = m;
-                l = m + 1;
-            }
-        }
-        return best;   
+        _smallestProd = minProduct;
+        _largestProd = maxProduct;
     }
 
-    long long Rank(vector<int>& nums1, vector<int>& nums2, long long prod) const {
-        long long rank = 1;
-        for(const auto & num : nums1){
-            int numSmaller = numElementsSmaller(nums2, num, prod);
-            rank += numSmaller;
-        }  
+    long long Rank(long long prod) const {
+        long long rank = 0;
+        //count values <= prod when both negative
+        for(int i1 = 0, i2 = _nums2FirstPos - 1; i1 < _nums1FirstPos && i2 >= 0;){
+            long long thisProduct = (long long)_nums1[i1] * _nums2[i2];
+            if(thisProduct <= prod) {
+                rank += _nums1FirstPos - i1;
+                i2--;
+            } else {
+                i1++;
+            }
+        }
+        
+        //count values <= prod when both positive
+        for(int i1 = _nums1FirstPos, i2 = _nums2.size() - 1; i1 < _nums1.size() && i2 >= _nums2FirstPos;){
+            long long thisProduct = (long long)_nums1[i1] * _nums2[i2];
+            if(thisProduct <= prod) {
+                rank += i2 - _nums2FirstPos + 1;
+                i1++;
+            } else {
+                i2--;
+            }
+        }
+
+        //count values <= prod when num1 is positive and num2 is negative
+        for(int i1 = _nums1.size() - 1, i2 = _nums2FirstPos - 1; i1 >= _nums1FirstPos && i2 >= 0;){
+            long long thisProduct = (long long)_nums1[i1] * _nums2[i2];
+            if(thisProduct <= prod) {
+                rank += i2 + 1;
+                i1--;
+            } else {
+                i2--;
+            }
+        }
+
+
+        //count values <= prod when num1 is negative and num2 is positive
+        for(int i1 = 0, i2 = _nums2FirstPos; i1 < _nums1FirstPos && i2 < _nums2.size();) {
+            long long thisProduct = (long long)_nums1[i1] * _nums2[i2];
+            if(thisProduct <= prod) {
+                rank += _nums2.size() - i2;
+                i1++;
+            } else {
+                i2++;
+            }
+        }
+        
         return rank;
     }
+public:
+    long long kthSmallestProduct(vector<int>& nums1, vector<int>& nums2, long long k) {
+        _nums1 = nums1;
+        _nums2 = nums2;
+        _k = k;
 
-    static int numElementsSmaller(vector<int>& nums2, int num, long long prod) {
-        if(num == 0) {
-            if(prod <= 0) {
-                return 0;
+        SetupSplit();
+        SetSearchBounds();
+
+        //Rank(-30);
+
+
+        // for(const auto & num : nums1){
+        //     for(const auto & num2 : nums2) {
+        //         long long prod = (long long)num * num2;
+        //         cout<<prod<<" is rank "<<Rank(prod)<<endl;
+        //     }
+        // }
+
+        long long l = _smallestProd;
+        long long r = _largestProd;
+        while(l <= r) {
+            long long m = (l + r) / 2;
+            long long rank = Rank(m);
+
+            if(rank < k) {
+                l = m + 1;
+            } else {
+                r = m - 1;
             }
-            return nums2.size();
         }
+        return l;
+    }       
 
-        if(num < 0) {
-            long long target = floor((double) prod / num);
-            const auto & itr = upper_bound(nums2.begin(), nums2.end(), target);
-            return nums2.end() - itr;
-        }
-
-        long long target = ceil((double) prod / num);
-        const auto & itr = lower_bound(nums2.begin(), nums2.end(), target);
-        return itr - nums2.begin();
-    }
+    
 
 };
