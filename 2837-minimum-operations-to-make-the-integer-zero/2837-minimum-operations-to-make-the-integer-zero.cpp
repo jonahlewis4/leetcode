c2e2e9c764bd8da78b2034f75152c9312a8f0d08
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-09-05 13:09:13 -0700

    Time: 0 ms (100%), Space: 8.3 MB (52.76%) - LeetHub

---
+++ 2837-minimum-operations-to-make-the-integer-zero/2837-minimum-operations-to-make-the-integer-zero.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int makeTheIntegerZero(int num1, int num2) {
+        int k = 1;
+        while(true) {
+            long long x = num1 - ((long long)k * num2);
+            if(x < 0) {
+                return -1;
+            }
+            if(k > x) {
+                return -1;
+            }
+
+            if(__builtin_popcountll(x) <= k) {
+                return k;
+            }
+            k++;
+        }
+        return -1;
+    }
+};
+
