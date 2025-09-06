865613a5bb93dcc733be4861274e71992cff51cf
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-09-06 15:26:14 -0700

    Time: 0 ms (100%), Space: 150.6 MB (65.9%) - LeetHub

--- 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.cpp
+++ 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
     bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
-        bool first = false;
-        bool second = false;
-        bool third = false;
-        
-        int targ1 = target[0];
-        int targ2 = target[1];
-        int targ3 = target[2];
-        
-        for(const vector<int>& triplet : triplets) {
-            int opt1 = triplet[0];
-            int opt2 = triplet[1];
-            int opt3 = triplet[2];
-
-            
-            //if all opts <= target
-            if(opt1 <= targ1 && opt2 <= targ2 && opt3 <= targ3) {
-                first |= opt1 == targ1;
-                second |= opt2 == targ2;
-                third |= opt3 == targ3;
+        //go through each and see if theres one with matching column and none of the 
+        //ones in the find are greater than the target.
+        int n = target.size();
+        for(int i = 0; i < n; i++){
+            bool ithMatch = false;
+            for(int r = 0; r < triplets.size(); r++){
+                if(triplets[r][i] == target[i]){
+                    bool canUse = true;
+                    for(int j = 0; j < n; j++){
+                        if(triplets[r][j] > target[j]){
+                            canUse = false;
+                            break;
+                        }
+                    }
+                    if(canUse){
+                        ithMatch = true;
+                        break;
+                    }
+                }
             }
-
-            if(first && second && third) {
-                return true;
+            if(!ithMatch){
+                return false;
             }
         }
-
-        return false;
+        return true;
     }
 };
