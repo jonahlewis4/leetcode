4be9d65dc1b70732325680f30cfc54801cf73061
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-09-11 16:13:12 -0700

    Time: 0 ms (100%), Space: 20.5 MB (62.99%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
+        if(nums.size() == 1) {
+            return 0;
+        }
+        int end = 0;
         int jumps = 0;
-        int nextStart = 0;
-        int nextEnd = 0;
+
+        int nextEnd = nums.front();
+
         for(int i = 0; i < nums.size() - 1; i++) {
-            nextStart = max(nextStart, i + nums[i]);
 
+            int reachable = i + nums[i];
+            nextEnd = max(reachable, nextEnd);
 
-            if(i == nextEnd) {
-                nextEnd = nextStart;
+            if(i == end) {
+                end = nextEnd;
                 jumps++;
             }
         }
-        return jumps;
-    }
+        return jumps; 
+    }  
 };
