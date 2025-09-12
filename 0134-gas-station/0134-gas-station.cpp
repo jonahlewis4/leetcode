a271c538495aa2c3111130a00812fdc430d4cca9
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-09-12 13:08:10 -0700

    Time: 1 ms (46.31%), Space: 112.4 MB (74.75%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -2,28 +2,23 @@ class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         int sum = 0;
+
+        int endMin = 0;
+        int minSum = gas.front() - cost.front();
+
         for(int i = 0; i < gas.size(); i++) {
-            gas[i] = gas[i] - cost[i];
-            sum += gas[i];
+            int change = gas[i] - cost[i];
+            sum += change;
+            if(sum < minSum) {
+                minSum = sum;
+                endMin = i;
+            }
         }
 
         if(sum < 0) {
             return -1;
         }
 
-        //find start of maximum continuous sum
-
-        int curTank = 0;
-        int bestStart = 0;
-        for(int i = 0; i < gas.size(); i++) {
-            int idx = i % gas.size();
-            curTank += gas[idx];
-            if(curTank < 0) {
-                bestStart = i + 1;
-                curTank = 0;
-            }
-        }
-
-        return bestStart;
+        return (endMin + 1) % gas.size();
     }
 };
