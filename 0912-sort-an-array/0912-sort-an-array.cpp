880f3a634f829c5596acf8a140129621c33fec0f
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-09-22 23:24:59 -0700

    Time: 87 ms (63.64%), Space: 138.8 MB (49.82%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,12 +1,24 @@
 class Solution {
 public:
+    static int maxDig(vector<int>& nums) {
+        int high = 0;
+        for(const int num : nums) {
+            if(num == 0) {
+                continue;
+            }
+            int leadingZs = __builtin_clz(num);
+            int numDigits = 32 - leadingZs;
+            high = max(high, numDigits);
+        }
+        return high;
+    }
     vector<int> sortArray(vector<int>& nums) {       
         int small = *min_element(nums.begin(), nums.end());
         int offset = -small;
         for(auto & num : nums) {
             num += offset;
         } 
-        for(int i = 0; i < 32; i++){
+        for(int i = 0; i < maxDig(nums); i++){
             digSort(nums, i);
         }
         for(auto & num : nums) {
