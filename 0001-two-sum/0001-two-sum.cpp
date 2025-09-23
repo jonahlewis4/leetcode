b8ddc43bc67e41723b04f2e2e3a5d28acb050fe2
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-09-22 17:56:36 -0700

    Time: 3 ms (69.91%), Space: 14.8 MB (35.98%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,12 +3,14 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> vals;
         for(int i = 0; i < nums.size(); i++) {
-            int comp = target - nums[i];
-            if(vals.find(comp) != vals.end()){
+            int num = nums[i];
+            int comp = target - num;
+            if(vals.find(comp) != vals.end()) {
                 return {i, vals[comp]};
             }
-            vals[nums[i]] = i;
+            vals[num] = i;
         }
         return {};
+
     }
 };
