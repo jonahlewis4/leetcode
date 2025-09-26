e0d50f044742e1c3b87ba19c844a65365224a377
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-09-25 20:08:39 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.cpp
+++ 0611-valid-triangle-number/0611-valid-triangle-number.cpp
@@ -3,11 +3,14 @@ public:
     int triangleNumber(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int total = 0;
+        int r = 2;
         for(int i = 0; i < nums.size(); i++) {
             for(int j = i + 1; j < nums.size(); j++){
                 int neededLength = nums[i] + nums[j] - 1;
-                vector<int>::iterator it = (upper_bound(nums.begin() + j + 1, nums.end(), neededLength));
-                int numLTE = it - (nums.begin() + j + 1);
+                while(r < nums.size() && nums[r] <= neededLength){
+                    r++;
+                }
+                int numLTE = r - j - 1;
                 total += numLTE;
             }
         }
