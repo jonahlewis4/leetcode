5c83f65e80676134e4676c79660600e83b46d7ef
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-09-26 00:38:13 -0700

    Time: 0 ms (100%), Space: 20.9 MB (85.11%) - LeetHub

--- 0370-range-addition/0370-range-addition.cpp
+++ 0370-range-addition/0370-range-addition.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     vector<int> getModifiedArray(int length, vector<vector<int>>& updates) {
-        vector<int> sweepLine(length + 1, 0);
+        vector<int> res(length, 0);
         for(const vector<int>& update : updates) {
             int startIdx = update.front();
             int endIdx = update[1];
             int amt = update.back();
-            sweepLine[startIdx] += amt;
-            sweepLine[endIdx + 1] -= amt;
+            res[startIdx] += amt;
+            if(endIdx + 1 < res.size()) {
+                res[endIdx + 1] -= amt;
+            }
         }
-        vector<int> res(length);
-        res.front() = sweepLine.front();
         for(int i = 1; i < res.size(); i++) {
-            res[i] = sweepLine[i] + res[i - 1];
+            res[i] += res[i - 1];
         }
         return res;
 
