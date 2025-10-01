426daa03b57b4dd5395021ad6eba00c0e2c7742d
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-09-30 18:29:28 -0700

    Time: 0 ms (100%), Space: 7.8 MB (80.28%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.cpp
+++ 1518-water-bottles/1518-water-bottles.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
         int used = 0;
-        while(numBottles > 0) {
+        while(true) {
             int changeAble = numBottles / numExchange;
             if(changeAble == 0) {
                 return used + numBottles;
