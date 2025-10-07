f16ae95fe34bb4520b76c9923e32df617f16ddcf
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-10-07 02:11:27 -0700

    Time: 6 ms (97.1%), Space: 93.4 MB (31.18%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.cpp
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -1,80 +1,40 @@
 class Solution {
-    class RadixSort {
-public:
-    static int maxDig(vector<int>& nums) {
-        int high = 0;
-        for(const int num : nums) {
-            if(num == 0) {
-                continue;
-            }
-            int leadingZs = __builtin_clz(num);
-            int numDigits = 32 - leadingZs;
-            high = max(high, numDigits);
-        }
-        return high;
-    }
-    static void sortArray(vector<int>& nums) {       
-        int small = *min_element(nums.begin(), nums.end());
-        int offset = -small;
-        for(auto & num : nums) {
-            num += offset;
-        } 
-        for(int i = 0; i < maxDig(nums); i++){
-            digSort(nums, i);
-        }
-        for(auto & num : nums) {
-            num -= offset;
-        }
-    }
-
-    static void digSort(vector<int>& nums, int d) {
-        vector<vector<int>> c(2);
-        for(int i = 0; i < nums.size(); i++) {
-            int num = nums[i];
-            int dig = (num >> d) & 1;
-            c[dig].push_back(num);
-        }
-
-        int i = 0;
-        for(int j = 0; j <= 1; j++) {
-            for(int k = 0; k < c[j].size(); k++) {
-                nums[i] = c[j][k];
-                i++;
-            }
-        }
-    }
-};
 public:
     int maximumGap(vector<int>& nums) {
-        
         if(nums.size() == 1) {
             return 0;
         }
-        RadixSort::sortArray(nums);
-        int best = 0;
-        for(int i = 1; i < nums.size(); i++) {
-            int delta = nums[i] - nums[i - 1];
-            best = max(delta, best);
-        }
-        return best;
+        int n = nums.size();
+        int maxVal = *max_element(nums.begin(), nums.end());
+        int minVal = *min_element(nums.begin(), nums.end());
 
-    }
+        int bucketSize = ceil((double)(maxVal - minVal) / (n - 1));
 
-    void countSort(vector<int> &nums) {
-        unordered_map<int ,int> count;
-        for(const int num : nums) {
-            count[num]++;
-        }
+        vector<pair<int, int>> buckets(n - 1, {
+            INT_MAX,
+            INT_MIN
+        });
+        for(const int i : nums) {
+            int iAdjusted = i - minVal;
 
-        int resI = 0;
-        for(int i = 0; i <= *(max_element(nums.begin(), nums.end())); i++) {
-            if(!count.contains(i)) {
-                continue;
+            int bucketI = iAdjusted / (bucketSize + 1);
+            buckets[bucketI] = {
+                min(buckets[bucketI].first, i),
+                max(buckets[bucketI].second, i),
+            };
+        } 
+
+        int lastMax = INT_MIN;
+        int largestGap = bucketSize;
+        for(auto & [s, l] : buckets) {
+            if(lastMax != INT_MIN && s != INT_MAX) {
+                int gap = s - lastMax;
+                largestGap = max(gap, largestGap);
             }
-            for(int j = 0; j < count[i]; j++){
-                nums[resI] = i;
-                resI++;
-            }
+            
+            lastMax = max(l, lastMax);        
         }
+        
+        return largestGap;
     }
 };
