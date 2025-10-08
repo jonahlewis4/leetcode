71ed8d1def1d50e80d203ab36283986fb6b01aa4
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-10-07 21:11:09 -0700

    Time: 39 ms (84.25%), Space: 125.1 MB (65.71%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -2,27 +2,18 @@ class Solution {
 public:
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
         sort(potions.begin(), potions.end());
-        vector<pair<int, int>> spellsAndIs(spells.size());
-        for(int i = 0; i < spellsAndIs.size(); i++) {
-            spellsAndIs[i] = {
-                spells[i],
-                i
-            };
-        }
-        sort(spellsAndIs.begin(), spellsAndIs.end());
 
         vector<int> res(spells.size());
-        int potionI = potions.size() - 1;
-        for(int i = 0; i < spellsAndIs.size(); i++) {
-            long long spell = (long long)spellsAndIs[i].first;
-            int idx = spellsAndIs[i].second;
+        for(int i = 0; i < spells.size(); i++) {
+            int spell = spells[i];
+            long long minPotion = ceil((double)success / spell);
 
-            while(potionI >= 0 && spell * potions[potionI] >= success){
-                potionI--;
-            }
+            //get iterator to first value minPotion or greater
+            auto itr = lower_bound(potions.begin(), potions.end(), minPotion);
+            //itr is the first valid option
 
-            int diff = potions.size() - potionI - 1;
-            res[idx] = diff;
+            int usable = potions.end() - itr;
+            res[i] = usable;
         }
 
         return res;
