8c52285418c1b0ac119a35e1a2e388fede24bbff
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-10-12 01:10:46 -0700

    Time: 0 ms (100%), Space: 11.3 MB (46.6%) - LeetHub

---
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        int start = 0x40000000;
+        int res = 0;
+        while(start > 0) {
+            if((left & start) == (right & start)){
+                res += (left & start) / start * start; 
+            } else {
+                return res;
+            }
+            start /= 2;
+        }
+        return res;
+    }
+};
