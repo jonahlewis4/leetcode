b3391daf04b8a2a88c7848b639b11a4bc80c10fb
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-10-13 00:31:38 -0700

    Time: 0 ms (100%), Space: 17.3 MB (27.17%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
@@ -1,18 +1,37 @@
 class Solution {
+    bool isAna(string a, string b) {
+        if(a.size() != b.size()){
+            return false;
+        }
 
+        vector<char> ac(26, 0);
+        
+        for(const char c : a) {
+            ac[c - 'a']++;
+        }
+
+        vector<char> bc(26, 0);
+        for(const char c : b){
+            bc[c - 'a']++;
+            if(bc[c - 'a'] > ac[c - 'a']) {
+                return false;
+            }
+        }
+
+        return true;
+
+        
+    }
 public:
     vector<string> removeAnagrams(vector<string>& words) {
         vector<string> res;
-    
         res.push_back(words.front());
         for(int i = 1; i < words.size(); i++){
-            string second = res.back();
-            string third = words[i];
-            sort(second.begin(), second.end());
-            sort(third.begin(), third.end());
-            if(second == third) {
+            string& second = res.back();
+            string& third = words[i];
+            if(isAna(second, third)) {
             } else {
-                res.push_back(words[i]);
+                res.push_back(third);
             }
         }
         return res;
