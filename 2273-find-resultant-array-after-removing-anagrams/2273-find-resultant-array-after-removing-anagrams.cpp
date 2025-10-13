ad3eb71090c424917dc40e5a6aa21302f92067fc
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-10-13 00:28:24 -0700

    Time: 6 ms (22.11%), Space: 15.9 MB (88.15%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
@@ -1,37 +1,18 @@
 class Solution {
-    bool isAna(string a, string b) {
-        if(a.size() != b.size()){
-            return false;
-        }
 
-        unordered_map<char, int> ac;
-        
-        for(const char c : a) {
-            ac[c]++;
-        }
-
-        unordered_map<char, int> bc;
-        for(const char c : b){
-            bc[c]++;
-            if(bc[c] > ac[c]) {
-                return false;
-            }
-        }
-
-        return true;
-
-        
-    }
 public:
     vector<string> removeAnagrams(vector<string>& words) {
         vector<string> res;
+    
         res.push_back(words.front());
         for(int i = 1; i < words.size(); i++){
-            string& second = res.back();
-            string& third = words[i];
-            if(isAna(second, third)) {
+            string second = res.back();
+            string third = words[i];
+            sort(second.begin(), second.end());
+            sort(third.begin(), third.end());
+            if(second == third) {
             } else {
-                res.push_back(third);
+                res.push_back(words[i]);
             }
         }
         return res;
