1d6339a1d272e4bc60370e94439d61e6accc49d5
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-10-13 22:28:52 -0700

    Time: 0 ms (100%), Space: 13.3 MB (76.18%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,42 +1,27 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
         int rowCount = matrix.size();
         int colCount = matrix.front().size();
-
-        //find which row that the target belongs in
-
-        int bottom = 0;;
-        int top = rowCount - 1;
-
-        int row = -1;
-        while(bottom <= top) {
-            int mid = (top + bottom) / 2;
-            if(target >= matrix[mid].front() && target <= matrix[mid].back()){
-                row = mid;
-                break;
-            } else if (target >= matrix[mid].back()) {
-                bottom = mid + 1;
-            } else {
-                top = mid - 1;
-            }
-        }
-        if(row == -1) {
-            return false;
-        }
-
-        const vector<int>& theRow = matrix[row];
+        
         int l = 0;
-        int r = theRow.size() - 1;
+        int r = rowCount * colCount - 1;
         while(l <= r) {
-            int m = (l + r) / 2;
-            int value = theRow[m];
-            if(value == target) {
+            int m = l + (r - l)/2;
+            
+            //flatten m to row and column
+            int row = m / colCount;
+            int col = m - row * colCount; 
+
+            int searchValue = matrix[row][col];
+
+            if(target == searchValue) {
                 return true;
-            } else if (value < target){
-                l = m + 1;
-            } else {
+            } else if (target < searchValue) {
                 r = m - 1;
+            } else {
+                l = m + 1;
             }
         }
 
