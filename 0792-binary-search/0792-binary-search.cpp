8189717fb3964cb6c5db31728da235d3f6df6fc3
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-10-13 17:39:56 -0700

    Time: 0 ms (100%), Space: 31.3 MB (79.76%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int l = 0;
-        int r = nums.size() - 1;
-        while(l <= r){
-            int m = (l + r) / 2;
-            if(nums[m] == target){
-                return m;
-            }
-            else if (nums[m] < target){
-                l = m + 1;
-            } else {
-                r = m - 1;
-            }
+        auto itr = lower_bound(nums.begin(), nums.end(), target);
+        if(itr == nums.end()) {
+            return -1;
         }
-        return -1;
+
+        if(*itr != target) {
+            return -1;
+        }
+        
+        return itr - nums.begin();
+
     }
 };
