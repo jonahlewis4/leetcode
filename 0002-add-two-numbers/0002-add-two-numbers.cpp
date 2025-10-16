bbbb123ef09a0c044272de97c6fb3101684d38f9
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-10-16 01:52:47 -0700

    Time: 0 ms (100%), Space: 76.5 MB (98.08%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,53 +11,34 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* cur = nullptr;
         int carry = 0;
-        ListNode* head;
-        while(l1 != nullptr || l2 != nullptr){
-            if(l1 != nullptr && l2 != nullptr){
-                int sum = l1->val + l2->val + carry;
-                goofyAhAppend(head, cur, sum, carry);
-                l1 = l1->next;
-                l2 = l2->next;
-            } else if (l1 != nullptr){
-                int sum = l1->val + carry;
-                goofyAhAppend(head, cur, sum, carry);
-                l1 = l1->next;
-            } else {
-                int sum = l2->val + carry;
-                goofyAhAppend(head, cur, sum, carry);
+        ListNode* returnValue = l1;
+        ListNode* prev = nullptr;
+        while(l1 != nullptr) {
+            int sum = l1->val;
+            if(l2 != nullptr) {
+                sum += l2->val;
+            }
+            sum += carry;
+            int digit = sum % 10;
+            carry = sum / 10;
+            l1->val = digit;
+
+            prev = l1;
+            l1 = l1->next;
+            if(l2 != nullptr) {
                 l2 = l2->next;
             }
+
+            if(l1 == nullptr && l2 != nullptr) {
+                prev->next = l2;
+                l1 = l2;
+                l2 = nullptr;
+            }
         }
-
-        if(carry > 0){
-            goofyAhAppend(head, cur, 1, carry);
+        if(carry > 0) {
+            prev->next = new ListNode(1);
         }
-        return head;
-
-
+        return returnValue;
     }
-
-    void goofyAhAppend(ListNode* &head, ListNode* &cur, int sum, int &carry){
-        int sumRem = sum % 10;
-        carry = sum / 10;
-        if(cur == nullptr){
-            cur = new ListNode(sumRem);
-            head = cur;
-        } else {
-            cur -> next = new ListNode(sumRem);
-            cur = cur->next;
-        }
-    }
-
-    int listLength (ListNode* head){
-        ListNode* cur = head;
-        int len = 0;
-        while(cur != nullptr){
-            cur = cur->next;
-        }
-        return len;
-    }
-
 };
