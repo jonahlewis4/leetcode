1d67fd6ebbc41c716d80c9aef6ca9eb26ef32dd2
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-10-22 03:09:38 -0700

    Time: 5 ms (86.2%), Space: 9.9 MB (98.59%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -2,14 +2,17 @@ class Solution {
 public:
     int change(int amount, vector<int>& coins) {
         vector<int> dp(amount + 1, 0);
-        dp[0] = 1; // Base case: 1 way to make amount 0
+        dp.front() = 1;
 
-        for (int coin : coins) {
-            for (int amt = coin; amt <= amount; ++amt) {
-                dp[amt] = min((long long)INT_MAX, dp[amt] + (long long)dp[amt - coin]);
+        for(int coinIdx = coins.size() - 1; coinIdx >= 0; coinIdx--) {
+            for(int amt = coins[coinIdx]; amt <= amount; amt++){
+                int below = dp[amt];
+                int leftIdx = amt - coins[coinIdx];
+                int left = dp[leftIdx];
+                dp[amt] = min((long long) INT_MAX , (long long)below + left);
             }
         }
 
-        return dp[amount];
+        return dp.back();
     }
 };
