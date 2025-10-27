a03f0df6937b89bd5f5a491541c2e7cd263d5fbd
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-10-27 00:49:29 -0700

    Time: 83 ms (61.99%), Space: 45.3 MB (47.04%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -29,21 +29,18 @@ public:
             .dest = k,
             .weight = 0
         });
-        vector<bool> visited(n + 1, false);
-        int visitCount = 0;
 
-        while(!pq.empty() && visitCount < n){
+
+        while(!pq.empty()){
             edge e = pq.top();
             pq.pop();
-            if(visited[e.dest]){
+            if(dist[e.dest] < e.weight){
                 continue;
             }
-            visited[e.dest] = true;
-            visitCount++;
             dist[e.dest] = e.weight;
             for(auto const & n : adjList[e.dest]){
                 int newWeight = e.weight + n.weight;
-                if(newWeight < dist[n.dest] && !visited[n.dest]){
+                if(newWeight < dist[n.dest]){
                     pq.push({
                         .dest = n.dest,
                         .weight = newWeight
