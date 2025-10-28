aea0f42f333f58a4d2b524531ff43d1da3274a56
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-10-27 23:01:19 -0700

    Time: 7 ms (44.95%), Space: 22 MB (80.73%) - LeetHub

--- 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.cpp
+++ 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.cpp
@@ -1,41 +1,27 @@
 class Solution {
 public:
     int countValidSelections(vector<int>& nums) {
-        int total = 0;
+        int ans = 0;
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        int leftSum = 0;
+        int rightSum = sum;
+
         for(int i = 0; i < nums.size(); i++) {
-            int num = nums[i];
-            if(num == 0) {
-                total += check2(nums, i, 1);
-                total += check2(nums, i, -1);
-            }
-        }
-        return total;
-    }
+            if(nums[i] == 0){
+                int rml = rightSum - leftSum;
+                int lmr = leftSum - rightSum;
+                if(rml == 0 || rml == 1) {
+                    ans++;
+                }
 
-    int check2(vector<int> nums, int i, int dir) {
-        return check(nums, i, dir);
-    }
-
-    int check(vector<int>& nums, int i, int dir){
-        if(i < 0 || i >= nums.size()){
-            bool nonZero = false;
-            for(const int num : nums) {
-                if(num != 0){
-                    nonZero = true;
-                    break;
+                if(lmr == 0 || lmr == 1){
+                    ans++;
                 }
             }
-
-            return !nonZero;
+            leftSum += nums[i];
+            rightSum -= nums[i];
         }
 
-        int num = nums[i];
-        if(num == 0) {
-            return check(nums, i + 1 * dir, dir);
-        }
-
-        nums[i]--;
-        dir = -dir;
-        return check(nums, i + 1 * dir, dir);
+        return ans;
     }
 };
