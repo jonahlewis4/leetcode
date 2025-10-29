d80774b0a334f5f78e6ca9523a25dff5f128db0b
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-10-28 21:25:42 -0700

    Time: 0 ms (100%), Space: 8.1 MB (99.74%) - LeetHub

--- 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.cpp
+++ 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.cpp
@@ -1,18 +1,8 @@
 class Solution {
-    static inline vector<int> set = {
-        0b1, 
-        0b11,
-        0b111,
-        0b1111,
-        0b11111,
-        0b111111,
-        0b1111111,
-        0b11111111,
-        0b111111111,
-        0b1111111111
-    };
 public:
     int smallestNumber(int n) {
-        return *lower_bound(set.begin(), set.end(), n);
+        int numBits = 32 - __builtin_clz(n);
+        int next =  1 << numBits;
+        return next - 1;
     }
 };
