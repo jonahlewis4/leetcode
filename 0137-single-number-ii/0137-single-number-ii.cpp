2d42d9ef064315a9b601d7c382ff6ce8a656d0bf
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-10-28 23:44:42 -0700

    Time: 0 ms (100%), Space: 13.3 MB (66.41%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -4,13 +4,13 @@ public:
         int count = 0;
 
         for(int digNum = 0; digNum < 32; digNum++){
-            int thisDigit = 0;
+            uint thisDigit = 0;
             for(const auto & num : nums) {
-                int numDigit = (num & (1 << digNum)) >> digNum;
+                uint numDigit = (num >> digNum) & 1;
                 thisDigit += numDigit;
             }
 
-            count |= (thisDigit % 3) << digNum;
+            count |= ((thisDigit % 3) << digNum);
         }
 
         return count;
