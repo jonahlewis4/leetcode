b9bf8ac70240eb3b81536710ed675961d4b12257
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-10-28 23:03:29 -0700

    Time: 0 ms (100%), Space: 13.5 MB (38.2%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        vector<int> count(32, 0);
-        for(const int num : nums) {
-            uint num2 = num;
-            int mPlier = 0;
-            while(num2 > 0){
-                int digit = num2 % 2;
-                if(digit){
-                    count[mPlier]++;
-                    count[mPlier] %= 3;
-                }
-                mPlier++;
-                num2 /= 2;
+        int count = 0;
+
+        for(int digNum = 0; digNum < 32; digNum++){
+            int thisDigit = 0;
+            for(const auto & num : nums) {
+                int numDigit = (num & (1 << digNum)) >> digNum;
+                thisDigit += numDigit;
             }
+
+            count |= (thisDigit % 3) << digNum;
         }
 
-        int res = 0;
-        uint mPlier = 1;
-        for(int i = 0; i < count.size(); i++){
-            int amt = count[i];
-            res += mPlier * amt;
-            mPlier *= 2;
-        }
-
-        return res;
-
-
+        return count;
     }
 };
