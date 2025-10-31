5d5a2fc25165b0ff1ef62f727f815c837ff30e16
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-10-30 20:54:08 -0700

    Time: 0 ms (100%), Space: 7.9 MB (6.47%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.cpp
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -2,12 +2,6 @@ class Solution {
 public:
     int bulbSwitch(int n) {
         
-        for(int i = 1; true; i++){
-            if(i * i > n){
-                return i - 1;
-            }
-        }
-
-
+        return sqrt(n);
     }
 };
