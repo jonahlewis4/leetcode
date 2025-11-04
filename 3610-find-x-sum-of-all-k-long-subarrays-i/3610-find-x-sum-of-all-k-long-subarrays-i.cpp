ea5e56090a5bdd6c9755a273077f51a5c1777f5e
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-11-03 17:29:48 -0800

    Time: 24 ms (23.32%), Space: 36.3 MB (40.26%) - LeetHub

---
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    vector<int> findXSum(vector<int>& nums, int k, int x) {
+        vector<int> res(nums.size() - k + 1);
+        for(int i = 0; i < res.size(); i++) {
+            int end = i + k;
+
+            res[i] = xSum(nums, i, end, x);
+        }
+
+        return res;
+    }
+
+    int xSum(const vector<int>& nums, int begin, int end, int x) {
+        unordered_map<int, int> freq;
+        for(int i = begin; i < end; i++) {
+            freq[nums[i]]++;
+        }
+
+        int sum = xLargestSum(freq, x);
+        return sum;
+    }
+
+    int xLargestSum(unordered_map<int, int>& freq, int x) {
+
+        const auto &cmp = [&freq](const int a, const int b){
+            int c1 = freq[a];
+            int c2 = freq[b];
+
+            if(c1 < c2) {
+                return false;
+            }
+            if(c1 > c2) {
+                return true;
+            }
+
+            return a > b;
+        };
+        priority_queue<int, vector<int>, decltype(cmp)> pq(cmp);
+
+
+        for(const auto & [num, count] : freq) {
+            if(pq.size() < x) {
+                pq.push(num);
+            } else {
+                int topNum = pq.top();
+                int topCount = freq[topNum];
+                
+                if(count > topCount || (count == topCount && num > topNum)){
+                    pq.pop();
+                    pq.push(num);
+                }
+            }
+        }
+    
+        int sum = 0;
+        while(!pq.empty()){
+            int num = pq.top();
+            pq.pop();
+            int count = freq[num];
+            int localSum = num * count;
+            sum += localSum;
+        }
+
+        return sum;
+    }
+};
