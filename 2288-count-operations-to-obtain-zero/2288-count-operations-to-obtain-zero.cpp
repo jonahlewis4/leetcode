66de29d553a042aca0741a66f587e8efd2155619
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-11-08 17:37:26 -0800

    Time: 0 ms (100%), Space: 7.9 MB (71.74%) - LeetHub

--- 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.cpp
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int countOperations(int num1, int num2) {
-        for(int i = 0; true; i++) {
-            if(num1 < num2) {
-                swap(num1, num2);
-            }
-
+        for(int i = 0; true; ) {
             if(num1 == 0 || num2 == 0) {
                 return i;
             }
 
-            num1 -= num2;
+            i += num1 / num2;
+            num1 %= num2;
+
+            swap(num1, num2);
+
 
         }
     }
