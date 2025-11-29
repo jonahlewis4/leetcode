5a5fdf46f69d04522fc0f5585fe136c865940dbe
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-11-29 01:14:51 -0800

    Time: 0 ms (100%), Space: 22.5 MB (45.74%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.cpp
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -1,32 +1,27 @@
 class Solution {
     bool possible(const vector<int>& citations, int h) {
-        //get number smaller
-        auto itr = upper_bound(citations.begin(), citations.end(), h - 1);
-        //itr is first number >= h
-
-        //count num elements BEFORE itr
-        int numBefore = itr - citations.begin();
-
-        int numWith = citations.size() - numBefore;
-        return numWith >= h;
-
+        int numFrom = citations.size() - h;
+        if(h >= citations.size()) {
+            return citations.front() >= h;
+        }
+        return citations[h] >= numFrom; 
     }
 public:
     int hIndex(vector<int>& citations) {
         int l = 0;
-        int r = citations.size();
+        int r = citations.size() - 1;
 
         while(l <= r) {
             int mid = l + (r - l) / 2;
             bool valid = possible(citations, mid);
 
             if(valid) {
-                l = mid + 1;
-            } else {
                 r = mid - 1;
+            } else {
+                l = mid + 1;
             }
         }
 
-        return r;
+        return citations.size() - l;
     }
 };
