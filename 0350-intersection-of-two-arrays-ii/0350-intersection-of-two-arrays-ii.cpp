a68a89d11e26283fa8678337ebf6403b1fe87a5b
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-12-03 04:21:59 -0800

    Time: 3 ms (32.01%), Space: 14.8 MB (41.01%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        if(nums1.size() > nums2.size()) {
+            swap(nums1, nums2);
+        }
+
+        unordered_map<int, int> count;
+        for(const int i : nums1) {
+            count[i]++;
+        }
+
+        vector<int> res;
+        for(const int i : nums2) {
+            if(count[i] > 0){
+                count[i]--;
+                res.push_back(i);
+            }
+        }
+
+        return res;
+    }
+};
