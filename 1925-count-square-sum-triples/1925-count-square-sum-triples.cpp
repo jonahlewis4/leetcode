9565df397583a7e77a4607074b84e88d45a609a0
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-12-07 20:53:43 -0800

    Time: 84 ms (25.22%), Space: 7.9 MB (21.68%) - LeetHub

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    static bool isPerfectSquare(int n) {
+        for(int i = 1; i * i <= n; i++) {
+            if(i * i == n) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+public:
+    int countTriples(int n) {
+        int count = 0;
+        for(int a = 1; a <= n; a++) {
+            for(int b = 1; sqrt(b * b + a * a) <= n; b++) {
+                int cSquared = b * b + a * a;
+                if(isPerfectSquare(cSquared)) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
