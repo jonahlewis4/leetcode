071b3115ad665095df669a7b5a965b72e730f100
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-01-24 18:14:15 -0800

    Time: 3 ms (21.08%), Space: 11.1 MB (42.81%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        unordered_map<int, int> count;
+        for(const int i : arr1) {
+            count[i]++;
+        }
+
+        vector<int> res(arr1.size(), 0);
+        int j = 0;
+        for(int i = 0; i < arr2.size(); i++) {
+            int val = arr2[i];
+            int c = count[val];
+            for(int k = 0; k < c; k++) {
+                res[j] = val;
+                j++;
+            }
+            count.erase(val);
+        }
+
+        int sortStart = j;
+        while(!count.empty()) {
+            const auto& [val, c] = *count.begin();
+            cout<<"val: "<<val<<endl<<"c: "<<c<<endl;
+            for(int k = 0; k < c; k++) {
+                res[j] = val;
+                j++;
+            }
+            count.erase(val);
+        }
+
+        sort(res.begin() + sortStart, res.end());
+
+        return res;
+    }
+};
