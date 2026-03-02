41d6f1e0b31ce6d7bc53dfdb81558cd727ab0d69
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-03-02 13:11:37 -0800

    Time: 31 ms (6.23%), Space: 17.4 MB (8.06%) - LeetHub

---
+++ 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkDistances(string s, vector<int>& distance) {
+        vector<vector<int>> arr(26);
+        for(int i = 0; i < s.size(); i++) {
+            char c = s[i];
+            arr[c - 'a'].push_back(i);
+        }
+
+        for(int i = 0; i < distance.size(); i++) {
+            int d = distance[i];
+            int trueDist = arr[i].empty() 
+                ? d 
+                : arr[i].back() - arr[i].front() - 1;
+            cout<<trueDist<<endl;
+            cout<<d<<endl;
+            cout<<i<<endl;
+            if(trueDist != d) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
