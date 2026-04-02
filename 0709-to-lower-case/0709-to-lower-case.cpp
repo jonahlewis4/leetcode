172ef7c0ce5d3f2be7b4e6dabbffd06e1852526e
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-04-01 18:11:57 -0700

    Time: 0 ms (100%), Space: 8.1 MB (81.83%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.cpp
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -1,4 +1,12 @@
 class Solution {
+private:
+    static char tolower(const char c) {
+        if(c >= 'A' && c <='Z') {
+            return c - 'A' + 'a';
+        }
+        return c;
+
+    }
 public:
     string toLowerCase(string s) {
         for(char& c : s) {
