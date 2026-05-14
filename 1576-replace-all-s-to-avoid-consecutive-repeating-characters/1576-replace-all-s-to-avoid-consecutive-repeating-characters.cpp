544043a03b8fad1c1091f68c0c35a4f0dc294e5d
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-05-14 14:39:38 -0700

    Time: 0 ms (100%), Space: 8.5 MB (13.71%) - LeetHub

---
+++ 1576-replace-all-s-to-avoid-consecutive-repeating-characters/1576-replace-all-s-to-avoid-consecutive-repeating-characters.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string modifyString(string s) {
+        if(s.front() == '?') {
+            s.front() = 'a';
+            if(s.size() > 1 && s[1] == 'a') {
+                s.front() = 'b';
+            }
+        }
+
+        if(s.back() == '?') {
+            s.back() = 'a';
+            if(s.size() > 1 && s[s.size() - 2] == 'a') {
+                s.back() = 'b';
+            }
+        }
+        for(int i = 1; i < s.size(); i++) {
+            if(s[i] == '?') {
+                s[i] = 'a';
+                if(s[i - 1] == 'a' || s[i + 1] == 'a') {
+                    s[i] = 'b';
+                    if(s[i - 1] == 'b' || s[i + 1] == 'b') {
+                        s[i] = 'c';
+                    }
+                }
+            }
+        }
+        return s;
+    }
+};
