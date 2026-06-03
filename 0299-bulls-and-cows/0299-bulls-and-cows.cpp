13224eebf6f84e6b3dbe38cf9403f3711d2bb996
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-06-02 17:11:00 -0700

    Time: 0 ms (100%), Space: 8.9 MB (55.31%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.cpp
+++ 0299-bulls-and-cows/0299-bulls-and-cows.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     string getHint(string secret, string guess) {
-        unordered_multiset<char> cowCandidates;
+        std::array<int, 10> cowCandidates{};
+        int bulls = 0;
         for(int i = 0; i < secret.size(); i++) {
             if(secret[i] != guess[i]) {
-                cowCandidates.insert(secret[i]);
+                int idx = secret[i] - '0';
+                cowCandidates[idx]++;
+            } else {
+                bulls++;
             }
         }
-        int bulls = secret.size() - cowCandidates.size();
         int cows = 0;
         for(int i = 0; i < secret.size(); i++) {
             if(secret[i] != guess[i]) {
-                auto itr = cowCandidates.find(guess[i]);
-                if(itr != cowCandidates.end()) {
-                    cowCandidates.erase(itr);
+                int idx = guess[i] - '0';
+                int remaining = cowCandidates[idx];
+                if(remaining > 0) {
                     cows++;
+                    cowCandidates[idx]--;
                 }
             }
         }
