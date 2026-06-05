94d8b520fc5b8ef8ad900471bb410a56183a3f94
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-06-05 13:30:50 -0700

    Time: 0 ms (100%), Space: 8 MB (36.27%) - LeetHub

--- 0868-binary-gap/0868-binary-gap.cpp
+++ 0868-binary-gap/0868-binary-gap.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int binaryGap(int n) {
-        if(__builtin_popcount(n) == 1) {
-            return 0;
-        }
 
         int lastOne = -1;
         int maxGap = 0;
         while(n > 0) {
             int digit = n & 1;
-            cout<<digit<<endl;
             if(digit == 1) {
                 if(lastOne == -1) {
                     lastOne = 1;
