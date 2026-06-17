eb5444a9b277fd30c76a7964e35c03331cbf4637
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-06-17 16:34:46 -0700

    Time: 0 ms (100%), Space: 115.6 MB (91.54%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        if(nums.size() < 3) {
-            return false;
-        }
-        vector<int> smallestBefore(nums.size());
-        vector<int> largestBefore(nums.size());
-        int smallest = INT_MAX;
-        for(int i = 0; i < nums.size(); i++) {
-            smallestBefore[i] = smallest;
-            smallest = min(smallest, nums[i]);
-        }
-        int largest = INT_MIN;
-        for(int i = nums.size() - 1; i >= 0; i--) {
-            largestBefore[i] = largest;
-            largest = max(largest, nums[i]);
-        }
- 
-        for(int i = 1; i <= nums.size() - 2; i++) {
-            if(smallestBefore[i] < nums[i] && largestBefore[i] > nums[i]) {
+        int smallest = nums.front();
+        int num2Beat = INT_MAX;
+
+        for(int i = 1; i < nums.size(); i++) {
+            int num = nums[i];
+            if(num < smallest) {
+                smallest = num;
+            }
+
+            if(num < num2Beat && num > smallest){
+                num2Beat = num;
+            }
+
+            if(num > num2Beat) {
                 return true;
             }
         }
 
         return false;
+        
     }
 };
