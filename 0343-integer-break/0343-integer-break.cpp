bdb11a0f5261c81de1c614892cda883ae44acc55
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-07-04 16:18:29 -0700

    Time: 0 ms (100%), Space: 8.5 MB (51.55%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,33 +1,37 @@
 class Solution {
-    vector<int> c;
-    int h(int n) {
-    
-        if(c[n]){
-            return c[n];
-        }
-
-        int best = n;
-        for(int i = 1; i < n; i++) {
-            int remaining = n - i;
-            int sub = h(remaining);
-            int product = i * sub;
-            best = max(product, best);
-        }
-
-        c[n] = best;
-        return best;
-    }
 public:
     int integerBreak(int n) {
-        c.resize(n, 0);
-        int best = 0;
-        for(int i = 1; i < n; i++) {
-            int remaining = n - i;
-            int sub = h(remaining);
-            int product = i * sub;
-            best = max(product, best);
+        if(n == 2) {
+            return 1;
+        }
+        if(n == 3) {
+            return 2;
         }
 
-        return best;
+        vector<int> c(n + 1);
+        c[2] = 2;
+        c[1] = 1;
+        c[3] = 3;
+        for(int i = 4; i <= n; i++) {
+            int best = 0;
+            for(int j = 1; j < i; j++) {
+                int k = i - j;
+                int product = c[k] * c[j];
+                best = max(best, product);
+            }
+            c[i] = best;
+        }
+        return c.back();
+
+        // c.resize(n, 0);
+        // int best = 0;
+        // for(int i = 1; i < n; i++) {
+        //     int remaining = n - i;
+        //     int sub = h(remaining);
+        //     int product = i * sub;
+        //     best = max(product, best);
+        // }
+
+        // return best;
     }
 };
