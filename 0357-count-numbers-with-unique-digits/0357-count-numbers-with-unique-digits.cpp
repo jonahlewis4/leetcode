d66ad6204cb19f17c149c1e8f60931bb01438ff8
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-07-05 17:34:04 -0700

    Time: 0 ms (100%), Space: 7.9 MB (19.6%) - LeetHub

--- 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
@@ -1,55 +1,19 @@
 class Solution {
-    static int getRepeated(int n) {
-        if(n <= 1) {
-            return 0;
-        }
-
-
-        if(n == 2) {
-            return 9;
-        }
-        
-        int total = 0;
-        for(int i = 1; i <= n - 1; i++) {
-            int subTotal = 0;
-            for(int j = i + 1; j <= n; j++) {
-                //i j 10 10
-                //i 9 j 10
-                //i 9 8 j
-                //9 i j 10
-                //9 i 8 j
-                //9 8 i j
-
-                //i j 10
-                //i 9 j
-                //9 i j
-
-                int iCon = 9;
-                int jCon = 1;
-                int afterJCon = max(1.0, pow(10, (n - j)));
-                
-                int r = j - 2;
-                int beforeICon = 1;
-                for(int k = 1; k <= r; k++) {
-                    beforeICon *= (9 - k + 1);
-                }
-                int subSubTotal = iCon * jCon * beforeICon * afterJCon;
-                //cout<<i<<" "<<j<<" "<<subSubTotal<<endl;
-                subTotal += subSubTotal;
-            }
-            total += subTotal;
-        }
-        //cout<<"_____________"<<endl;
-        return total;
-    
-        
-    }
 public:
     int countNumbersWithUniqueDigits(int n) {
-        int repeated = 0;
-        for(int i = 1; i <= n; i++) {
-            repeated += getRepeated(i);
+        if(n == 0) {
+            return 1;
         }
-        return pow(10, n) - repeated;
+        
+        int total = 10;
+        int last = 9;
+        for(int i = 2; i <= n; i++) {
+            last = last * (11 - i);
+            total += last;
+        }
+
+        return total;
+         
+
     }
 };
