d8d6d8880607f29e24e028d53e112a4c3c77f84e
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-07-09 13:31:08 -0700

    Time: 0 ms (100%), Space: 8.1 MB (5.19%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+        vector<int> getDivisors(int num) {
+            if(num == 1) {
+                return {};
+            }
+            vector<int> res{1};
+            for(int i = 2; i * i <= num; i++){
+                if(num % i == 0) {
+                    res.push_back(i);
+                    if(num/i != i) {
+                        res.push_back(num / i);
+
+                    }
+                }
+            }
+            return res;
+        }
+public:
+    bool checkPerfectNumber(int num) {
+        vector<int> divisors = getDivisors(num);
+        int sum = accumulate(divisors.begin(), divisors.end(), 0);
+        return sum == num;
+    }
+};
