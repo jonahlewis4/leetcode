f012984ec483073ff1752b68a728adb738540468
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-07-10 13:23:48 -0700

    Time: 0 ms (100%), Space: 15.3 MB (53.1%) - LeetHub

--- 0372-super-pow/0372-super-pow.cpp
+++ 0372-super-pow/0372-super-pow.cpp
@@ -2,36 +2,24 @@ class Solution {
 private:
     static int modPow(int a, int power) {
         a %= 1337;
-        if(power == 0) {
-            return 1;
-        }
-        int runningPower = 1;
-        int runningResult = a;
-        while(runningPower != power) {
-            runningResult *= runningResult;
-            runningResult %= 1337;
-
-            runningPower *= 2;
-
-            if(runningPower * 2 > power) {
-                int difference = power - runningPower;
-                int subPow = modPow(a, difference);
-                return (subPow * runningResult) % 1337;
+        int result = 1;
+        while(power > 0) {
+            if(power & 1) {
+                result *= (a);
+                result %= 1337;
             }
+            a = (a * a) % 1337;
+            power >>= 1;
         }
-        return runningResult;
+        return result;
     }
 public:
     int superPow(int a, vector<int>& b) {
-        int oldA = a;
-        a = modPow(a, b.front());
-        for(int i = 1; i < b.size(); i++) {
-            a = modPow(a, 10);
-            int digit = b[i];
-            int next = modPow(oldA, digit);
-            a = (a * next) % 1337;
+        int result = 1;
+        for(const int digit : b) {
+            result = (modPow(result, 10) * modPow(a, digit) )%1337;
         }
 
-        return a;
+        return result;
     }
 };
