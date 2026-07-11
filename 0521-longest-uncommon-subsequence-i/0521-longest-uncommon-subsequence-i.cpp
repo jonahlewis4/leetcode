66ce6ad2b5ee6b589ef5f3a03b47b2589dc10417
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-07-11 15:40:27 -0700

    Time: 0 ms (100%), Space: 8 MB (68.95%) - LeetHub

---
+++ 0521-longest-uncommon-subsequence-i/0521-longest-uncommon-subsequence-i.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findLUSlength(string a, string b) {
+        if(a.size() > b.size()) {
+            return a.size();
+        } else if (a.size() < b.size()) {
+            return b.size();
+        }
+        return a == b ? -1 : a.size();
+    }
+};
