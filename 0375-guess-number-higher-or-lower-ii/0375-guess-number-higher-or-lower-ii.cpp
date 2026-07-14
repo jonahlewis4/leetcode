4f40da1bb128f1a4d4b84c13089645ab3c510cbc
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-07-14 14:15:51 -0700

    Time: 30 ms (94.85%), Space: 11.2 MB (27.66%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -28,8 +28,44 @@ private:
     }
 public:
     int getMoneyAmount(int n) {
-        //1 and n
-        dp.resize(n + 1, vector<int>(n + 1, 0));
-        return c(1, n);
+        if(n == 1) {
+            return 0;
+        }
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1));
+        
+        dp[n][n] = 0;
+        dp[n-1][n] = n - 1;
+
+        for(int l = n - 2; l >= 1; l--) {
+            dp[l][l] = 0;
+            dp[l][l+1] = l;
+            for(int r = l+2; r <= n; r++) {
+                // //1 2 3 4
+                //1//0 1 2 ?
+                //2//  0 1 3
+                //3//    0 1 
+                //4//      0
+
+                int smallest = INT_MAX;
+                for(int guess = l + 1; guess <= r - 1; guess++) {
+                    int cost = guess + max(dp[l][guess - 1], dp[guess + 1][r]);
+                    smallest = min(smallest, cost);
+                }
+                dp[l][r] = smallest;
+            }
+        }
+
+        
+        return dp[1].back();
+
+    }
+    void p(vector<vector<int>> dp) {
+        int n = dp.size() - 1;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                cout<<dp[i][j]<<" ";
+            }
+            cout<<endl;
+        }
     }
 };
