90004bbd529021e97e2c2ecc0e050dc17cd83d8b
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-07-14 19:07:20 -0700

    Time: 0 ms (100%), Space: 10 MB (86.18%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
@@ -1,38 +1,20 @@
 class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
-        vector<int> up(nums.size());
-        vector<int> down(nums.size());
+        int up = 1;
+        int down = 1;
 
-        for(int i = 0; i < nums.size(); i++) {
+        for(int i = 1; i < nums.size(); i++) {
             int upMax = 0;
             int downMax = 0;
-            for(int j = i - 1; j >= 0; j--) {
-                if(upMax && downMax){
-                    break;
-                }
-
-                if(nums[j] < nums[i]) {
-                    upMax = max(down[j], upMax);
-                }
-                if(nums[j] > nums[i]) {
-                    downMax =  max(up[j], downMax);
-                }
+        
+            if(nums[i] < nums[i-1]) {
+                up = down + 1;
             }
-            upMax++;
-            downMax++;
-            up[i] = upMax;
-            down[i] = downMax;
+            if(nums[i] > nums[i-1]) {
+                down = up + 1;
+            }
         }
-
-        for(int i = 0; i < nums.size(); i++) {
-            cout<<up[i]<<" ";
-        }
-        cout<<endl;
-        for(int i = 0; i < nums.size(); i++) {
-            cout<<down[i]<<" ";
-        }
-        cout<<endl;
-        return max(up.back(), down.back());
+        return max(up, down);
     }
 };
