eb5213bc1b285bff4436f291e3304c7861c2e192
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-07-19 11:24:54 -0700

    Time: 1089 ms (5.02%), Space: 12.1 MB (93.94%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(const string& s) {
+        string build = "";
+        for(int i = 0; i < s.size(); i++) {
+            char c = s[i];
+            build.push_back(c);
+            
+            int start = 0;
+            while(true) {
+                int newStart = s.find(build, start+build.size());
+                if(newStart == string::npos || newStart - start > build.size()) {
+                    break;
+                }
+                if(newStart + build.size() == s.size()) {
+                    cout<<build<<" repeats"<<endl;
+                    return true;
+                }
+
+                start = newStart;
+
+            }
+        }
+
+        return false;
+    }
+};
