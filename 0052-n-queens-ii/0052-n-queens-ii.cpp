9e0a7f3ab96d4441f1d072e0b542bc972fce6ad4
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-07-20 13:53:51 -0700

    Time: 0 ms (100%), Space: 7.8 MB (96.69%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.cpp
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -1,14 +1,10 @@
 class Solution {
     int total = 0;
     int n = 0;
-    vector<bool> rows;
     vector<bool> cols;
     vector<bool> sumDiag;
     vector<bool> diffDiag;
     bool valid(int row, int col) const {
-        if(rows[row]) {
-            return false;
-        }
         if(cols[col]) {
             return false;
         }
@@ -22,13 +18,11 @@ class Solution {
     }
 
     void set(int row, int col) {
-        rows[row] = true;
         cols[col] = true;
         sumDiag[row+col] = true;
         diffDiag[row-col+n-1] = true;
     }
     void unset(int row, int col) {
-        rows[row] = false;
         cols[col] = false;
         sumDiag[row+col] = false;
         diffDiag[row-col+n-1] = false;
@@ -50,7 +44,6 @@ class Solution {
     }
 public:
     int totalNQueens(int n) {
-        rows.resize(n, false);
         cols.resize(n, false);
         sumDiag.resize(n*2-1, false);
         diffDiag.resize(n*2-1, false);
