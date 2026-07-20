91e2d7eef715f0b5379b366db950c9869b193e34
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-07-20 13:51:58 -0700

    Time: 3 ms (39.07%), Space: 7.8 MB (90.06%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,61 @@
+class Solution {
+    int total = 0;
+    int n = 0;
+    vector<bool> rows;
+    vector<bool> cols;
+    vector<bool> sumDiag;
+    vector<bool> diffDiag;
+    bool valid(int row, int col) const {
+        if(rows[row]) {
+            return false;
+        }
+        if(cols[col]) {
+            return false;
+        }
+        if(sumDiag[row+col]) {
+            return false;
+        }
+        if(diffDiag[row-col+n-1]){
+            return false;
+        }
+        return true;
+    }
+
+    void set(int row, int col) {
+        rows[row] = true;
+        cols[col] = true;
+        sumDiag[row+col] = true;
+        diffDiag[row-col+n-1] = true;
+    }
+    void unset(int row, int col) {
+        rows[row] = false;
+        cols[col] = false;
+        sumDiag[row+col] = false;
+        diffDiag[row-col+n-1] = false;
+    }
+
+    void r(int row) {
+        if(row == n) {
+            total++;
+            return;
+        }
+        for(int c = 0; c < n; c++) {
+            //if a queen can go in this spot, add it, go down a row, then backtrack
+            if(valid(row,c)){
+                set(row,c);
+                r(row+1);
+                unset(row,c);
+            }
+        }
+    }
+public:
+    int totalNQueens(int n) {
+        rows.resize(n, false);
+        cols.resize(n, false);
+        sumDiag.resize(n*2-1, false);
+        diffDiag.resize(n*2-1, false);
+        this->n = n;
+        r(0);
+        return total;
+    }
+};
