d31ed8a798ab33dd7208fd5a44dd947f09933b4f
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-07-20 22:33:17 -0700

    Time: 6 ms (70.71%), Space: 17.2 MB (98.44%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,40 +1,47 @@
 class Solution {
-    static int largestRectangleInHistogram(vector<int>& hist) {
-        stack<int> stack;
-        stack.push(-1); 
-        hist.push_back(0);
-
-        int best = 0;
-        for(int i = 0; i < hist.size(); i++) {
-            int curHeight = hist[i];
-            while(stack.top() != -1 && hist[stack.top()] > curHeight) {
-                int popped = stack.top();
-                stack.pop();
-                int oldHeight = hist[popped];
-                int width = i - stack.top() - 1;
-                int area = oldHeight * width;
-                best = max(best, area);
-
-            }
-            stack.push(i);
-        }
-
-        return best;
-    }
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
+        vector<int> left(matrix.front().size(), 0);
+        vector<int> right(matrix.front().size(), matrix.front().size());
+        vector<int> h(matrix.front().size(), 0);
         int best = 0;
-        vector<int> histogram(matrix.front().size(), 0);
-        for(int r = 0; r < matrix.size(); r++) {
+        for(int r = 0 ; r < matrix.size(); r++) {
+            //update heights
             for(int c = 0; c < matrix.front().size(); c++) {
                 if(matrix[r][c] == '1') {
-                    histogram[c]++;
+                    h[c]++;
                 } else {
-                    histogram[c] = 0;
+                    h[c] = 0;
                 }
             }
-            int largest = largestRectangleInHistogram(histogram);
-            best = max(best, largest);
+
+            //update to most restrictive boundary
+            int lastZero = -1;
+            for(int c = 0; c < matrix.front().size(); c++) {
+                if(matrix[r][c] == '1') {
+                    left[c] = max(lastZero + 1, left[c]);
+                } else {
+                    left[c] = 0;
+                    lastZero = c;
+                }
+            }   
+
+            lastZero = matrix.front().size();
+
+            for(int c = matrix.front().size() - 1; c >= 0; c--) {
+                if(matrix[r][c] == '1') {
+                    right[c] = min(lastZero-1, right[c]);
+                } else {
+                    right[c] = matrix.front().size() - 1;
+                    lastZero = c;
+                }
+
+                int width = right[c] - left[c] + 1;
+                int area = h[c] * width;
+                best = max(area, best);
+                
+            }
+            
         }
         return best;
     }
