65712074d00cac5a932a6fd4d7d6759f1c32344b
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-07-20 19:13:56 -0700

    Time: 0 ms (100%), Space: 7.8 MB (94.21%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,58 +1,36 @@
 class Solution {
-    static int factorial(int n) {
-        int res = 1;
-        for(int i = 1; i <= n; i++) {
-            res *= i;
-        }
-        return res;
-    }
-    static string search(int before, int subProblemSize, vector<bool>& used, int k) {
-        
-        int unusedNumbers = 0;
-        for(const bool wasUsed : used) {
-            if(!wasUsed){
-                unusedNumbers++;
-            }
-        }
-
-
-        if(unusedNumbers == 1) {
-            int res = 0;
-            for(int i = 0; i < used.size(); i++) {
-                if(!used[i]) {
-                    return to_string(i + 1);
-                }
-            }
-        }
-
-        int remaining = k - before;
-        int subproblemsToSkip = (remaining-1) / subProblemSize;
-        
-
-        int pickedSubProblem = 0;
-        int skipped = -1;
-        for(int i = 0; i < used.size(); i++) {
-            if(skipped == subproblemsToSkip) {
-                break;
-            }
-
-            if(!used[i]) {
-                skipped++;
-                pickedSubProblem = i + 1;
-            }
-        }
-
-        used[pickedSubProblem-1] = true;
-        unusedNumbers--;
-        return to_string(pickedSubProblem) + search(before + subproblemsToSkip * subProblemSize, subProblemSize/unusedNumbers, used, k);
-    }
 public:
     string getPermutation(int n, int k) {
-        vector<bool> used(n, false);
-        int totalPermutations = factorial(n);
-        int subProblemSize = totalPermutations / n;
+        vector<int> choices;
+        int subProblemSize = 1;
+        for(int i = 1; i <= n; i++) {
+            choices.push_back(i);
+            subProblemSize *= i;
+        }
 
-        return search(0, subProblemSize, used, k);
+        subProblemSize /= n;
 
+        string res = "";
+        for(int i = 1; i <= n; i++) {
+            if(k == 0) {
+                break;
+            }
+            cout<<i<<endl;
+            int bucket = (k-1) / subProblemSize;
+            res.push_back(choices[bucket] + '0');
+            choices.erase(choices.begin() + bucket);
+
+            k %= subProblemSize;
+            if(k == 0) {
+                k += subProblemSize;
+            }
+
+            if(subProblemSize > 1) {
+                subProblemSize /= (n-i); 
+            }
+
+        }
+
+        return res;
     }
 };
