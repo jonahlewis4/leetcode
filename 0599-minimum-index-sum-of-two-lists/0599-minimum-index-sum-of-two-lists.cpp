fb147806e3ccd2c8af9da982dd6db3351f14fa2b
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-07-25 12:41:48 -0700

    Time: 12 ms (87.79%), Space: 41.8 MB (20.03%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cpp
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
-        unordered_map<string, int> m1;
-        for(int i = 0; i < list1.size(); i++) {
-            string s = list1[i];
-            m1[s] = i;
-        }
         unordered_map<string, int> m2;
         for(int i = 0; i < list2.size(); i++) {
             string s = list2[i];
@@ -14,7 +9,8 @@ public:
 
         int curSum = INT_MAX;
         vector<string> res;
-        for(const auto [key, idx] : m1) {
+        for(int idx = 0; idx < list1.size(); idx++) {
+            string& key = list1[idx];
             if(m2.contains(key)) {
                 int sum = idx + m2[key];
                 if(sum < curSum) {
