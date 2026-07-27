12677a62f6bc1f1497d5c6539afdf0f161aebbb8
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-07-27 14:25:52 -0700

    Time: 0 ms (100%), Space: 13.5 MB (60.66%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
@@ -3,8 +3,6 @@ public:
     int maxProduct(vector<int>& nums) {
         int biggest = INT_MIN;
         int secondBiggest = INT_MIN;
-        int smallest = INT_MAX;
-        int secondSmallest = INT_MAX;
 
         for(const int num : nums) {
             if(num > biggest) {
@@ -14,18 +12,9 @@ public:
                 secondBiggest = num;
             }
 
-            if(num < smallest) {
-                secondSmallest = smallest;
-                smallest = num;
-            } else if (num < secondSmallest) {
-                secondSmallest = num;
-            }
         }
 
-        return max(
-            (biggest - 1) * (secondBiggest - 1),
-            (smallest - 1) * (secondSmallest - 1)
+        return (biggest - 1) * (secondBiggest - 1);
 
-        );
     }
 };
