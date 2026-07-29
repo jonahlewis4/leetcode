316396e51df3b99963c6bdc150f9cc54797f58e7
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-07-29 01:42:23 -0700

    Time: 0 ms (100%), Space: 18.4 MB (31.34%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -12,34 +12,31 @@ public:
             {0,-1},
         };
 
-        queue<pair<int, int>> q;
-        q.push({sr, sc});
+        stack<pair<int, int>> stack;
+        stack.push({sr, sc});
         
         image[sr][sc] = color;
 
-        while(!q.empty()) {
-            int n = q.size();
-            for(int i = 0; i < n; i++) {
-                pair<int, int> point = q.front();
-                q.pop();
+        while(!stack.empty()) {
+            pair<int, int> point = stack.top();
+            stack.pop();
 
-                int r = point.first;
-                int c = point.second;
+            int r = point.first;
+            int c = point.second;
 
-                for(const pair<int, int>& dir : dirs) {
-                    int rDelta = dir.first;
-                    int cDelta = dir.second;
-                    int newR = r + rDelta;
-                    int newC = c + cDelta;
+            for(const pair<int, int>& dir : dirs) {
+                int rDelta = dir.first;
+                int cDelta = dir.second;
+                int newR = r + rDelta;
+                int newC = c + cDelta;
 
-                    if(newR < 0 || newC < 0 || newR >= image.size() || newC >= image.front().size()) {
-                        continue;
-                    }
+                if(newR < 0 || newC < 0 || newR >= image.size() || newC >= image.front().size()) {
+                    continue;
+                }
 
-                    if(image[newR][newC] == ogVal && image[newR][newC] == ogVal) {
-                        image[newR][newC] = color;
-                        q.push({newR, newC});
-                    }
+                if(image[newR][newC] == ogVal && image[newR][newC] == ogVal) {
+                    image[newR][newC] = color;
+                    stack.push({newR, newC});
                 }
             }
         }
