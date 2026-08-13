0cb01fbd7aa3dfd31e22670b2ffac929b91fa70c
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-08-12 18:30:24 -0700

    Time: 11 ms (24.04%), Space: 18.3 MB (42.55%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        priority_queue<int> pq;
-        for(const vector<int>& row : matrix) {
-            for(const int val : row) {
-                pq.push(val);
-                if(pq.size() > k) {
-                    pq.pop();
-                }
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> pq;
+        
+        for(int r = 0; r < matrix.size(); r++) {
+            pq.push({matrix[r][0], r, 0});
+        }
+    
+        for(int i = 1; i < k; i++) {
+            auto [val, row, col] = pq.top();
+            pq.pop();
+            if(col + 1 < matrix.size()) {
+                pq.push({matrix[row][col + 1], row, col + 1});
             }
         }
-        return pq.top();
+
+        tuple<int, int, int> res = pq.top();
+        return get<0>(res);
+
+        
     }
 };
