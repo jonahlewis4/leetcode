4bb8c6be05a6e7ebaf472eeccaaa55de00700847
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-08-12 18:54:14 -0700

    Time: 5 ms (49.44%), Space: 17.1 MB (64.7%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,22 +1,33 @@
 class Solution {
+    int ltEq(vector<vector<int>>& matrix, int val) {
+        int total = 0;
+        for(const vector<int>& row : matrix) {
+            for(const int x : row) {
+                if(x <= val) {
+                    total++;
+                }
+            }
+        }
+        return total;
+    }
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> pq;
-        
-        for(int r = 0; r < matrix.size(); r++) {
-            pq.push({matrix[r][0], r, 0});
-        }
-    
-        for(int i = 1; i < k; i++) {
-            auto [val, row, col] = pq.top();
-            pq.pop();
-            if(col + 1 < matrix.size()) {
-                pq.push({matrix[row][col + 1], row, col + 1});
+        int l = matrix.front().front();
+        int r = matrix.back().back();
+        int res = r;
+        while(l <= r) {
+            int m = l + (r-l)/2;
+            
+            int lessOrEqual = ltEq(matrix, m);
+            if(lessOrEqual >= k) {
+                r = m - 1;
+                res = m;
+            } else {
+                l = m + 1;
             }
         }
 
-        tuple<int, int, int> res = pq.top();
-        return get<0>(res);
+        return res;
 
         
     }
