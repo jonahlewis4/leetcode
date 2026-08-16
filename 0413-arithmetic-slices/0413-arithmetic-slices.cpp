8045d6d1817ac179bbc60716bbd8ace847e40a91
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-08-15 18:51:14 -0700

    Time: 0 ms (100%), Space: 10.9 MB (60.26%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.cpp
+++ 0413-arithmetic-slices/0413-arithmetic-slices.cpp
@@ -2,16 +2,21 @@ class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
         int total = 0;
-        for(int i = 0; i + 2 < nums.size(); i++) {
-            int prevDiff = nums[i+1] - nums[i];
-            for(int j = i + 2; j < nums.size(); j++) {
-                int curDiff = nums[j] - nums[j-1];
-                if(prevDiff == curDiff) {
-                    total++;
-                } else {
-                    break;
-                }
+        if(nums.size() < 3) {
+            return 0;
+        }
+        int prevDiff = nums[1] - nums.front();
+        int streak = 0;
+        for(int i = 2; i < nums.size(); i++) {
+            int curDiff = nums[i] - nums[i - 1];
+            if(curDiff == prevDiff){
+                streak++;
+                total += streak;
+            } else {
+                streak = 0;
             }
+            prevDiff = curDiff;
+            
         }
         return total;
     }
