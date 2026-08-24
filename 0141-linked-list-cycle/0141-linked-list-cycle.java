feea34761b1501853e7f8ab3ae18715cc53181ff
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-08-24 06:42:21 -0500

    Time: 1 ms (13.02%), Space: 46.7 MB (73.31%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while(true) {
+            slow = slow.next;
+            if(slow == null) {
+                return false;
+            }
+            if(fast == null) {
+                fast = head;
+            } else {
+                fast = fast.next;
+            }
+            if(fast == null) {
+                fast = head;
+            } else {
+                fast = fast.next;
+            }
+            if(slow == fast) {
+                return true;
+            }
+        }
+    }
+}
