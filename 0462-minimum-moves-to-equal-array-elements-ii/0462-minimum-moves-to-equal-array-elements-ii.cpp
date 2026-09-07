2013d961de6b0a5880e4a222d2b76df0eefbf3b2
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-09-07 14:06:26 -0400

    Time: 4 ms (15.95%), Space: 15.7 MB (81.2%) - LeetHub

--- 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,21 +1,16 @@
 class Solution {
-    pair<int, int> middle_element(vector<int>& nums) {
+    int middle_element(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        if(nums.size() % 2 == 1) {
-            return {nums[nums.size()/2], nums[nums.size()/2]};
-        }
-        return {nums[nums.size()/2], nums[nums.size()/2-1]};
+        return nums[nums.size()/2];
     }
 public:
     int minMoves2(vector<int>& nums) {
-        pair<int, int> targ = middle_element(nums);
+        int targ = middle_element(nums);
 
         int res1 = 0;
-        int res2 = 0;
-        for(const int num : nums) {
-            res1 += abs(num - targ.first);
-            res2 += abs(num - targ.second);
+                for(const int num : nums) {
+            res1 += abs(num - targ);
         }  
-        return min(res1, res2);     
+        return res1;     
     }
 };
