e0c356a3dc80d24d1789ccc7697fc3792857421e
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-09-07 14:08:44 -0400

    Time: 0 ms (100%), Space: 15.7 MB (50.37%) - LeetHub

--- 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,12 +1,8 @@
 class Solution {
-    int middle_element(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        return nums[nums.size()/2];
-    }
 public:
     int minMoves2(vector<int>& nums) {
-        int targ = middle_element(nums);
-
+        nth_element(nums.begin(), nums.begin() + nums.size()/2, nums.end());
+        int targ = nums[nums.size()/2];
         int res1 = 0;
                 for(const int num : nums) {
             res1 += abs(num - targ);
