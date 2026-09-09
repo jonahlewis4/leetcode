18b340b9b97ba4b566bb871b61d64c507793150c
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-09-08 21:21:17 -0400

    Time: 667 ms (11.16%), Space: 82.8 MB (52.94%) - LeetHub

--- 0464-can-i-win/0464-can-i-win.cpp
+++ 0464-can-i-win/0464-can-i-win.cpp
@@ -4,36 +4,20 @@ class Solution {
     
     //a call to r means: can player 1 win if mask looks like this on their turn.
     unordered_map<int, int> map;
-    bool r(int mask) {
+    bool r(int mask, int localTotal) {
         if(map.contains(mask)) {
             return map[mask];
         }
         int sumAlreadyChosen = 0;
-
-        for(int i = 0; i < topNumber; i++) {
-            if((mask & (1<<i))) {
-                int thisNum = i + 1;
-                sumAlreadyChosen += thisNum;
-            }
-        }
-
-        int localTotal = total - sumAlreadyChosen;
-
-        //if there's a number >= localTotal, return true;
-        for(int i = topNumber - 1; i >= 0; i--) {
-            if((mask & (1<<i)) == 0) {
-                int thisNum = i + 1;
-                if(thisNum >= localTotal) {
-                    map[mask] = true;
-                    return true;
-                }
-            }
-        }
-
+        int goal = total - localTotal;
         //now check that if there is a choice where it is impossible for player 2 guarantee victory
         for(int i = 0; i < topNumber; i++) {
             if((mask & (1<<i)) == 0) {
-                if(!r(mask | (1<<i))){
+                int thisNum = i + 1;
+                if(thisNum >= goal) {
+                    return true;
+                }
+                if(!r(mask | (1<<i), localTotal + thisNum)){
                     map[mask] = true;
                     return true;
                 }                
@@ -53,6 +37,6 @@ public:
         }
         topNumber = maxChoosableInteger;
         total = desiredTotal; 
-        return r(0);
+        return r(0, 0);
     }
 };
