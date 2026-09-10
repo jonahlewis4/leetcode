98db47b72efeb31c681db2e1a139f5ca9570d298
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-09-09 20:45:46 -0400

    Time: 1 ms (38.93%), Space: 10.9 MB (63.65%) - LeetHub

--- 0467-unique-substrings-in-wraparound-string/0467-unique-substrings-in-wraparound-string.cpp
+++ 0467-unique-substrings-in-wraparound-string/0467-unique-substrings-in-wraparound-string.cpp
@@ -4,40 +4,23 @@ class Solution {
     }
 public:
     int findSubstringInWraproundString(string s) {
-        int res = 0;
-        for(char c = 'a'; c <= 'z'; c++) {
-            int start = 0;
-            int end = 1;
-            int best = 0;
-            while(start < s.size()) {
-                while(start < s.size() && s[start] != c) {
-                    start++;
-                    end = start + 1;
-                }
-
-                if(start >= s.size()) {
-                    break;
-                }
-
-                bool streak = false;
-
-                if(end < s.size()) {
-                    char prev = s[end-1];
-                    uint8_t prevVal = char2Position(prev+1);
-                    uint8_t curVal = char2Position(s[end]);
-                    streak = prevVal == curVal;
-                }
-                
-                if(!streak) {
-                    best = max(end - start, best);
-                    start=end;
-                }
-
-                end++;
+        vector<int> longest(26, 0);
+        char firstChar = s.front();
+        longest[firstChar - 'a'] = 1;
+        int streak = 1;
+        for(int i = 1; i < s.size(); i++) {
+            char curChar = s[i];
+            uint8_t pos1 = char2Position(s[i-1] + 1);
+            uint8_t pos2 = char2Position(curChar);
+            if(pos1 == pos2) {
+                streak++;
+            } else {
+                streak = 1;
             }
-            res += best;
+            longest[curChar - 'a'] = max(streak, longest[curChar - 'a']);
+
         }
 
-        return res;
+        return accumulate(longest.begin(), longest.end(), 0);
     }
 };
