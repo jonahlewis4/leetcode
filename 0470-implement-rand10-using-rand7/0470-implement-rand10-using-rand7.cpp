3b83e10606e3074fa33885489aa0c9088d30eb88
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-09-11 19:31:11 -0400

    Time: 5 ms (61.53%), Space: 11.1 MB (3.57%) - LeetHub

---
+++ 0470-implement-rand10-using-rand7/0470-implement-rand10-using-rand7.cpp
@@ -0,0 +1,15 @@
+// The rand7() API is already defined for you.
+// int rand7();
+// @return a random integer in the range 1 to 7
+
+class Solution {
+public:
+    int rand10() {
+        int total = 0;
+        for(int i = 0; i < 5; i++) {
+            int sub = rand7() - 1;
+            total += sub;
+        }
+        return total % 10 + 1;
+    }
+};
