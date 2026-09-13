72770282f13710379ebdf8c01d3f7cb31555b12f
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-09-13 16:08:38 -0400

    Time: 3 ms (94.58%), Space: 22.9 MB (53.06%) - LeetHub

--- 0477-total-hamming-distance/0477-total-hamming-distance.cpp
+++ 0477-total-hamming-distance/0477-total-hamming-distance.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     int totalHammingDistance(vector<int>& nums) {
+        array<int, 32> arr;
+        arr.fill(0);
         int total = 0;
-        for(int i = 0; i < nums.size(); i++) {
-            for(int j = i + 1; j < nums.size(); j++) {
-                int comb = nums[i] ^ nums[j];
-                total += __builtin_popcount(comb);
+        for(const int num : nums) {
+            for(int i = 0; i < 32; i++) {
+                int cur = num & (1<<i);
+                if(cur > 0) {
+                    arr[i]++;
+                }
             }
+        }
+
+        for(int i = 0; i < 32; i++) {
+            int count = arr[i];
+            int otherCount = nums.size() - count;
+            total += count * otherCount;
         }
         return total;
     }
