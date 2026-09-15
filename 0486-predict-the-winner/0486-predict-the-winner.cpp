5c3c8310aaf06adaa8f3b5d02df15d3f85bfbdf0
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-09-15 19:55:39 -0400

    Time: 0 ms (100%), Space: 10.4 MB (10.73%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,7 +1,12 @@
 class Solution {
+    vector<vector<int>> cache;
     int maxScore(int totalPoints, int l, int r, vector<int>& nums) {
         if(l > r) {
             return 0;
+        }
+
+        if(cache[l][r] != -1) {
+            return cache[l][r];
         }
         int opLeft = maxScore(totalPoints - nums[l], l+1, r, nums);
         int ourLeftPoints = totalPoints - opLeft;
@@ -9,11 +14,14 @@ class Solution {
         int opRight = maxScore(totalPoints - nums[r], l, r - 1, nums);
         int ourRightPoints = totalPoints - opRight;
 
-        return max(ourLeftPoints, ourRightPoints);
+        int res = max(ourLeftPoints, ourRightPoints);
+        cache[l][r] = res;
+        return res;
         
     }
 public:
     bool predictTheWinner(vector<int>& nums) {
+        cache.resize(nums.size(), vector<int>(nums.size(), -1));
         int sum = accumulate(nums.begin(), nums.end(), 0);
         int bestScore = maxScore(sum, 0, nums.size() - 1, nums);
         int opScore = sum - bestScore;
