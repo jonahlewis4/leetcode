333206b1ce82f804ffd7219d1308660bd056e702
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-09-17 20:31:39 -0400

    Time: 67 ms (17.89%), Space: 36.6 MB (22.43%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> findSubsequences(vector<int>& nums) {
-        unordered_set<string> used;
-        vector<vector<string>> cache(nums.size());
+        set<vector<int>> used;
+        vector<vector<vector<int>>> cache(nums.size());
         for(int i = nums.size() - 2; i >= 0; i--) {
             for(int j = i + 1; j < nums.size(); j++) {
                 if(nums[j] >= nums[i]) {
-                    string next = to_string(nums[j]) + "." + to_string(nums[i]);
+                    vector<int> next = {nums[j], nums[i]};
                      if(!used.contains(next)) {
                         used.insert(next);
                         cache[i].push_back(next);
                     }
-                    for(const string& s : cache[j]) {
-                        string next = s + "."  + to_string(nums[i]);
+                    for(vector<int> next : cache[j]) {
+                        next.push_back(nums[i]);
                         if(!used.contains(next)) {
                             used.insert(next);
                             cache[i].push_back(next);
@@ -22,26 +22,11 @@ public:
             }
         }
         vector<vector<int>> res;
-        for(const string s : used) {
-            res.push_back({});
-            int cur = 0;
-            int pow = 1;
-            for(int i = s.size() - 1; i >= 0; i--) {
-                if(s[i] == '-'){
-                    cur = -cur;
-                    continue;
-                }
-                if(s[i] == '.'){
-                    res.back().push_back(cur);
-                    cur = 0;
-                    pow = 1;
-                    continue;
-
-                }
-                cur += pow * (s[i] - '0');
-                pow *= 10;
-            }
-            res.back().push_back(cur);
+        for(const vector<int>& v : used) {
+            res.push_back(v);
+        }
+        for(vector<int>& v : res) {
+            reverse(v.begin(), v.end());
         }
         return res;
     }
