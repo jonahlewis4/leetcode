f5b8b8f0772b3174839d63e3f414e021150fa1ca
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-09-19 11:44:10 -0500

    Time: 2 ms (79.2%), Space: 28.1 MB (94.39%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,41 +1,28 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        vector<pair<int, int>> descend(nums.size());
-        for(int i = 0; i < nums.size(); i++) {
-            descend[i] = {nums[i], i};
+        stack<int> stack;
+
+        vector<int> res(nums.size(), INT_MIN);
+        for(int i = 0; i < nums.size() * 2; i++) {
+            int idx = i % nums.size();
+
+            while(!stack.empty() && nums[stack.top()] < nums[idx]){
+                res[stack.top()] = nums[idx];
+                stack.pop();
+            }
+            if(res[idx] == INT_MIN) {
+                stack.push(idx);
+            }
         }
-        sort(descend.begin(), descend.end(), greater<>());
-        set<int> avail;
-        vector<int> res(nums.size());
-        vector<int> updates;
-        for(int i = 0; i < descend.size(); i++) {
-            if(i > 0 && descend[i].first < descend[i - 1].first) {
-                while(!updates.empty()) {
-                    avail.insert(updates.back());
-                    updates.pop_back();
-                }
-            }
-            int num = descend[i].first;
-            int idx = descend[i].second;
 
-            //search for one after idx in avail
-            set<int>::iterator itr = avail.upper_bound(idx);
-            if(itr != avail.end()) {
-                int ansIdx = *itr;
-                res[idx] = nums[ansIdx];
-            } else {
-                if(!avail.empty()) {
-                    int ansIdx = *avail.begin();
-                    res[idx] = nums[ansIdx];
-                } else {
-                    res[idx] = -1;
-                }
-            }
 
-            updates.push_back(idx);
+        while(!stack.empty()) {
+            res[stack.top()] = -1;
+            stack.pop();
         }
 
         return res;
+
     }
 };
