8c8d54c0ca7c59ff262941af1b592657e3c2e9f8
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-09-21 18:56:25 -0500

    Time: 15 ms (99.85%), Space: 9.2 MB (98.33%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,31 +1,48 @@
 class Solution {
-    int calc(int l, int r, const string& s) {
-        if(l > r) {
-            return 0;
-        }
-        if(l == r) {
-            return 1;
-        }
+    // int calc(int l, int r, const string& s) {
+    //     if(l > r) {
+    //         return 0;
+    //     }
+    //     if(l == r) {
+    //         return 1;
+    //     }
 
-        if(cache[l][r] != -1) {
-            return cache[l][r];
-        }
-        if(s[l] == s[r]) {
-            int res = 2 + calc(l+1, r-1, s);
-            cache[l][r] = res;
-            return res;
-        }
-        int optL = calc(l+1, r, s);
-        int optR = calc(l, r-1, s);
+    //     if(cache[l][r] != -1) {
+    //         return cache[l][r];
+    //     }
+    //     if(s[l] == s[r]) {
+    //         int res = 2 + calc(l+1, r-1, s);
+    //         cache[l][r] = res;
+    //         return res;
+    //     }
+    //     int optL = calc(l+1, r, s);
+    //     int optR = calc(l, r-1, s);
         
-        int res = max(optL, optR);
-        cache[l][r] = res;
-        return res;
-    }
-    vector<vector<int>> cache;
+    //     int res = max(optL, optR);
+    //     cache[l][r] = res;
+    //     return res;
+    // }
+    
 public:
     int longestPalindromeSubseq(string s) {
-        cache.resize(s.size(), vector<int>(s.size(), -1));
-        return calc(0, s.size() - 1, s);
+        vector<int> dp(s.size(), 0);
+        for(int l = s.size() - 1; l >= 0; l--) {
+            int botLeft = 0;
+            dp[l] = 1;
+            for(int r = l + 1; r < s.size(); r++) {
+                int res;
+                if(s[l] == s[r]) {
+                    res = 2 + botLeft;
+                } else {
+                    int optL = dp[r-1];
+                    int optB = dp[r];
+                    res = max(optL, optB);
+                }
+                botLeft = dp[r];
+                dp[r] = res;
+            }
+        }
+
+        return dp.back();
     }
 };
