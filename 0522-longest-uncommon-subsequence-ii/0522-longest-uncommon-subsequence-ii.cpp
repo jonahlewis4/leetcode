0d65bcd8fb4197cd037e86144f6e1bd0d6ee0a0a
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-09-22 19:27:00 -0500

    Time: 0 ms (100%), Space: 11.1 MB (21.5%) - LeetHub

---
+++ 0522-longest-uncommon-subsequence-ii/0522-longest-uncommon-subsequence-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    bool isSub(const string& small, const string& large) {
+        int smallI = 0;
+        int largeI = 0;
+
+        while(largeI < large.size()){
+            if(large[largeI] == small[smallI]) {
+                smallI++;
+            }
+            if(smallI >= small.size()){
+                return true;
+            }
+            largeI++;
+        }
+
+        return false;
+    }
+public:
+    int findLUSlength(vector<string>& strs) {
+        sort(strs.begin(), strs.end(), [](const string&a, const string&b){return a.size() > b.size();});
+        for(int i = 0; i < strs.size(); i++) {
+            bool works = true;
+            for(int j = 0; j < strs.size() && strs[j].size() >= strs[i].size(); j++) {
+                if(i==j){
+                    continue;
+                }
+                if(isSub(strs[i], strs[j])) {
+                    works = false;
+                    break;
+                }
+            }
+            if(works) {
+                return strs[i].size();
+            }
+        }
+        return -1;
+    }
+};
