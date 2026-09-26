e3d9a67ffde8e025ee0775ad41edda63955dbf4b
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-09-26 17:27:21 -0500

    Time: 2 ms (94.67%), Space: 9 MB (24.56%) - LeetHub

---
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private: 
+    int n;
+    int r(int i, int mask) {
+        if(i > n) {
+            return 1;
+        }
+        
+        if(cache[mask] != -1) {
+            return cache[mask];
+        }
+        int total = 0;
+        for(int j = 1; j <= n; j++) {
+            if((mask&(1<<j))!=0) {
+                continue;
+            }
+
+            if(!(i % j == 0 || j%i==0)){
+                continue;
+            }
+
+            int newMask = (mask | (1<<j));
+            total += r(i+1, newMask);
+        }
+        cache[mask] = total;
+        return total;
+    }
+public:
+    vector<int> cache;
+    int countArrangement(int n) {
+        cache.resize((1<<n+1)+1,-1);
+        this->n = n;
+        return r(1, 0);
+    }
+};
