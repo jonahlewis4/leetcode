92ee70e2b3403033cf3476bd338d987046705bae
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-09-25 19:35:30 -0500

    Time: 19 ms (7.6%), Space: 27.4 MB (16.29%) - LeetHub

--- 0554-brick-wall/0554-brick-wall.cpp
+++ 0554-brick-wall/0554-brick-wall.cpp
@@ -1,53 +1,36 @@
 class Solution {
 public:
     int leastBricks(vector<vector<int>>& wall) {
-        vector<pair<long long, int>> sweep;
-        
-        bool all1 = true;
+        map<long long, int> sweep;
+        bool all1s = true;
         for(const vector<int>& row : wall) {
-            
             long long sum = 0;
             for(const int brick : row) {
                 long long start = sum + 1;
                 sum += brick;
                 long long end = sum - 1;
-                sweep.push_back({start, 1});
-                sweep.push_back({end + 1, -1});
+                sweep[start]++;
+                sweep[end+1]--;
             }
             if(row.size() != 1) {
-                all1 = false;
+                all1s=false;
             }
         }
-
-        if(all1) {
+        if(all1s) {
             return wall.size();
         }
-        long long sum = 0;
+        int sum = 0;
 
-        sort(sweep.begin(), sweep.end(), [](const pair<long long, int>& a, const pair<long long, int>& b) {
-            if(a.first < b.first) {
-                return true;
+        long long rowLen = accumulate(wall.front().begin(), wall.front().end(), 0l);
+        int least = INT_MAX;
+        for(const auto&[key, value] : sweep) {
+            if(key == rowLen){
+                continue;
             }
-            if(a.first > b.first) {
-                return false;
-            }
-            return a.second > b.second;
-        });
-
-        long long least = INT_MAX;
-        int prevStamp = sweep.front().first;
-        for(const pair<long long, int>& event : sweep) {
-            //cout<<"event: "<<event.front()<<"|"<<event.back()<<endl;
-            int stamp = event.first;
-            if(stamp != prevStamp) {
-                //cout<<sum<<endl;
-                least = min(least, sum);
-            }
-            int num = event.second;
+            int num = value;
             sum += num;
-            prevStamp = stamp;
+            least = min(least, sum);
         }
-        //least = min(least, sum);
         return least;
     }
 };
