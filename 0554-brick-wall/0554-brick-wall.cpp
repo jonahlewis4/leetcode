53a7e8183b282427d106370f46f6c76969b265a5
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2026-09-25 19:42:16 -0500

    Time: 3 ms (94.46%), Space: 26.5 MB (75.35%) - LeetHub

--- 0554-brick-wall/0554-brick-wall.cpp
+++ 0554-brick-wall/0554-brick-wall.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
     int leastBricks(vector<vector<int>>& wall) {
-        map<long long, int> sweep;
-        bool all1s = true;
+        unordered_map<long long, int> sweep;
         for(const vector<int>& row : wall) {
             long long sum = 0;
-            for(const int brick : row) {
-                long long start = sum + 1;
+            for(int i = 0; i < row.size() - 1; i++) {
+                int brick  = row[i];
                 sum += brick;
-                long long end = sum - 1;
-                sweep[start]++;
-                sweep[end+1]--;
-            }
-            if(row.size() != 1) {
-                all1s=false;
+                long long end = sum;
+                sweep[end]++;
             }
         }
-        if(all1s) {
-            return wall.size();
-        }
-        int sum = 0;
 
-        long long rowLen = accumulate(wall.front().begin(), wall.front().end(), 0l);
-        int least = INT_MAX;
-        for(const auto&[key, value] : sweep) {
-            if(key == rowLen){
-                continue;
-            }
-            int num = value;
-            sum += num;
-            least = min(least, sum);
+        int least = 0;
+        for(const auto& [key, value] : sweep) {
+            least = max(least, value);
         }
-        return least;
+        return wall.size()-least;
+        
     }
 };
